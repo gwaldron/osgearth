944f88c424898177b7bdab1e9000c6fadd111548
Jason Beverage <jasonbeverage@pelicanmapping.com>
2008-12-10 17:37:15 +0000

    Added open source headers and removed use of libsquish.  Files are now cached to their native format.

--- src/osgEarth/FileCache.cpp
+++ src/osgEarth/FileCache.cpp
@@ -1,7 +1,24 @@
-#include <osgEarth/FileCache>
-#include <osgEarth/ImageUtils>
-#include <osgEarth/md5>
+/* -*-c++-*- */
+/* osgEarth - Dynamic map generation toolkit for OpenSceneGraph
+ * Copyright 2008-2009 Pelican Ventures, Inc.
+ * http://osgearth.org
+ *
+ * osgEarth is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 
+#include <osgEarth/FileCache>
+#include <osgEarth/md5>
 #include <osgDB/ReadFile>
 #include <osgDB/WriteFile>
 #include <osgDB/FileNameUtils>
@@ -24,7 +41,7 @@ std::string md5hash(std::string filename)
 
 osgEarth::FileCache::FileCache(const std::string &_cache_path)
 {
-    image_extension = "dds";
+    image_extension = "";
     cache_path = _cache_path;
     _offline = false;
 }
@@ -69,10 +86,6 @@ osg::Image* osgEarth::FileCache::readImageFile(const std::string& filename, cons
         //Write the file to the cache
         if (!cachedFilename.empty())
         {
-            if ((image_extension == "dds") && (ImageUtils::canCompress( image.get() ) ) )
-            {
-                image = ImageUtils::compress(image.get(), ImageUtils::LOW);
-            }
             osg::notify(osg::INFO) << "Writing " << filename << " to cache " << cachedFilename <<  std::endl;
             osgDB::writeImageFile(*image.get(), cachedFilename, options);
         }
--- src/osgEarth/Mercator.cpp
+++ src/osgEarth/Mercator.cpp
@@ -1,6 +1,24 @@
+/* -*-c++-*- */
+/* osgEarth - Dynamic map generation toolkit for OpenSceneGraph
+ * Copyright 2008-2009 Pelican Ventures, Inc.
+ * http://osgearth.org
+ *
+ * osgEarth is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 #include <osgEarth/Mercator>
 #include <osgEarth/PlateCarre>
-#include <osgEarth/ImageUtils>
 #include <osg/Math>
 #include <osg/Notify>
 #include <sstream>
@@ -243,10 +261,6 @@ struct MercatorTile
     MercatorTile( MercatorTileKey* _key, MercatorTileSource* source ) : key( _key )
     {
         image = source->createImage( key.get() );
-        if (image.valid() && osgEarth::ImageUtils::isCompressed(image.get()))
-        {
-            image = osgEarth::ImageUtils::decompress( image.get(), GL_RGB );
-        }
         key->getPixelExtents( min_x, min_y, max_x, max_y );
         double dummy;
         key->getGeoExtents( dummy, min_lat, dummy, max_lat );

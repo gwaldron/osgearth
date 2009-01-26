fa0caf373e24c742ac5fa9c0d0e3cb82f08b370a
Jason Beverage <jasonbeverage@pelicanmapping.com>
2009-01-26 03:54:02 +0000

    Rewrote isRelativePath and getFullPath to not depend on getRealPath as the resulting paths may not actually exist and realpath will fail on linux.

--- src/osgEarth/FileUtils.cpp
+++ src/osgEarth/FileUtils.cpp
@@ -20,27 +20,81 @@
 #include <osgEarth/FileUtils>
 #include <osgDB/FileNameUtils>
 #include <osg/Notify>
+#include <list>
 
 using namespace osgEarth;
 
 bool osgEarth::isRelativePath(const std::string& fileName)
 {
-    std::string real_path = osgDB::convertToLowerCase( osgDB::convertFileNameToNativeStyle( osgDB::getRealPath( fileName ) ) );
-    std::string tmp_path = osgDB::convertToLowerCase( osgDB::convertFileNameToNativeStyle ( fileName ) );
+    //If it is a URL, it is not relative
+    if (osgDB::containsServerAddress( fileName ) ) return false;
 
-    return (real_path != tmp_path);
+    std::string native = osgDB::convertFileNameToNativeStyle(fileName);
+#if defined(WIN32)  && !defined(__CYGWIN__)
+    //Check to see if the path begins with a drive letter like "C:\data"
+    if (native.size() >= 3 && native[1] == ':' && native[2] == '\\') return false;
+
+    //Check to see if the path is network path like "\\server1\data"
+    if (native.size() >= 2 && native[0] == '\\' && native[1] == '\\') return false;
+
+    //The path must be relative
+    return true;
+#else
+    //Absolute paths in Unix will start with a '/'
+    return !(fileName.size() >= 1 && fileName[0] == '/');
+#endif
 }
 
 std::string osgEarth::getFullPath(const std::string& relativeTo, const std::string &relativePath)
 {
     if (!isRelativePath(relativePath))
     {
+        //osg::notify(osg::NOTICE) << relativePath << " is not a relative path " << std::endl;
         return relativePath;
     }
 
-    std::string fullPath = osgDB::convertFileNameToUnixStyle(osgDB::concatPaths( osgDB::getFilePath( osgDB::getRealPath( relativeTo )), relativePath));
-    fullPath = osgDB::getRealPath(fullPath);    
-    return fullPath;
+    //Note:  Modified from VPB
+
+    //Concatinate the paths together
+    std::string filename = osgDB::concatPaths( osgDB::getFilePath( osgDB::getRealPath( relativeTo )), relativePath);
+
+    std::list<std::string> directories;
+    int start = 0;
+    for (unsigned int i = 0; i < filename.size(); ++i)
+    {
+        if (filename[i] == '\\' || filename[i] == '/')
+        {
+            //Get the current directory
+            std::string dir = filename.substr(start, i-start);
+
+            if (dir != "..")
+            {
+                if (dir != ".")
+                {
+                  directories.push_back(dir);
+                }
+            }
+            else if (!directories.empty())
+            {
+                directories.pop_back();
+            }
+            start = i + 1;
+        }
+    }
+
+    std::string path;
+    for (std::list<std::string>::iterator itr = directories.begin();
+         itr != directories.end();
+         ++itr)
+    {
+        path += *itr;
+        path += "/";
+    }
+
+    path += filename.substr(start, std::string::npos);
+
+    //osg::notify(osg::NOTICE) << "FullPath " << path << std::endl;
+    return path;
 }
 
 bool osgEarth::isZipPath(const std::string &path)

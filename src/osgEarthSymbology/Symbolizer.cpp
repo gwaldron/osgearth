bb0abef26731fd7661259e79b3810a64c4f52eb8
Glenn Waldron <gwaldron@gmail.com>
2010-03-03 15:35:30 +0000

    Added symbology stubs

---
+++ src/osgEarthSymbology/Symbolizer.cpp
@@ -0,0 +1,37 @@
+/* -*-c++-*- */
+/* osgEarth - Dynamic map generation toolkit for OpenSceneGraph
+ * Copyright 2008-2009 Pelican Ventures, Inc.
+ * http://osgearth.org
+ *
+ * osgEarth is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+#include <osgEarthSymbology/Symbolizer>
+
+using namespace osgEarth::Symbology;
+
+Symbolizer::Symbolizer()
+{
+    _root = new osg::Group();
+}
+
+void
+Symbolizer::traverse( osg::NodeVisitor& nv )
+{
+    // TODO:
+    // traverse into the scene graph that was compiled by the
+    // Symbolizer's "compile" method.
+
+    _root->accept( nv );
+}
+

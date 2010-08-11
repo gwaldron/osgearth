41f3e416b4d21db841d3c46b3c646cd7ebd2fde3
Tim Moore <timoore33@gmail.com>
2010-08-11 11:45:20 +0200

    allow map to have a different tile size from the patch set

--- Projected.cpp
+++ Projected.cpp
@@ -23,6 +23,8 @@
 #include <osg/Math>
 
 #include <osgEarth/GeoData>
+#include <osgEarth/HeightFieldUtils>
+#include <osgEarth/ThreadingUtils>
 
 namespace seamless
 {
@@ -42,18 +44,21 @@ public:
     ProjectedOptions(const ProjectedOptions& rhs,
                      const CopyOp& copyop = CopyOp::SHALLOW_COPY)
     {
+
     }
-    void setGeoExtent(const GeoExtent& geoExtent) { _geoExtent = geoExtent; }
-    GeoExtent getGeoExtent() const { return _geoExtent; }
     void setTileKey(TileKey* key) { _tileKey = key; }
     TileKey* getTileKey() const { return _tileKey.get(); }
 protected:
-    GeoExtent _geoExtent;
     ref_ptr<TileKey> _tileKey;
 };
 
 namespace
 {
+// Create a tile key for the map using the normalized patch
+// extents. This might look a little dicey, but there is enough
+// precision in the extents to recover the integer tile key
+// coordinates up to a LOD of 56. That should be sufficient :)
+
 TileKey* makeTileKey(const Projected* ps, const ProjectedOptions* pjoptions)
 {
     Vec2d ll, ur;
@@ -64,12 +69,54 @@ TileKey* makeTileKey(const Projected* ps, const ProjectedOptions* pjoptions)
     return new TileKey(pjoptions->getPatchLevel(), x, y,
                        ps->getMap()->getProfile());
 }
+
+HeightField* resampleHeightField(HeightField* hf, int newDim)
+{
+    const unsigned nCols = hf->getNumColumns();
+    const unsigned nRows = hf->getNumRows();
+    if (nCols == newDim && nRows == newDim)
+        return hf;
+    HeightField* result = new HeightField;
+    result->allocate(newDim, newDim);
+    result->setOrigin(hf->getOrigin());
+    result->setXInterval(hf->getXInterval() * static_cast<float>(nCols)
+                         / static_cast<float>(newDim));
+    result->setYInterval(hf->getYInterval() * static_cast<float>(nRows)
+                         / static_cast<float>(newDim));
+    for (unsigned r = 0; r < newDim; ++r)
+    {
+        for (unsigned c = 0; c < newDim; ++c)
+        {
+            float height
+                = HeightFieldUtils::getHeightAtNormalizedLocation(
+                    hf, static_cast<double>(c) / (newDim - 1),
+                    static_cast<double>(r) / (newDim - 1), INTERP_BILINEAR);
+            result->setHeight(c, r, height);
+        }
+    }
+    return result;
+}
 }
 
+// Hard-wire the patch resolution and screen-space polygon size.
 Projected::Projected(Map* map)
     : PatchSet(64), _map(map)
 {
     setPrecisionFactor(8);
+    const MapLayerList& heightList = _map->getHeightFieldMapLayers();
+    {
+        int maxLevel = 0;
+        Threading::ScopedReadLock lock(_map->getMapDataMutex());
+        for (MapLayerList::const_iterator itr = heightList.begin(),
+                 end = heightList.end();
+             itr != end;
+             ++itr)
+            if ((*itr)->maxLevel().isSet()
+                && (*itr)->maxLevel().get() > maxLevel)
+                maxLevel = (*itr)->maxLevel().get();
+        if (maxLevel > 0)
+            setMaxLevel(maxLevel);
+    }
 }
 
 void Projected::fillPatch(const std::string& filename, Patch* patch,
@@ -82,11 +129,7 @@ void Projected::fillPatch(const std::string& filename, Patch* patch,
                                                       INTERP_BILINEAR);
     ref_ptr<Patch::Data> data = new Patch::Data;
     int patchDim = _resolution + 1;
-    if (patchDim != hf->getNumColumns() || patchDim != hf->getNumColumns())
-    {
-        OSG_FATAL << "patch size " << patchDim << "is not equal to height field dimensions " << hf->getNumColumns() << "x" << hf->getNumRows() << "\n";
-        return;
-    }
+    hf = resampleHeightField(hf, patchDim);
     Vec3Array* verts = new Vec3Array(patchDim * patchDim);
     Vec3Array* normals = new Vec3Array(patchDim * patchDim);
     for (int j = 0; j < patchDim; ++j)

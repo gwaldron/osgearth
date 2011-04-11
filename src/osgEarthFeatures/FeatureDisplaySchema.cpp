0ce7a8475c2f9fa6f488c1e9c6c65374acd72bd7
Glenn W <gwaldron@gmail.com>
2011-04-11 14:38:13 -0400

    Renamed FeatureDisplaySchema (from FeatureLevelSchema)

--- src/osgEarthFeatures/FeatureDisplaySchema.cpp
+++ src/osgEarthFeatures/FeatureDisplaySchema.cpp
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
-#include <osgEarthFeatures/FeatureLevelSchema>
+#include <osgEarthFeatures/FeatureDisplaySchema>
 #include <list>
 #include <deque>
 

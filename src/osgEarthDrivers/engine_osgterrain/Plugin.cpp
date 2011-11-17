ecd072c7503ea2d5026968f689fda68b28e430a7
Glenn W <gwaldron@gmail.com>
2011-11-17 10:14:12 -0500

    Terrain plugin: knocked info message down to debug

--- src/osgEarthDrivers/engine_osgterrain/Plugin.cpp
+++ src/osgEarthDrivers/engine_osgterrain/Plugin.cpp
@@ -114,7 +114,7 @@ public:
                     osg::Timer_t start = osg::Timer::instance()->tick();
                     engineNode->fireTerrainChanged( key, node );
                     osg::Timer_t end = osg::Timer::instance()->tick();
-                    OE_INFO << "Took " << osg::Timer::instance()->delta_m(start, end) << "ms to fire terrain callbacks" << std::endl;
+                    OE_DEBUG << "Took " << osg::Timer::instance()->delta_m(start, end) << "ms to fire terrain callbacks" << std::endl;
                 }
 
                 return ReadResult( node, ReadResult::FILE_LOADED );

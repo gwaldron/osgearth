7330a5ac03af74b167ae99411a3bf653fd8b3262
Jason Beverage <jasonbeverage@pelicanmapping.com>
2012-02-27 12:43:11 -0500

    More work on terrain profile

--- src/applications/osgearth_terrainprofile/osgearth_terrainprofile.cpp
+++ src/applications/osgearth_terrainprofile/osgearth_terrainprofile.cpp
@@ -43,11 +43,13 @@ class TerrainProfile
 public:
     TerrainProfile();
 
+    TerrainProfile(const TerrainProfile& profile);
+
     double getDistance( int i ) const;
     double getTotalDistance() const;
 
     void getElevationRanges(double &min, double &max );
-    
+
     double getElevation( int i ) const;
     unsigned int getNumElevations() const;
 
@@ -67,6 +69,12 @@ private:
 /***************************************************/
 TerrainProfile::TerrainProfile():
 _spacing( 1.0 )
+{
+}
+
+TerrainProfile::TerrainProfile(const TerrainProfile& rhs):
+_spacing( rhs._spacing ),
+_elevations( rhs._elevations )
 {
 }
 
@@ -134,30 +142,147 @@ TerrainProfile::getElevationRanges(double &min, double &max )
 
 /**************************************************/
 
-void computeTerrainProfile( osgEarth::MapNode* mapNode, double startX, double startY, double endX, double endY, unsigned int numSamples, TerrainProfile& profile)
+class TerrainProfileCalculator : public TerrainCallback
 {
-    double startXRad = osg::DegreesToRadians( startX );
-    double startYRad = osg::DegreesToRadians( startY );
-    double endXRad = osg::DegreesToRadians( endX );
-    double endYRad = osg::DegreesToRadians( endY );
+public:
 
-    double distance = osgEarth::GeoMath::distance(startYRad, startXRad, endYRad, endXRad );
-    
-    double spacing = distance / ((double)numSamples - 1.0);
-    
-    profile.setSpacing( spacing );
-    profile.clear();
-    
-    for (unsigned int i = 0; i < numSamples; i++)
+    struct ChangedCallback : public osg::Referenced
     {
-        double t = (double)i / (double)numSamples;
-        double lat, lon;
-        GeoMath::interpolate( startYRad, startXRad, endYRad, endXRad, t, lat, lon );
-        double hamsl;
-        mapNode->getTerrain()->getHeight( osg::RadiansToDegrees(lon), osg::RadiansToDegrees(lat), &hamsl );
-        profile.addElevation( hamsl );
+    public:
+        virtual void onChanged(const TerrainProfileCalculator* sender) {};
+    };
+
+    typedef std::list< osg::ref_ptr<ChangedCallback> > ChangedCallbackList;
+
+
+    TerrainProfileCalculator(MapNode* mapNode, const GeoPoint& start, const GeoPoint& end):
+    _mapNode( mapNode ),
+        _start( start),
+        _end( end ),
+        _numSamples( 100 )
+    {        
+        _mapNode->getTerrain()->addTerrainCallback( this );        
+        recompute();
     }
-}
+
+    ~TerrainProfileCalculator()
+    {
+        _mapNode->getTerrain()->removeTerrainCallback( this );
+    }
+
+    void addChangedCallback( ChangedCallback* callback )
+    {
+        _changedCallbacks.push_back( callback );
+    }
+
+    void removeChangedCallback( ChangedCallback* callback )
+    {
+        ChangedCallbackList::iterator i = std::find( _changedCallbacks.begin(), _changedCallbacks.end(), callback);
+        if (i != _changedCallbacks.end())
+        {
+            _changedCallbacks.erase( i );
+        }    
+    }
+
+    const TerrainProfile& getProfile() const
+    {
+        return _profile;
+    }
+
+    unsigned int getNumSamples() const
+    {
+        return _numSamples;
+    }
+
+    void setNumSamples( unsigned int numSamples)
+    {
+        if (_numSamples != numSamples)
+        {
+            _numSamples = numSamples;
+            recompute();
+        }
+    }
+
+    const GeoPoint& getStart() const
+    {
+        return _start;
+    }
+
+    const GeoPoint& getEnd() const
+    {
+        return _end;
+    }
+
+    void setStartEnd(const GeoPoint& start, const GeoPoint& end)
+    {
+        if (_start != start || _end != end)
+        {
+            _start = start;
+            _end = end;
+            recompute();
+        }
+    }
+
+    virtual void onTileAdded(const osgEarth::TileKey& tileKey, osg::Node* terrain, TerrainCallbackContext&)
+    {
+        GeoExtent extent( _start.getSRS());
+        extent.expandToInclude(_start.x(), _start.y());
+        extent.expandToInclude(_end.x(), _end.y());
+        
+        if (tileKey.getExtent().intersects( extent ))
+        {
+            recompute();
+        }
+    }
+
+    void recompute()
+    {
+        computeTerrainProfile( _mapNode.get(), _start, _end, _numSamples, _profile);
+        for( ChangedCallbackList::iterator i = _changedCallbacks.begin(); i != _changedCallbacks.end(); i++ )
+        {
+            i->get()->onChanged(this);
+        }	
+    }
+
+    static void computeTerrainProfile( osgEarth::MapNode* mapNode, const GeoPoint& start, const GeoPoint& end, unsigned int numSamples, TerrainProfile& profile)
+    {
+        GeoPoint geoStart(start);
+        geoStart.makeGeographic();
+        
+        GeoPoint geoEnd(end);
+        geoEnd.makeGeographic();
+
+        double startXRad = osg::DegreesToRadians( geoStart.x() );
+        double startYRad = osg::DegreesToRadians( geoStart.y() );
+        double endXRad = osg::DegreesToRadians( geoEnd.x() );
+        double endYRad = osg::DegreesToRadians( geoEnd.y() );
+
+        double distance = osgEarth::GeoMath::distance(startYRad, startXRad, endYRad, endXRad );
+
+        double spacing = distance / ((double)numSamples - 1.0);
+
+        profile.setSpacing( spacing );
+        profile.clear();
+
+        for (unsigned int i = 0; i < numSamples; i++)
+        {
+            double t = (double)i / (double)numSamples;
+            double lat, lon;
+            GeoMath::interpolate( startYRad, startXRad, endYRad, endXRad, t, lat, lon );
+            double hamsl;
+            mapNode->getTerrain()->getHeight( osg::RadiansToDegrees(lon), osg::RadiansToDegrees(lat), &hamsl );
+            profile.addElevation( hamsl );
+        }
+    }
+
+private:
+    GeoPoint _start;
+    GeoPoint _end;
+    unsigned int _numSamples;
+    TerrainProfile _profile;
+    osg::ref_ptr< MapNode > _mapNode;
+    ChangedCallbackList _changedCallbacks;
+};
 
 osg::Node* createBackground(double width, double height, const osg::Vec4f& backgroundColor)
 {
@@ -201,136 +326,182 @@ osg::Camera* createHud(double width, double height)
     return hud;
 }
 
-osg::Node* createProfileNode(TerrainProfile& profile, double graphWidth, double graphHeight, const osg::Vec4& color)
-{    
-    osg::Geometry* geom = new osg::Geometry;
-    osg::Vec3Array* verts = new osg::Vec3Array();
-    verts->reserve( profile.getNumElevations() );
-    geom->setVertexArray( verts );
+class TerrainProfileGraph : public osg::Group
+{
+public:
 
-    osg::Vec4Array* colors = new osg::Vec4Array();
-    colors->push_back( color );
-    geom->setColorArray( colors );
-    geom->setColorBinding( osg::Geometry::BIND_OVERALL );
-
-
-    double minElevation, maxElevation;
-    profile.getElevationRanges( minElevation, maxElevation );
-    double elevationRange = maxElevation - minElevation;
-    
-    double totalDistance = profile.getTotalDistance();
-    
-
-    for (unsigned int i = 0; i < profile.getNumElevations(); i++)
+    struct GraphChangedCallback : public TerrainProfileCalculator::ChangedCallback
     {
-        double distance = profile.getDistance( i );
-        double elevation = profile.getElevation( i );
-
-        double x = (distance / totalDistance) * graphWidth;
-        double y = ( (elevation - minElevation) / elevationRange) * graphHeight;
-        verts->push_back( osg::Vec3(x, y, 0 ) );
+        GraphChangedCallback( TerrainProfileGraph* graph):
+    _graph( graph )
+    {
     }
-   
-    geom->addPrimitiveSet( new osg::DrawArrays( GL_LINE_STRIP, 0, verts->size()) );
-    osg::Geode* geode = new osg::Geode;
-    geode->addDrawable( geom );
-    return geode; 
-}
+
+    virtual void onChanged(const TerrainProfileCalculator* sender )
+    {
+        _graph->setTerrainProfile( sender->getProfile() );
+    }
+
+    TerrainProfileGraph* _graph;
+    };
+
+    TerrainProfileGraph( TerrainProfileCalculator* profileCalculator, double graphWidth = 200, double graphHeight = 200 ):
+      _profileCalculator( profileCalculator ),
+      _graphWidth( graphWidth ),
+      _graphHeight( graphHeight ),
+      _color( 1.0f, 1.0f, 0.0f, 1.0f)
+    {
+        _graphChangedCallback = new GraphChangedCallback( this );
+        _profileCalculator->addChangedCallback( _graphChangedCallback.get() );
+    }
+
+      ~TerrainProfileGraph()
+      {
+           _profileCalculator->removeChangedCallback( _graphChangedCallback.get() );
+      }
+
+      void setTerrainProfile( const TerrainProfile& profile)
+      {
+          _profile = profile;
+          redraw();
+      }
+
+      void redraw()
+      {
+          removeChildren( 0, getNumChildren() );
+
+          osg::Geometry* geom = new osg::Geometry;
+          osg::Vec3Array* verts = new osg::Vec3Array();
+          verts->reserve( _profile.getNumElevations() );
+          geom->setVertexArray( verts );
+
+          osg::Vec4Array* colors = new osg::Vec4Array();
+          colors->push_back( _color );
+          geom->setColorArray( colors );
+          geom->setColorBinding( osg::Geometry::BIND_OVERALL );
+
+          double minElevation, maxElevation;
+          _profile.getElevationRanges( minElevation, maxElevation );
+          double elevationRange = maxElevation - minElevation;
+
+          double totalDistance = _profile.getTotalDistance();
+
+
+          for (unsigned int i = 0; i < _profile.getNumElevations(); i++)
+          {
+              double distance = _profile.getDistance( i );
+              double elevation = _profile.getElevation( i );
+
+              double x = (distance / totalDistance) * _graphWidth;
+              double y = ( (elevation - minElevation) / elevationRange) * _graphHeight;
+              verts->push_back( osg::Vec3(x, y, 0 ) );
+          }
+
+          geom->addPrimitiveSet( new osg::DrawArrays( GL_LINE_STRIP, 0, verts->size()) );
+          osg::Geode* geode = new osg::Geode;
+          geode->addDrawable( geom );
+          addChild( geode );
+      }
+
+      osg::Vec4f _color;
+    TerrainProfile _profile;
+    osg::ref_ptr< TerrainProfileCalculator > _profileCalculator;
+    double _graphWidth, _graphHeight;
+    osg::ref_ptr< GraphChangedCallback > _graphChangedCallback;
+};
 
 class TerrainProfileEventHandler : public osgGA::GUIEventHandler
 {
 public:
-    TerrainProfileEventHandler(osgEarth::MapNode* mapNode, osg::Group* root, osg::Group* profileNode, double graphWidth, double graphHeight):
+    TerrainProfileEventHandler(osgEarth::MapNode* mapNode, osg::Group* root, TerrainProfileCalculator* profileCalculator):
       _mapNode( mapNode ),
-      _root( root ),
-      _profileNode( profileNode ),
-      _startValid( false ),
-      _graphWidth( graphWidth ),
-      _graphHeight( graphHeight )
-    {        
-    }
+          _root( root ),
+          _startValid( false ),
+          _profileCalculator( profileCalculator )
+      {
+          _start = profileCalculator->getStart().vec3d();
+          _end = profileCalculator->getEnd().vec3d();
+          compute();
+      }
 
 
-    bool handle( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa )
-    {
-        if (ea.getEventType() == ea.PUSH && ea.getButton() == osgGA::GUIEventAdapter::MIDDLE_MOUSE_BUTTON)
-        {
-            osg::Vec3d world;
-            if ( _mapNode->getTerrain()->getWorldCoordsUnderMouse( aa.asView(), ea.getX(), ea.getY(), world ))
-            {
-                GeoPoint mapPoint;
-                _mapNode->getMap()->worldPointToMapPoint( world, mapPoint );
+      bool handle( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa )
+      {
+          if (ea.getEventType() == ea.PUSH && ea.getButton() == osgGA::GUIEventAdapter::MIDDLE_MOUSE_BUTTON)
+          {
+              osg::Vec3d world;
+              if ( _mapNode->getTerrain()->getWorldCoordsUnderMouse( aa.asView(), ea.getX(), ea.getY(), world ))
+              {
+                  GeoPoint mapPoint;
+                  _mapNode->getMap()->worldPointToMapPoint( world, mapPoint );
 
-                if (!_startValid)
-                {
-                    _startValid = true;
-                    _start = mapPoint.vec3d();
-                    if (_featureNode.valid())
-                    {
-                        _root->removeChild( _featureNode.get() );
-                        _featureNode = 0;
-                    }
-                }
-                else
-                {
-                    _end = mapPoint.vec3d();
-                    compute();
-                    _startValid = false;                    
-                }
-            }        
-        }
-        return false;
-    }
+                  if (!_startValid)
+                  {
+                      _startValid = true;
+                      _start = mapPoint.vec3d();
+                      if (_featureNode.valid())
+                      {
+                          _root->removeChild( _featureNode.get() );
+                          _featureNode = 0;
+                      }
+                  }
+                  else
+                  {
+                      _end = mapPoint.vec3d();
+                      compute();
+                      _startValid = false;                    
+                  }
+              }        
+          }
+          return false;
+      }
 
-    void compute()
-    {
-        computeTerrainProfile( _mapNode, _start.x(), _start.y(), _end.x(), _end.y(), 100, _profile);
-        _profileNode->removeChildren(0, _profileNode->getNumChildren());
-        _profileNode->addChild( createProfileNode( _profile, _graphWidth, _graphHeight, osg::Vec4f(1,0,0,1)) );
+      void compute()
+      {
+          //Tell the calculator about the new start/end points
+          _profileCalculator->setStartEnd( GeoPoint(_mapNode->getMapSRS(), _start.x(), _start.y(), 0),
+                                           GeoPoint(_mapNode->getMapSRS(), _end.x(), _end.y(), 0));
 
-        if (_featureNode.valid())
-        {
-            _root->removeChild( _featureNode.get() );
-            _featureNode = 0;
-        }
+          if (_featureNode.valid())
+          {
+              _root->removeChild( _featureNode.get() );
+              _featureNode = 0;
+          }
 
-        LineString* line = new LineString();
-        line->push_back( _start );
-        line->push_back( _end );
-        Feature* feature = new Feature(line, _mapNode->getMapSRS());
-        feature->geoInterp() = GEOINTERP_GREAT_CIRCLE;    
+          LineString* line = new LineString();
+          line->push_back( _start );
+          line->push_back( _end );
+          Feature* feature = new Feature(line, _mapNode->getMapSRS());
+          feature->geoInterp() = GEOINTERP_GREAT_CIRCLE;    
 
-        //Define a style for the line
-        Style style;
-        LineSymbol* ls = style.getOrCreateSymbol<LineSymbol>();
-        ls->stroke()->color() = Color::Yellow;
-        ls->stroke()->width() = 2.0f;
-        ls->tessellation() = 20;
+          //Define a style for the line
+          Style style;
+          LineSymbol* ls = style.getOrCreateSymbol<LineSymbol>();
+          ls->stroke()->color() = Color::Yellow;
+          ls->stroke()->width() = 2.0f;
+          ls->tessellation() = 20;
 
-        style.getOrCreate<AltitudeSymbol>()->clamping() = AltitudeSymbol::CLAMP_TO_TERRAIN;
+          style.getOrCreate<AltitudeSymbol>()->clamping() = AltitudeSymbol::CLAMP_TO_TERRAIN;
 
-        feature->style() = style;
+          feature->style() = style;
 
-        _featureNode = new FeatureNode( _mapNode, feature );
-         //Disable lighting
-        _featureNode->getOrCreateStateSet()->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
-        _root->addChild( _featureNode.get() );
+          _featureNode = new FeatureNode( _mapNode, feature );
+          //Disable lighting
+          _featureNode->getOrCreateStateSet()->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
+          _root->addChild( _featureNode.get() );
 
-    }
-
-
+      }
 
 
-    osgEarth::MapNode* _mapNode;
-    osg::Group* _profileNode;
-    osg::Group* _root;
-    TerrainProfile _profile;
-    osg::ref_ptr< FeatureNode > _featureNode;
-    bool _startValid;
-    osg::Vec3d _start;
-    osg::Vec3d _end;  
-    double _graphWidth;
-    double _graphHeight;
+
+
+      osgEarth::MapNode* _mapNode;      
+      osg::Group* _root;
+      TerrainProfileCalculator* _profileCalculator;
+      osg::ref_ptr< FeatureNode > _featureNode;
+      bool _startValid;
+      osg::Vec3d _start;
+      osg::Vec3d _end;  
 };
 
 
@@ -361,7 +532,7 @@ main(int argc, char** argv)
 
     osgEarth::Util::EarthManipulator* manip = new EarthManipulator();    
     viewer.setCameraManipulator( manip );
-    
+
     root->addChild( earthNode );    
 
     double backgroundWidth = 500;
@@ -374,16 +545,20 @@ main(int argc, char** argv)
     osg::Camera* hud = createHud( backgroundWidth, backgroundHeight );
     root->addChild( hud );
 
+    osg::ref_ptr< TerrainProfileCalculator > calculator = new TerrainProfileCalculator(mapNode, 
+                                                                                       GeoPoint(mapNode->getMapSRS(), -124.0, 40.0, 0),
+                                                                                       GeoPoint(mapNode->getMapSRS(), -75.1, 39.2, 0)
+                                                                                       );
+    
     hud->addChild( createBackground( graphWidth, graphHeight, osg::Vec4(0,0,0,0.5)) );
 
-    osg::Group* profileNode = new osg::Group;
+    osg::Group* profileNode = new TerrainProfileGraph( calculator.get(), graphWidth, graphHeight );
     hud->addChild( profileNode );
-       
-    
-    
+
+
     viewer.getCamera()->addCullCallback( new AutoClipPlaneCullCallback(mapNode->getMap()) );
 
-    viewer.addEventHandler( new TerrainProfileEventHandler( mapNode, root, profileNode, graphWidth, graphHeight ) );
+    viewer.addEventHandler( new TerrainProfileEventHandler( mapNode, root, calculator ) );
 
 
 

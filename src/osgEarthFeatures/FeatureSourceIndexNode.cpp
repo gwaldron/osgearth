8b1dfcd68e8f4b850d1b83d80a9ae9812f119657
Glenn W <gwaldron@gmail.com>
2012-03-20 12:27:29 -0400

    Add: FEatureSourceIndexNode

---
+++ src/osgEarthFeatures/FeatureSourceIndexNode.cpp
@@ -0,0 +1,184 @@
+/* -*-c++-*- */
+/* osgEarth - Dynamic map generation toolkit for OpenSceneGraph
+ * Copyright 2008-2010 Pelican Mapping
+ * http://osgearth.org
+ *
+ * osgEarth is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+#include <osgEarthFeatures/FeatureSourceIndexNode>
+#include <algorithm>
+
+using namespace osgEarth;
+using namespace osgEarth::Features;
+
+#define LC "[FeatureSourceIndexNode] "
+
+// for testing:
+#undef  OE_DEBUG
+#define OE_DEBUG OE_INFO
+
+//-----------------------------------------------------------------------------
+
+FeatureSourceIndexNode::Collect::Collect( FeatureIDDrawSetMap& index ) :
+osg::NodeVisitor( osg::NodeVisitor::TRAVERSE_ALL_CHILDREN ),
+_index          ( index ),
+_psets          ( 0 )
+{
+    _index.clear();
+}
+
+void
+FeatureSourceIndexNode::Collect::apply( osg::Geode& geode )
+{
+    for( unsigned i = 0; i < geode.getNumDrawables(); ++i )
+    {
+        osg::Geometry* geom = dynamic_cast<osg::Geometry*>( geode.getDrawable(i) );
+        if ( geom )
+        {
+            osg::Geometry::PrimitiveSetList& psets = geom->getPrimitiveSetList();
+            for( unsigned p = 0; p < psets.size(); ++p )
+            {
+                osg::PrimitiveSet* pset = psets[p];
+                RefFeatureID* fid = dynamic_cast<RefFeatureID*>( pset->getUserData() );
+                if ( fid )
+                {
+                    FeatureDrawSet& drawSet = _index[*fid];
+                    drawSet[geom].push_back( pset );
+                    _psets++;
+                }
+            }
+        }
+    }
+    traverse( geode );
+}
+
+//-----------------------------------------------------------------------------
+
+FeatureSourceIndexNode::FeatureSourceIndexNode(FeatureSource* featureSource) : 
+_featureSource( featureSource )
+{
+    //nop
+}
+
+
+// Rebuilds the feature index based on all the tagged primitive sets found in a graph
+void
+FeatureSourceIndexNode::reindex()
+{
+    _drawSets.clear();
+
+    Collect c(_drawSets);
+    this->accept( c );
+
+    OE_DEBUG << LC 
+        << "Reindexed, drawables = " << _drawSets.size()
+        << ", total prim sets = " << c._psets << std::endl;
+}
+
+
+// Tags all the primitive sets in a Drawable with the specified FeatureID
+void
+FeatureSourceIndexNode::tagPrimitiveSets(osg::Drawable* drawable, FeatureID fid)
+{
+    if ( drawable == 0L )
+        return;
+
+    osg::Geometry* geom = drawable->asGeometry();
+    if ( !geom )
+        return;
+
+    RefFeatureID* rfid = 0L;
+
+    PrimitiveSetList& plist = geom->getPrimitiveSetList();
+    for( PrimitiveSetList::iterator p = plist.begin(); p != plist.end(); ++p )
+    {
+        if ( !rfid )
+            rfid = new RefFeatureID(fid);
+
+        p->get()->setUserData( rfid );
+    }
+}
+
+
+bool
+FeatureSourceIndexNode::getFID(osg::PrimitiveSet* primSet, FeatureID& output) const
+{
+    const RefFeatureID* fid = dynamic_cast<const RefFeatureID*>( primSet->getUserData() );
+    if ( fid )
+    {
+        output = *fid;
+        return true;
+    }
+
+    OE_DEBUG << LC << "getFID failed b/c the primSet was not tagged with a RefFeatureID" << std::endl;
+    return false;
+}
+
+
+bool
+FeatureSourceIndexNode::getFID(osg::Drawable* drawable, int primIndex, FeatureID& output) const
+{
+    if ( primIndex < 0 )
+        return false;
+
+    for( FeatureIDDrawSetMap::const_iterator i = _drawSets.begin(); i != _drawSets.end(); ++i )
+    {
+        const FeatureDrawSet& drawSet = i->second;
+        FeatureDrawSet::const_iterator d = drawSet.find(drawable);
+        if ( d != drawSet.end() )
+        {
+            const osg::Geometry* geom = drawable->asGeometry();
+            if ( geom )
+            {
+                const PrimitiveSetList& geomPrimSets = geom->getPrimitiveSetList();
+
+                unsigned encounteredPrims = 0;
+                for( PrimitiveSetList::const_iterator p = geomPrimSets.begin(); p != geomPrimSets.end(); ++p )
+                {
+                    const osg::PrimitiveSet* pset = p->get();
+                    unsigned numPrims = pset->getNumPrimitives();
+                    encounteredPrims += numPrims;
+
+                    if ( encounteredPrims >= (unsigned)primIndex )
+                    {
+                        const RefFeatureID* fid = dynamic_cast<const RefFeatureID*>( pset->getUserData() );
+                        if ( fid )
+                        {
+                            output = *fid;
+                            return true;
+                        }
+                        else
+                        {
+                            OE_WARN << LC << "INTERNAL ERROR: found primset, but it's not tagged with a FID" << std::endl;
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return false;
+}
+
+
+
+FeatureSourceIndexNode::FeatureDrawSet&
+FeatureSourceIndexNode::getDrawSet(const FeatureID& fid )
+{
+    static FeatureDrawSet s_empty;
+
+    FeatureIDDrawSetMap::iterator i = _drawSets.find(fid);
+    return i != _drawSets.end() ? i->second : s_empty;
+}

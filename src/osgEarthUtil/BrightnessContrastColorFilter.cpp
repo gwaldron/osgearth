7bf6906a27c4e604883e9a84814bfa3e8098cecb
Glenn W <gwaldron@gmail.com>
2012-06-11 17:14:24 -0400

    color filters: remove email

--- src/osgEarthUtil/BrightnessContrastColorFilter.cpp
+++ src/osgEarthUtil/BrightnessContrastColorFilter.cpp
@@ -16,7 +16,7 @@
 * You should have received a copy of the GNU Lesser General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>
 *
-* Original author: Thomas Lerman (ThomasL@ImSAR.com)
+* Original author: Thomas Lerman
 */
 #include <osgEarthUtil/BrightnessContrastColorFilter>
 #include <osgEarth/ShaderComposition>

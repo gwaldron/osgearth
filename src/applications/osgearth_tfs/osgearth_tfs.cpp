569e7819202c609cb3bdbbac193befa4a50a8556
Jason Beverage <jasonbeverage@pelicanmapping.com>
2012-06-11 14:17:58 -0400

    Added ability to use the CropFilter to add a feature to multiple output tiles.

--- src/applications/osgearth_tfs/osgearth_tfs.cpp
+++ src/applications/osgearth_tfs/osgearth_tfs.cpp
@@ -23,6 +23,7 @@
 #include <osgDB/FileUtils>
 #include <osgEarth/XmlUtils>
 #include <osgEarthUtil/TFS>
+#include <osgEarthFeatures/CropFilter>
 
 #include <osgEarthDrivers/feature_ogr/OGRFeatureOptions>
 
@@ -62,25 +63,7 @@ public:
     }
 
     bool getIsSplit() const { return _isSplit;}
-
-    bool containsCentroid(Feature* feature)
-    {        
-        Geometry* featureGeom = feature->getGeometry();
-        if ( featureGeom && featureGeom->isValid() )
-        {
-            Bounds bounds = featureGeom->getBounds();
-            if ( bounds.isValid() )
-            {
-                osg::Vec3d centroid = bounds.center();
-                if ( _key.getExtent().contains( centroid.x(), centroid.y() ) )
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
+    
     void split()
     {
         if (!_isSplit)
@@ -137,39 +120,66 @@ void FeatureTile::accept(FeatureTileVisitor* v)
 class AddFeatureVisitor : public FeatureTileVisitor
 {
 public:
-    AddFeatureVisitor( Feature* feature, int maxFeatures, int firstLevel, int maxLevel):
+    AddFeatureVisitor( Feature* feature, int maxFeatures, int firstLevel, int maxLevel, CropFilter::Method cropMethod):
       _feature( feature ),
       _maxFeatures( maxFeatures ),      
       _maxLevel( maxLevel ),
       _firstLevel( firstLevel ),
       _added(false),
-      _levelAdded(-1)
+      _numAdded( 0 ),
+      _levelAdded(-1),
+      _cropMethod( cropMethod )
     {
 
     }
 
     virtual void traverse( FeatureTile* tile)
-    {
-        //If the node contains the feature, and it doesn't contain the max number of features add it.  If it's already full then 
-        //split it.
-        if (tile->containsCentroid( _feature ) )
+    {        
+        if (_added && _cropMethod != CropFilter::METHOD_CROPPING) return;
+
+        GeoExtent featureExtent(_feature->getSRS(), _feature->getGeometry()->getBounds());
+        if (featureExtent.intersects( tile->getExtent()))
         {
+            //If the node contains the feature, and it doesn't contain the max number of features add it.  If it's already full then 
+            //split it.
             if (tile->getKey().getLevelOfDetail() >= _firstLevel && 
-               (tile->getFeatures().size() < _maxFeatures || tile->getKey().getLevelOfDetail() == _maxLevel))
+                (tile->getFeatures().size() < _maxFeatures || tile->getKey().getLevelOfDetail() == _maxLevel || tile->getKey().getLevelOfDetail() == _levelAdded))
             {
-                tile->getFeatures().push_back( _feature.get() );
-                //OE_NOTICE << "Adding feature to tile " << tile->getKey().str() << " which now has " << tile->getFeatures().size() << " features" << std::endl;
-                //TODO:  Go ahead and write the tile to disk if it's full
-                _added = true;
-                _levelAdded = tile->getKey().getLevelOfDetail();
+                if (_levelAdded < 0 || _levelAdded == tile->getKey().getLevelOfDetail())
+                {
+                    osg::ref_ptr< Feature > clone = new Feature( *_feature, osg::CopyOp::DEEP_COPY_ALL );
+                    FeatureList features;
+                    features.push_back( clone );
+
+                    CropFilter cropFilter(_cropMethod);
+                    FilterContext context(0);
+                    context.extent() = tile->getExtent();
+                    cropFilter.push( features, context );
+
+
+                    if (!features.empty() && clone->getGeometry() && clone->getGeometry()->size() > 0)
+                    {
+                        tile->getFeatures().push_back( clone );
+                        _added = true;
+                        _levelAdded = tile->getKey().getLevelOfDetail();
+                        _numAdded++;                   
+                    }
+                }
+
+                if (_cropMethod == CropFilter::METHOD_CROPPING)
+                {
+                    tile->traverse( this );
+                }
             }
             else
             {   
-                //OE_NOTICE << "Splitting" << std::endl;
                 tile->split();
                 tile->traverse( this );
             }
+
         }
+
+
     }
 
     int _levelAdded;
@@ -178,6 +188,9 @@ public:
     int _maxFeatures;
     int _firstLevel;
     int _maxLevel;
+    int _numAdded;
+
+    CropFilter::Method _cropMethod;
 
 
 
@@ -231,15 +244,20 @@ public:
 void buildTFS(FeatureSource* features, int firstLevel, int maxLevel, int maxFeatures, 
                                        const std::string& dest, const std::string& layername,
                                        const std::string& description,
-                                       const Query& query )
+                                       const Query& query,
+                                       CropFilter::Method cropMethod)
 {        
-    const GeoExtent &extent = features->getFeatureProfile()->getExtent();
-    osg::ref_ptr< const osgEarth::Profile > profile = osgEarth::Profile::create(extent.getSRS(), extent.xMin(), extent.yMin(), extent.xMax(), extent.yMax(), 1, 1);
+    osg::ref_ptr< osgEarth::SpatialReference > wgs84 = osgEarth::SpatialReference::create("epsg:4326");
     
+    //Transform to lat/lon extents
+    GeoExtent extent = features->getFeatureProfile()->getExtent().transform( wgs84.get() );
 
-    TileKey rootKey = TileKey(0, 0, 0, profile );
+    osg::ref_ptr< const osgEarth::Profile > profile = osgEarth::Profile::create(extent.getSRS(), extent.xMin(), extent.yMin(), extent.xMax(), extent.yMax(), 1, 1);
+   
 
+    TileKey rootKey = TileKey(0, 0, 0, profile );    
 
+    
     osg::ref_ptr< FeatureTile > root = new FeatureTile( rootKey );
     //Loop through all the features and try to insert them into the quadtree
     osg::ref_ptr< FeatureCursor > cursor = features->createFeatureCursor( query );
@@ -252,10 +270,16 @@ void buildTFS(FeatureSource* features, int firstLevel, int maxLevel, int maxFeat
     {        
         osg::ref_ptr< Feature > feature = cursor->nextFeature();
 
+        //Reproject the feature to WGS84 if it's not already
+        if (!feature->getSRS()->isEquivalentTo( wgs84 ) )
+        {
+            feature->getSRS()->transform(feature->getGeometry()->asVector(), wgs84 );
+        }
+
         if (feature->getGeometry() && feature->getGeometry()->getBounds().valid() && feature->getGeometry()->size() > 0)
         {
 
-            AddFeatureVisitor v(feature.get(), maxFeatures, firstLevel, maxLevel);
+            AddFeatureVisitor v(feature.get(), maxFeatures, firstLevel, maxLevel, cropMethod);
             root->accept( &v );
             if (!v._added)
             {
@@ -263,7 +287,7 @@ void buildTFS(FeatureSource* features, int firstLevel, int maxLevel, int maxFeat
                 failed++;
             }
             else
-            {
+            {                
                 if (highestLevel < v._levelAdded)
                 {
                     highestLevel = v._levelAdded;
@@ -312,6 +336,7 @@ usage( const std::string& msg )
         << "    --description                     ; The abstract/description of the layer" << std::endl
         << "    --expression                      ; The expression to run on the feature source, specific to the feature source" << std::endl
         << "    --orderby                         ; Sort the features, if not already included in the expression" << std::endl
+        << "    --crop                            ; Crops features instead of doing a centroid check.  Features can be added to multiple tiles when cropping is enabled" << std::endl
         << std::endl;
 
     return -1;
@@ -356,6 +381,12 @@ int main(int argc, char** argv)
 
     std::string queryOrderBy = "";
     while (arguments.read("--orderby", queryOrderBy));
+
+    CropFilter::Method cropMethod = CropFilter::METHOD_CENTROID;
+    if (arguments.read("--crop"))
+    {
+        cropMethod = CropFilter::METHOD_CROPPING;
+    }
     
     std::string filename;
 
@@ -392,6 +423,8 @@ int main(int argc, char** argv)
         return 1;
     }
 
+    std::string method = cropMethod == CropFilter::METHOD_CENTROID ? "Centroid" : "Cropping";
+
     OE_NOTICE << "Processing " << filename << std::endl
               << "  FirstLevel=" << firstLevel << std::endl
               << "  MaxLevel=" << maxLevel << std::endl
@@ -401,6 +434,7 @@ int main(int argc, char** argv)
               << "  Description=" << description << std::endl
               << "  Expression=" << queryExpression << std::endl
               << "  OrderBy=" << queryOrderBy << std::endl
+              << "  Method= " << method << std::endl
               << std::endl;
 
 
@@ -416,7 +450,7 @@ int main(int argc, char** argv)
     }
 
     osg::Timer_t startTime = osg::Timer::instance()->tick();
-    buildTFS( features.get(), firstLevel, maxLevel, maxFeatures, destination, layer, description, query);
+    buildTFS( features.get(), firstLevel, maxLevel, maxFeatures, destination, layer, description, query, cropMethod);
     osg::Timer_t endTime = osg::Timer::instance()->tick();
     OE_NOTICE << "Completed in " << osg::Timer::instance()->delta_s( startTime, endTime ) << " s " << std::endl;
 

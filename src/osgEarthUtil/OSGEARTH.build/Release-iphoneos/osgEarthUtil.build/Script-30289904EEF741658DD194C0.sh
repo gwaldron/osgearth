ab77caf11a32867b0fca65302f35de5c4468774a
Thomas Hogarth <thomas.hogarth@gmail.com>
2012-07-27 04:12:38 +0100

    delete

--- src/osgEarthUtil/OSGEARTH.build/Release-iphoneos/osgEarthUtil.build/Script-30289904EEF741658DD194C0.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/hogbox/Documents/AlphaPixel/osgEarthPort/osgearth/src/osgEarthUtil -f /Users/hogbox/Documents/AlphaPixel/osgEarthPort/osgearth/src/osgEarthUtil/CMakeScripts/osgEarthUtil_postBuildPhase.make$CONFIGURATION all

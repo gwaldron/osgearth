44c13232a95ad1e6c1deb023d527ca5aec7c5e09
Glenn W <gwaldron@gmail.com>
2012-10-30 17:24:16 -0400

    Registry: force "quadtree" driver as the default on Android

--- src/osgEarth/Registry.cpp
+++ src/osgEarth/Registry.cpp
@@ -123,6 +123,11 @@ _terrainEngineDriver( "osgterrain" )
     }
 
     // set the default terrain engine driver from the environment
+#ifdef OSG_GLES2_AVAILABLE
+    // default to "quadtree" if we're on iOS/Android/GLES
+    _terrainEngineDriver = "quadtree";
+#endif
+
     const char* teStr = ::getenv("OSGEARTH_TERRAIN_ENGINE");
     if ( teStr )
     {

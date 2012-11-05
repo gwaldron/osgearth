efd10cd7d4cc4748c743c8c5e418388184e36aa3
Jason Beverage <jasonbeverage@pelicanmapping.com>
2012-11-05 16:27:03 -0500

    Moved TMSBackFiller to it's own file.

--- src/applications/osgearth_backfill/osgearth_backfill.cpp
+++ src/applications/osgearth_backfill/osgearth_backfill.cpp
@@ -23,172 +23,20 @@
 #include <osgDB/WriteFile>
 
 #include <osgEarth/Common>
-#include <osgEarth/Map>
 #include <osgEarth/FileUtils>
 #include <osgEarth/ImageUtils>
-#include <osgEarth/MapNode>
 #include <osgEarth/Registry>
 #include <osgEarth/StringUtils>
 #include <osgEarthUtil/TMS>
-#include <osgEarth/ImageMosaic>
+#include <osgEarthUtil/TMSBackFiller>
 
 
 #include <iostream>
 #include <sstream>
-#include <iterator>
 
 using namespace osgEarth;
 using namespace osgEarth::Util;
 using namespace osgEarth::Util::TMS;
-
-#define LC "[osgearth_backfill] "
-
-class TMSBackFiller
-{
-public:
-    TMSBackFiller()
-    {
-    }
-
-    /**
-    * Whether to dump out progress messages 
-    * default = false
-    */
-    void setVerbose( bool value ) { _verbose = value; }
-    bool getVerbose() const { return _verbose; }
-
-    /**
-    * The level to backfill up to
-    */
-    void setMinLevel( unsigned int value ) { _minLevel = value; }
-    unsigned int getMinLevel() const { return _minLevel; }
-
-    /**
-    * The level to start backfilling from.  All tiles up to the min level will be regenerated using tiles from this level of detail.
-    */
-    void setMaxLevel( unsigned int value ) { _maxLevel = value; }
-    unsigned int getMaxLevel() const { return _maxLevel; }
-
-    /**
-     * The bounds to backfill within
-     */
-    const Bounds& getBounds() const { return _bounds;}
-    void setBounds( Bounds& bounds) { _bounds = bounds;}
-
-    void process( const std::string& tms, osgDB::Options* options )
-    {               
-        std::string fullPath = getFullPath( "", tms );        
-        _options = options;
-
-        //Read the tilemap
-        _tileMap = TileMapReaderWriter::read( fullPath, 0 );
-        if (_tileMap)
-        {                        
-            //The max level is where we are going to read data from, so we need to start one level up.
-            osg::ref_ptr< const Profile> profile = _tileMap->createProfile();           
-
-            //If the bounds aren't valid just use the full extent of the profile.
-            if (!_bounds.valid())
-            {                
-                _bounds = profile->getExtent().bounds();
-            }
-
-
-            int firstLevel = _maxLevel-1;            
-            
-            GeoExtent extent( profile->getSRS(), _bounds );           
-
-            //Process each level in it's entirety
-            for (int level = firstLevel; level >= static_cast<int>(_minLevel); level--)
-            {
-                if (_verbose) OE_NOTICE << "Processing level " << level << std::endl;                
-
-                TileKey ll = profile->createTileKey(extent.xMin(), extent.yMin(), level);
-                TileKey ur = profile->createTileKey(extent.xMax(), extent.yMax(), level);
-
-                for (unsigned int x = ll.getTileX(); x <= ur.getTileX(); x++)
-                {
-                    for (unsigned int y = ur.getTileY(); y <= ll.getTileY(); y++)
-                    {
-                        TileKey key = TileKey(level, x, y, profile.get());
-                        processKey( key );
-                    }
-                }                
-
-            }            
-        }
-        else
-        {
-            OE_NOTICE << "Failed to load TileMap from " << _tmsPath << std::endl;
-        }
-    }
-
-    void processKey( const TileKey& key )
-    {
-        if (_verbose) OE_NOTICE << "Processing key " << key.str() << std::endl;
-
-        //Get all of the child tiles for this key, load them and mosaic them into a new tile
-        TileKey ulKey = key.createChildKey( 0 );
-        TileKey urKey = key.createChildKey( 1 );
-        TileKey llKey = key.createChildKey( 2 );
-        TileKey lrKey = key.createChildKey( 3 );
-
-        osg::ref_ptr< osg::Image > ul = readTile( ulKey );
-        osg::ref_ptr< osg::Image > ur = readTile( urKey );
-        osg::ref_ptr< osg::Image > ll = readTile( llKey );
-        osg::ref_ptr< osg::Image > lr = readTile( lrKey );
-
-        if (ul.valid() && ur.valid() && ll.valid() && lr.valid())
-        {            
-            //Merge them together
-            ImageMosaic mosaic;
-            mosaic.getImages().push_back( TileImage( ul.get(), ulKey ) );
-            mosaic.getImages().push_back( TileImage( ur.get(), urKey ) );
-            mosaic.getImages().push_back( TileImage( ll.get(), llKey ) );
-            mosaic.getImages().push_back( TileImage( lr.get(), lrKey ) );            
-
-            osg::ref_ptr< osg::Image> merged = mosaic.createImage();
-            if (merged.valid())
-            {
-                //Resize the image so it's the same size as one of the input files
-                osg::ref_ptr<osg::Image> resized;
-                ImageUtils::resizeImage( merged.get(), ul->s(), ul->t(), resized );
-                std::string outputFilename = getFilename( key );                
-                writeTile( key, resized.get() );
-            }
-        }                
-    }    
-
-private:
-
-    std::string getFilename( const TileKey& key )
-    {
-        return _tileMap->getURL( key, false );        
-    }
-
-    osg::Image* readTile( const TileKey& key )
-    {
-        std::string filename = getFilename( key );        
-        return osgDB::readImageFile( filename );        
-    }
-
-    void writeTile( const TileKey& key, osg::Image* image )
-    {
-        std::string filename = getFilename( key );
-        if ( !osgDB::fileExists( osgDB::getFilePath(filename) ) )
-            osgDB::makeDirectoryForFile( filename );
-        osgDB::writeImageFile( *image, filename, _options.get() );        
-    }    
-
-    osg::ref_ptr< TileMap > _tileMap;
-
-    unsigned int _minLevel;
-    unsigned int _maxLevel;
-    bool _verbose;
-    std::string _tmsPath;
-    Bounds _bounds;
-    osg::ref_ptr< osgDB::Options > _options;
-};
 
 /** Prints an error message, usage information, and returns -1. */
 int
@@ -225,9 +73,6 @@ message( const std::string& msg )
     return 0;
 }
 
-/**
- * Backfills data from a TMS dataset from a given level up.
- */
 int
 main(int argc, char** argv)
 {

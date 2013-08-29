befa845559c0c05bf19bb83e92face2a966ea767
Glenn W <gwaldron@gmail.com>
2013-08-29 13:27:38 -0400

    Fix: ElevationLayerVector::getHeightField was not properly falling back when isKeyValid=false, resulting in zero-elevation tiles appearing when the imagery LOD exceeded the elevation max LOD.

--- src/osgEarth/ElevationLayer.cpp
+++ src/osgEarth/ElevationLayer.cpp
@@ -563,9 +563,13 @@ ElevationLayerVector::createHeightField(const TileKey&                  key,
     {
         ElevationLayer* layer = i->get();
 
-        if ( layer->getEnabled() && layer->getVisible() && layer->isKeyValid( keyToUse ) )
+        if ( layer->getEnabled() && layer->getVisible() )
         {
-            GeoHeightField geoHF = layer->createHeightField( keyToUse, progress );
+            GeoHeightField geoHF;
+            if ( layer->isKeyValid(keyToUse) )
+            {
+                geoHF = layer->createHeightField( keyToUse, progress );
+            }
 
             // if "fallback" is set, try to fall back on lower LODs.
             if ( !geoHF.valid() && fallback )

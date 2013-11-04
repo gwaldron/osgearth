af78f5f32d3057f87ecfd9db0d5ab7bfa4f61192
Jeff Smith <jeffsmith@gmail.com>
2013-11-04 14:56:02 -0500

    Added icu libs to V8 cmake setup.

--- CMakeModules/FindV8.cmake
+++ CMakeModules/FindV8.cmake
@@ -58,9 +58,47 @@ FIND_LIBRARY(V8_SNAPSHOT_LIBRARY
     /usr/freeware/lib64
 )
 
+FIND_LIBRARY(V8_ICUUC_LIBRARY
+    NAMES icuuc libicuuc
+    PATHS
+    ${V8_DIR}
+    ${V8_DIR}/lib
+    ${V8_DIR}/build/Release/lib
+    $ENV{V8_DIR}
+    $ENV{V8_DIR}/lib
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local/lib
+    /usr/lib
+    /sw/lib
+    /opt/local/lib
+    /opt/csw/lib
+    /opt/lib
+    /usr/freeware/lib64
+)
+
+FIND_LIBRARY(V8_ICUI18N_LIBRARY
+    NAMES icui18n libicui18n
+    PATHS
+    ${V8_DIR}
+    ${V8_DIR}/lib
+    ${V8_DIR}/build/Release/lib
+    $ENV{V8_DIR}
+    $ENV{V8_DIR}/lib
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local/lib
+    /usr/lib
+    /sw/lib
+    /opt/local/lib
+    /opt/csw/lib
+    /opt/lib
+    /usr/freeware/lib64
+)
+
 SET(V8_FOUND "NO")
-IF(V8_BASE_LIBRARY AND V8_SNAPSHOT_LIBRARY AND V8_INCLUDE_DIR)
+IF(V8_BASE_LIBRARY AND V8_SNAPSHOT_LIBRARY AND V8_ICUUC_LIBRARY AND V8_ICUI18N_LIBRARY AND V8_INCLUDE_DIR)
     SET(V8_FOUND "YES")
-ENDIF(V8_BASE_LIBRARY AND V8_SNAPSHOT_LIBRARY AND V8_INCLUDE_DIR)
+ENDIF(V8_BASE_LIBRARY AND V8_SNAPSHOT_LIBRARY AND V8_ICUUC_LIBRARY AND V8_ICUI18N_LIBRARY AND V8_INCLUDE_DIR)
 
 

04ca310d8ccbe02cf9801e1dc8df5ae92d823d29
jasonbeverage <jasonbeverage@gmail.com>
2014-04-24 11:01:07 -0400

    Added WriteTMSHandler to TileHandler

--- src/osgEarth/TileHandler.cpp
+++ src/osgEarth/TileHandler.cpp
@@ -1,24 +1,29 @@
 /* -*-c++-*- */
 /* osgEarth - Dynamic map generation toolkit for OpenSceneGraph
- * Copyright 2008-2013 Pelican Mapping
- * http://osgearth.org
- *
- * osgEarth is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
- */
+* Copyright 2008-2013 Pelican Mapping
+* http://osgearth.org
+*
+* osgEarth is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
 #include <osgEarth/TileHandler>
 #include <osgEarth/ImageLayer>
 #include <osgEarth/ElevationLayer>
+#include <osgEarth/ImageToHeightFieldConverter>
+#include <osgEarth/ImageUtils>
+
+#include <osgDB/WriteFile>
+#include <osgDB/FileUtils>
 
 using namespace osgEarth;
 
@@ -58,3 +63,109 @@ bool CacheTileHandler::handleTile( const TileKey& key )
     }
     return false;        
 }   
+
+
+/*****************************************************************************************/
+// A global mutex to make sure that we're not creating directories concurrently.
+// If you don't do this you will get errors when writing from multiple threads
+static OpenThreads::Mutex s_dirLock;
+
+WriteTMSTileHandler::WriteTMSTileHandler(TerrainLayer* layer, const std::string& destination, const std::string& extension):
+_layer( layer ),
+    _destination( destination ),
+    _extension(extension),
+    _width(0),
+    _height(0)      
+{
+}
+
+const std::string& WriteTMSTileHandler::getExtension() const
+{
+    return _extension; 
+}
+
+const std::string& WriteTMSTileHandler::getDestination() const
+{
+    return _destination;
+}
+
+unsigned int WriteTMSTileHandler::getWidth() const
+{
+    return _width;
+}
+
+unsigned int WriteTMSTileHandler::getHeight() const
+{
+    return _height;
+}
+
+TerrainLayer* WriteTMSTileHandler::getLayer()
+{
+    return _layer; 
+}
+
+std::string WriteTMSTileHandler::getPathForTile( const TileKey &key )
+{
+    std::string layerFolder = toLegalFileName( _layer->getName() );         
+    unsigned w, h;
+    key.getProfile()->getNumTiles( key.getLevelOfDetail(), w, h );         
+
+    return Stringify() 
+        << _destination 
+        << "/" << layerFolder
+        << "/" << key.getLevelOfDetail() 
+        << "/" << key.getTileX() 
+        << "/" << h - key.getTileY() - 1
+        << "." << _extension;
+}
+
+
+bool WriteTMSTileHandler::handleTile( const TileKey& key )
+{
+    // Get the path to write to
+    std::string path = getPathForTile( key );
+
+    // attempt to create the output folder:        
+    {
+        OpenThreads::ScopedLock< OpenThreads::Mutex > lk( s_dirLock );
+        osgDB::makeDirectoryForFile( path );       
+    }
+
+    ImageLayer* imageLayer = dynamic_cast< ImageLayer* >( _layer.get() );
+    ElevationLayer* elevationLayer = dynamic_cast< ElevationLayer* >( _layer.get() );
+
+    if (imageLayer)
+    {                
+        GeoImage geoImage = imageLayer->createImage( key );
+
+        if (geoImage.valid())
+        {                
+            if (_width == 0 || _height == 0)
+            {
+                _width = geoImage.getImage()->s();
+                _height = geoImage.getImage()->t();
+            }
+            // OE_NOTICE << "Created image for " << key.str() << std::endl;
+            osg::ref_ptr< const osg::Image > final = geoImage.getImage();
+
+            // convert to RGB if necessary            
+            if ( _extension == "jpg" && final->getPixelFormat() != GL_RGB )
+            {
+                final = ImageUtils::convertToRGB8( final );
+            }                
+            return osgDB::writeImageFile(*final, path);
+        }            
+    }
+    else if (elevationLayer )
+    {
+        GeoHeightField hf = elevationLayer->createHeightField( key );
+        if (hf.valid())
+        {
+            // convert the HF to an image
+            ImageToHeightFieldConverter conv;
+            osg::ref_ptr< osg::Image > image = conv.convert( hf.getHeightField(), 32 );				
+            return osgDB::writeImageFile(*image.get(), path);
+        }            
+    }
+    return false;        
+}  

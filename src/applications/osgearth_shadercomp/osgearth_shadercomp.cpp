c5432bfa71aabcd11ac0296b26000cbfbbc55d71
gwaldron <gwaldron@gmail.com>
2014-06-17 09:40:36 -0400

    VP: added a ShaderComp::AcceptCallback test to osgearth_shadercomp

--- src/applications/osgearth_shadercomp/osgearth_shadercomp.cpp
+++ src/applications/osgearth_shadercomp/osgearth_shadercomp.cpp
@@ -46,10 +46,32 @@ int usage( const std::string& msg )
         << msg << "\n\n"
         << "USAGE: osgearth_shadercomp <earthfile> \n"
         << "           [--test1]    : Run the function injection test \n"
+        << "           [--test2]    : Run the accept callback test \n"
         << "           [--test5]    : Run the Program state set text \n"
         << std::endl;
 
     return -1;
+}
+
+//Utility:
+osg::Geode* makeGeom( float v )
+{
+    osg::Geode* geode = new osg::Geode();
+    osg::Geometry* geom = new osg::Geometry();
+    osg::Vec3Array* verts = new osg::Vec3Array();
+    verts->push_back( osg::Vec3(v-1, 0, 0) );
+    verts->push_back( osg::Vec3(v+1, 0, 0) );
+    verts->push_back( osg::Vec3(  0, 0, 2) );
+    geom->setVertexArray( verts );
+    geom->setUseDisplayList(false);
+    geom->setUseVertexBufferObjects(true);
+    osg::Vec4Array* colors = new osg::Vec4Array();
+    colors->push_back( osg::Vec4(0,0,1,1) );
+    geom->setColorArray(colors);
+    geom->setColorBinding(osg::Geometry::BIND_OVERALL);
+    geom->addPrimitiveSet(new osg::DrawArrays(GL_TRIANGLES,0,3));
+    geode->addDrawable(geom);
+    return geode;
 }
 
 //-------------------------------------------------------------------------
@@ -93,6 +115,42 @@ namespace TEST_1
 
 //-------------------------------------------------------------------------
 
+// Tests the VirtualProgram's ShaderComp::AcceptCallback
+namespace TEST_2
+{
+    const char* fragShader =
+        "#version 110\n"
+        "void make_it_red(inout vec4 color) {\n"
+        "    color.r = 1.0;\n"
+        "}\n";
+
+    struct Acceptor : public ShaderComp::AcceptCallback
+    {
+        // Return true to activate the shader function.
+        bool operator()(const osg::State& state)
+        {
+            osg::Camera* camera = *state.getGraphicsContext()->getCameras().begin();
+            if (!camera) return false;
+            osg::Viewport* viewport = camera->getViewport();
+            if (!viewport) return false;
+            return viewport->width() > 1000;
+        }
+    };
+
+    osg::Group* run(osg::Node* node)
+    {
+        VirtualProgram* vp = VirtualProgram::getOrCreate(node->getOrCreateStateSet());
+        vp->setFunction("make_it_red", fragShader, ShaderComp::LOCATION_FRAGMENT_LIGHTING, new Acceptor());
+        
+        osg::Group* g = new osg::Group();
+        g->addChild( node );
+        return g;
+    }
+
+}
+
+//-------------------------------------------------------------------------
+
 namespace TEST_5
 {
     char s_vert[] =
@@ -109,41 +167,14 @@ namespace TEST_5
         "#version " GLSL_VERSION_STR "\n"
         "void test( inout vec4 color ) { color = vec4(1.0,0.0,0.0,1.0); } \n";
 
-    osg::Geode* makeGeom( float v )
-    {
-        osg::Geode* geode = new osg::Geode();
-        osg::Geometry* geom = new osg::Geometry();
-        osg::Vec3Array* verts = new osg::Vec3Array();
-        verts->push_back( osg::Vec3(v-1, 0, 0) );
-        verts->push_back( osg::Vec3(v+1, 0, 0) );
-        verts->push_back( osg::Vec3(  0, 0, 2) );
-        geom->setVertexArray( verts );
-        geom->setUseDisplayList(false);
-        geom->setUseVertexBufferObjects(true);
-        osg::Vec4Array* colors = new osg::Vec4Array();
-        colors->push_back( osg::Vec4(0,0,1,1) );
-        geom->setColorArray(colors);
-        geom->setColorBinding(osg::Geometry::BIND_OVERALL);
-        geom->addPrimitiveSet(new osg::DrawArrays(GL_TRIANGLES,0,3));
-        geode->addDrawable(geom);
-        return geode;
-    }
-
     osg::Group* run()
     {
         osg::Node* n1 = makeGeom( -5 );
         osg::Node* n2 = makeGeom(  5 );
 
-#if 0
-        osg::Program* p = new osg::Program();
-        p->addShader( new osg::Shader(osg::Shader::VERTEX,   s_vert) );
-        p->addShader( new osg::Shader(osg::Shader::FRAGMENT, s_frag) );
-#else
-        VirtualProgram* p = new VirtualProgram();
-        p->setFunction("test", s_vp, ShaderComp::LOCATION_FRAGMENT_LIGHTING);
-#endif
-
-        n1->getOrCreateStateSet()->setAttributeAndModes( p, 1 );
+        VirtualProgram* vp = new VirtualProgram();
+        vp->setFunction("test", s_vp, ShaderComp::LOCATION_FRAGMENT_LIGHTING);
+        n1->getOrCreateStateSet()->setAttributeAndModes( vp, 1 );
 
         osg::Group* root = new osg::Group();
         root->getOrCreateStateSet()->setRenderBinDetails( 0, "TraversalOrderBin" );
@@ -165,13 +196,17 @@ int main(int argc, char** argv)
     osgViewer::Viewer viewer(arguments);
 
     bool test1 = arguments.read("--test1");
+    bool test2 = arguments.read("--test2");
     bool test5 = arguments.read("--test5");
-    bool ok    = test1 || test5; 
+    bool ok    = test1 || test2 || test5; 
 
     if ( !ok )
     {
         return usage("");
     }
+
+    LabelControl* label = new LabelControl();
+    ControlCanvas::get(&viewer,true)->addControl(label);
 
     if ( !test5 )
     {
@@ -183,19 +218,21 @@ int main(int argc, char** argv)
 
         viewer.setCameraManipulator( new osgEarth::Util::EarthManipulator() );
 
-        LabelControl* label = new LabelControl();
-        if ( !test5 )
-            ControlCanvas::get(&viewer,true)->addControl(label);
-
         if ( test1 )
         {
             viewer.setSceneData( TEST_1::run(earthNode) );
-            label->setText( "Function injection test: the map should appear hazy." );
+            label->setText( "Function injection test: the map appears hazy at high altitude." );
+        }
+        else if ( test2 )
+        {
+            viewer.setSceneData( TEST_2::run(earthNode) );
+            label->setText( "Accept callback test: the map turns red when viewport width > 1000" );
         }
     }
     else // if ( test5 )
     {
         viewer.setSceneData( TEST_5::run() );
+        label->setText( "Leakage test: red tri on the left, blue on the right." );
     }
 
     // add some stock OSG handlers:

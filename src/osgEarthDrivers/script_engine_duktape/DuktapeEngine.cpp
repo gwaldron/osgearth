4fdf237741bbfaf9280c7125d6c2607fed081ebc
gwaldron <gwaldron@gmail.com>
2014-06-21 15:52:08 -0400

    Duktape: optimized multi-context setup and feature update.

--- src/osgEarthDrivers/script_engine_duktape/DuktapeEngine.cpp
+++ src/osgEarthDrivers/script_engine_duktape/DuktapeEngine.cpp
@@ -25,7 +25,7 @@
 // defining this will setup and tear down a complete duktape heap/context
 // for each and every invocation. Good for testing memory usage until we
 // complete the feature set.
-#define MAXIMUM_ISOLATION
+//#define MAXIMUM_ISOLATION
 
 using namespace osgEarth;
 using namespace osgEarth::Features;
@@ -35,23 +35,12 @@ using namespace osgEarth::Drivers::Duktape;
 
 namespace
 {
-    std::string jsonEncode(duk_context* ctx, const char* str)
+    // Updates the global feature object with new attributes.
+    void updateFeature(duk_context* ctx, Feature const* feature)
     {
-        duk_push_string(ctx, str);
-        std::string result = duk_json_encode(ctx, -1);
-        duk_pop(ctx);
-        return result;
-    }
-
-    // Creates the feature object for ECMA5.
-    // TODO: for ECMA6 we might use a Proxy object and C callback? Maybe,
-    // but this is pretty quick.
-    // TODO: this will cause a Syntax Error for a property key or value that
-    // has an invalid encoding -- need to fix that.
-    std::string createFeatureScaffoldEcma5(duk_context* ctx, Feature const* feature)
-    {
-        // push a new object:
-        duk_push_object(ctx);
+        duk_push_global_object(ctx);        // [ global ]
+        duk_push_string(ctx, "feature");    // [ global, "feature" ]
+        duk_get_prop(ctx, -2);              // [ global, feature ]
 
         // add each property to the object:
         const AttributeTable& attrs = feature->getAttrs();
@@ -59,45 +48,13 @@ namespace
         {
             if ( !a->first.empty() )
             {
-                duk_push_string(ctx, a->second.getString().c_str());
-                duk_put_prop_string(ctx, -2, a->first.c_str());
+                duk_push_string(ctx, a->second.getString().c_str()); // [ global, feature, name ]
+                duk_put_prop_string(ctx, -2, a->first.c_str());      // [ global, feature ]
             }
         }
 
-        std::stringstream buf;
-        
-        buf << "var feature = "
-            << duk_json_encode(ctx, -1) << ";";
-
-        // pop the encoding result and the object.
-        duk_pop_2(ctx);
-
-        // This adds a meta-property "attributes" that allows you to use
-        // the idiom: feature.attributes[prop]
-        buf << "Object.defineProperty(feature, 'attributes', {get:function() {return feature;}});";
-
-        std::string str = buf.str();
-        return str;
+        duk_pop_2(ctx); // []
     }
-
-#if 0
-    // not used at the moment, but let's keep this here as a reference
-    // example for C callback usage:
-    extern "C"
-    {
-        /**
-         * C binding for "get_feature_attr(feature, attrName)" JS method
-         */
-        int oeduk_get_feature_attr(duk_context* ctx)
-        {
-            Feature*    feature = reinterpret_cast<Feature*>(duk_require_pointer(ctx, 0));
-            const char* key     = duk_require_string(ctx, 1);
-            std::string value = feature->getString(key);
-            duk_push_string(ctx, value.c_str());
-            return 1;
-        }
-    }
-#endif
 }
 
 //............................................................................
@@ -112,27 +69,32 @@ DuktapeEngine::Context::initialize(const ScriptEngineOptions& options)
 {
     if ( _ctx == 0L )
     {
-        // new allocation heap.
+        // new heap + context.
         _ctx = duk_create_heap_default();
 
-        // if there is a static script, evaluate it first
+        // if there is a static script, evaluate it first. This will register
+        // any functions or objects with the EcmaScript global object.
         if ( options.script().isSet() )
         {
-            bool ok = (duk_peval_string(_ctx, options.script()->getCode().c_str()) == 0);
+            bool ok = (duk_peval_string(_ctx, options.script()->getCode().c_str()) == 0); // [ "result" ]
             if ( !ok )
             {
                 const char* err = duk_safe_to_string(_ctx, -1);
                 OE_WARN << LC << err << std::endl;
             }
-            duk_pop(_ctx);
+            duk_pop(_ctx); // []
         }
 
-        // new value stack:
-        duk_push_global_object( _ctx );
+        // Create the global feature object.
+        {
+            duk_push_global_object(_ctx);             // [ global ]         feature object's home
+            duk_push_object(_ctx);                    // [ global feature ] empty object for starters
+            duk_put_prop_string(_ctx, -2, "feature"); // [ global ]         name it and add it to the global
+            duk_pop(_ctx);                            // []                 pop the global
 
-        //// install C bindings.
-        //duk_push_c_function( _ctx, oeduk_get_feature_attr, 2/*numargs*/);
-        //duk_put_prop_string( _ctx, -2, "c_get_feature_attr" );
+            // support for the idiom: feature.attributes['attr']
+            duk_eval_string_noresult(_ctx, "Object.defineProperty(feature, 'attributes', {get:function() {return feature;}});");
+        }
     }
 }
 
@@ -179,36 +141,25 @@ DuktapeEngine::run(const std::string&   code,
     duk_context* ctx = c._ctx;
 #endif
 
-    std::string finalCode = code;
+    // encode the feature in the global object:
+    updateFeature(ctx, feature);
 
-    if (feature)
-    {
-        // set a global object "feature" = native pointer
-        //duk_push_pointer(ctx, (void*)feature);
-        //duk_put_prop_string(ctx, -2, "c_feature");
-        
-        finalCode = createFeatureScaffoldEcma5(ctx, feature) + "\n" + finalCode;
-    }
-
-    std::string resultString;
 
     // run the script. On error, the top of stack will hold the error
     // message instead of the return value.
-    bool ok = (duk_peval_string(ctx, finalCode.c_str()) == 0);
+    std::string resultString;
+    bool ok = (duk_peval_string(ctx, code.c_str()) == 0); // [ "result" ]
     const char* resultVal = duk_to_string(ctx, -1);
     if ( resultVal )
         resultString = resultVal;
 
     if ( !ok )
     {
-        OE_WARN << LC << "Error: source =\n" << finalCode << std::endl;
+        OE_WARN << LC << "Error: source =\n" << code << std::endl;
     }
 
     // pop the return value:
-    duk_pop(ctx);
-
-    // force a garbage collection to keep memory in check
-    duk_gc(ctx, 0);
+    duk_pop(ctx); // []
 
     return ok ?
         ScriptResult(resultString, true) :

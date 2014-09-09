9577b4b4daf2738a859312e0c289586357ec47d2
Jeff Smith <jeffsmith@gmail.com>
2014-09-09 16:37:23 -0400

    Improved hole handling in the tessellator.

--- src/osgEarthFeatures/BuildGeometryFilter.cpp
+++ src/osgEarthFeatures/BuildGeometryFilter.cpp
@@ -64,6 +64,31 @@ namespace
     {
         return isCCW(x1, y1, x3, y3, x4, y4) != isCCW(x2, y2, x3, y3, x4, y4) && isCCW(x1, y1, x2, y2, x3, y3) != isCCW(x1, y1, x2, y2, x4, y4);
     }
+
+    bool holeCompare(osgEarth::Symbology::Ring* i, osgEarth::Symbology::Ring* j)
+    {
+        return i->getBounds().xMax() > j->getBounds().xMax();
+    }
+
+    bool segmentsIntersect(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4, double &xi, double &yi)
+    {
+        double d = (y4-y3) * (x2-x1) - (x4-x3) * (y2-y1);
+
+        if (d == 0) return false; // parallel
+
+        double ua = ((x4-x3) * (y1-y3) - (y4-y3) * (x1-x3)) / d;
+        double ub = ((x2-x1) * (y1-y3) - (y2-y1) * (x1-x3)) / d;
+
+        if (ua >= 0.0 && ua <= 1.0 && ub >= 0.0 && ub <= 1.0)
+        {
+            xi = x1 + ua * (x2 - x1);
+            yi = y1 + ua * (y2 - y1);
+
+            return true;
+        }
+
+        return false;
+    }
 }
 
 BuildGeometryFilter::BuildGeometryFilter( const Style& style ) :
@@ -492,7 +517,8 @@ BuildGeometryFilter::buildPolygon(Geometry*               ring,
     Polygon* poly = dynamic_cast<Polygon*>(ring);
     if ( poly )
     {
-        int offset = ring->size();
+        RingCollection ordered(poly->getHoles().begin(), poly->getHoles().end());
+        std::sort(ordered.begin(), ordered.end(), holeCompare);
 
         for( RingCollection::const_iterator h = poly->getHoles().begin(); h != poly->getHoles().end(); ++h )
         {
@@ -505,79 +531,123 @@ BuildGeometryFilter::buildPolygon(Geometry*               ring,
                 transformAndLocalize( hole->asVector(), featureSRS, holePoints.get(), mapSRS, world2local, makeECEF );
 
                 // find the point with the highest x value
-                int hCursor = 0;
-                for (int i=1; i < holePoints->size(); i++)
+                unsigned int hCursor = 0;
+                for (unsigned int i=1; i < holePoints->size(); i++)
                 {
                     if ((*holePoints)[i].x() > (*holePoints)[hCursor].x())
                       hCursor = i;
                 }
 
-                int pCursor = 0;
-                for (int i=0; i < allPoints->size(); i++)
+                double x1 = (*holePoints)[hCursor].x();
+                double y1 = (*holePoints)[hCursor].y();
+                double y2 = (*holePoints)[hCursor].y();
+
+                unsigned int edgeCursor = UINT_MAX;
+                double edgeDistance = DBL_MAX;
+                unsigned int foundPointCursor = UINT_MAX;
+                for (unsigned int i=0; i < allPoints->size(); i++)
                 {
-                    if ((*allPoints)[i].x() > (*holePoints)[hCursor].x())
+                    unsigned int next = i == allPoints->size() - 1 ? 0 : i + 1;
+                    double xMax = osg::maximum((*allPoints)[i].x(), (*allPoints)[next].x());
+
+                    if (xMax > (*holePoints)[hCursor].x())
                     {
+                        double x2 = xMax + 1.0;
+                        double x3 = (*allPoints)[i].x();
+                        double y3 = (*allPoints)[i].y();
+                        double x4 = (*allPoints)[next].x();
+                        double y4 = (*allPoints)[next].y();
+
+                        double xi=0.0, yi=0.0;
                         bool intersects = false;
-                        for (int j=0; j < allPoints->size(); j++)
+                        unsigned int hitPointCursor = UINT_MAX;
+                        if (y1 == y3 && x3 > x1)
                         {
-                            if (j == i)
-                              continue;
-
-                            if ((*allPoints)[j].x() > (*holePoints)[hCursor].x())
-                            {
-                                // check if the two line segments intersect
-                                int next = j == allPoints->size() - 1 ? 0 : j + 1;
-
-                                double x1 = (*holePoints)[hCursor].x();
-                                double y1 = (*holePoints)[hCursor].y();
-                                double x2 = (*allPoints)[i].x();
-                                double y2 = (*allPoints)[i].y();
-                                double x3 = (*allPoints)[j].x();
-                                double y3 = (*allPoints)[j].y();
-                                double x4 = (*allPoints)[next].x();
-                                double y4 = (*allPoints)[next].y();
-
-                                if (segmentsIntersect(x1, y1, x2, y2, x3, y3, x4, y4))
-                                {
-                                    intersects = true;
-                                    break;
-                                }
-                            }
+                            xi = x3;
+                            hitPointCursor = i;
+                            intersects = true;
+                        }
+                        else if (y1 == y4 && x4 > x1)
+                        {
+                            xi = x4;
+                            hitPointCursor = next;
+                            intersects = true;
+                        }
+                        else if (segmentsIntersect(x1, y1, x2, y2, x3, y3, x4, y4, xi, yi))
+                        {
+                            intersects = true;
                         }
 
-                        if (!intersects)
+                        double dist = (osg::Vec2d(xi, yi) - osg::Vec2d(x1, y1)).length();
+                        if (intersects && dist < edgeDistance)
                         {
-                          //TODO: instead of going with the first point that works, could be smarter
-                          //      and find the closest point that works to minimize long triangles.
-                          //      This would be slower though, so it's a trade-off to consider.
-
-
-                          // (*allPoints)[i] is a mutually visible point, connect the hole to the outer
-                          //  polygon with two coincident lines to join them
-
-                          int location = i;
-                          int p = hCursor;
-                          do
-                          {
-                              location = location >= allPoints->size() - 1 ? 0 : location + 1;
-                              allPoints->insert(allPoints->begin() + location, (*holePoints)[p]);
-
-                              //p = p == 0 ? holePoints->size() - 1 : p - 1;
-                              p = p == holePoints->size() - 1 ? 0 : p + 1;
-
-                          } while(p != hCursor);
-
-                          location = location >= allPoints->size() - 1 ? 0 : location + 1;
-                          allPoints->insert(allPoints->begin() + location, (*holePoints)[hCursor]);
-
-                          location = location >= allPoints->size() - 1 ? 0 : location + 1;
-                          allPoints->insert(allPoints->begin() + location, (*allPoints)[i]);
-
-                          break;
+                            foundPointCursor = hitPointCursor;
+                            edgeCursor = hitPointCursor != UINT_MAX ? hitPointCursor : (x3 >= x4 ? i : next);
+                            edgeDistance = dist;
                         }
                     }
                 }
-            }            
+
+                if (foundPointCursor == UINT_MAX && edgeCursor != UINT_MAX)
+                {
+                    // test for intersecting edges between x1 and x2
+                    // (skipping the two segments for which edgeCursor is a vert)
+
+                    double x2 = (*allPoints)[edgeCursor].x();
+                    y2 = (*allPoints)[edgeCursor].y();
+
+                    bool foundIntersection = false;
+                    for (unsigned int i=0; i < allPoints->size(); i++)
+                    {
+                        unsigned int next = i == allPoints->size() - 1 ? 0 : i + 1;
+
+                        if (i == edgeCursor || next == edgeCursor)
+                          continue;
+
+                        double x3 = (*allPoints)[i].x();
+                        double y3 = (*allPoints)[i].y();
+                        double x4 = (*allPoints)[next].x();
+                        double y4 = (*allPoints)[next].y();
+
+                        foundIntersection = foundIntersection || segmentsIntersect(x1, y1, x2, y2, x3, y3, x4, y4);
+
+                        if (foundIntersection)
+                        {
+                            unsigned int prev = i == 0 ? allPoints->size() - 1 : i - 1;
+
+                            if (!isCCW((*allPoints)[prev].x(), (*allPoints)[prev].y(), x3, y3, x4, y4))
+                            {
+                                edgeCursor = i;
+                                x2 = (*allPoints)[edgeCursor].x();
+                                y2 = (*allPoints)[edgeCursor].y();
+                                foundIntersection = false;
+                            }
+                        }
+
+                    }
+                }
+
+                if (edgeCursor != UINT_MAX)
+                {
+                    // build array of correctly ordered new points to add to the outer loop
+                    osg::ref_ptr<osg::Vec3Array> insertPoints = new osg::Vec3Array();
+                    insertPoints->reserve(holePoints->size() + 2);
+
+                    unsigned int p = hCursor;
+                    do
+                    {
+                        insertPoints->push_back((*holePoints)[p]);
+                        p = p == holePoints->size() - 1 ? 0 : p + 1;
+                    } while(p != hCursor);
+
+                    insertPoints->push_back((*holePoints)[hCursor]);
+                    insertPoints->push_back((*allPoints)[edgeCursor]);
+                    
+                    // insert new points into outer loop
+                    osg::Vec3Array::iterator it = edgeCursor == allPoints->size() - 1 ? allPoints->end() : allPoints->begin() + (edgeCursor + 1);
+                    allPoints->insert(it, insertPoints->begin(), insertPoints->end());
+                }
+            }
         }
     }
 
@@ -592,7 +662,7 @@ BuildGeometryFilter::buildPolygon(Geometry*               ring,
         if (!oeTess.tessellateGeometry(*osgGeom))
         {
             //fallback to osg tessellator
-            OE_WARN << LC << "OE Tessellation failed! Using OSG tessellator." << std::endl;
+            OE_WARN << LC << "OE Tessellation failed! Using OSG tessellator. (" << osgGeom->getName() << ")" << std::endl;
 
             osgUtil::Tessellator tess;
             tess.setTessellationType( osgUtil::Tessellator::TESS_TYPE_GEOMETRY );

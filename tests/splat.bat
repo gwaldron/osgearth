9572e7fe21c333423efaecabd0c51df534d973ba
gwaldron <gwaldron@gmail.com>
2014-11-04 14:23:24 -0500

    merge

---
+++ tests/splat.bat
@@ -0,0 +1,12 @@
+osgearth_viewer splat-test.earth ^
+	--ico ^
+	--logdepth ^
+	--sky ^
+	--uniform oe_splat_max_range 200000 0 ^
+	--uniform oe_splat_warp 0 0.01 ^
+	--uniform oe_splat_blur 1 4 ^
+	--uniform oe_splat_scaleOffset 0 7 ^
+	--uniform oe_nmap_intensity 0 2.0 ^
+	--uniform oe_nmap_scale 1 20 ^
+	%*
+	

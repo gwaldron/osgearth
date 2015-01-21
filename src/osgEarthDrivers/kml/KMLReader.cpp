fde5848b1c97dfdfd35218f3af7581a5dc95fa8c
Jason Beverage <jasonbeverage@gmail.com>
2015-01-21 13:51:58 -0500

    Changed KML driver to use the geographic srs of the map instead of egm96.

--- src/osgEarthDrivers/kml/KMLReader.cpp
+++ src/osgEarthDrivers/kml/KMLReader.cpp
@@ -78,7 +78,9 @@ KMLReader::read( xml_document<>& doc, const osgDB::Options* dbOptions )
     cx._mapNode   = _mapNode;
     cx._sheet     = new StyleSheet();
     cx._options   = _options;
-    cx._srs       = SpatialReference::create( "wgs84", "egm96" );
+    //cx._srs      = SpatialReference::create( "wgs84", "egm96" );
+    // Use the geographic srs of the map so that clamping will occur against the correct vertical datum.
+    cx._srs = _mapNode->getMapSRS()->getGeographicSRS();
     cx._referrer = context.referrer();
     cx._groupStack.push( root );
 

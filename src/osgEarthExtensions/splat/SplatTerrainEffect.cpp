4b20379d20c557db058755ad5468c80d110d70e9
gwaldron <gwaldron@gmail.com>
2015-01-21 15:08:47 -0500

    Splat: fixed bad uniform name

--- src/osgEarthExtensions/splat/SplatTerrainEffect.cpp
+++ src/osgEarthExtensions/splat/SplatTerrainEffect.cpp
@@ -95,7 +95,7 @@ _gpuNoise   ( false )
 
     SplatOptions def;
 
-    _scaleOffsetUniform    = new osg::Uniform("oe_splat_scaleOffset",      *def.scaleLevelOffset());
+    _scaleOffsetUniform    = new osg::Uniform("oe_splat_scaleOffsetInt",   *def.scaleLevelOffset());
     _warpUniform           = new osg::Uniform("oe_splat_warp",             *def.coverageWarp());
     _blurUniform           = new osg::Uniform("oe_splat_blur",             *def.coverageBlur());
     _useBilinearUniform    = new osg::Uniform("oe_splat_useBilinear",      (def.bilinearSampling()==true?1.0f:0.0f));

87a9fff9e99055b54813960136a0c2247e1909d0
gwaldron <gwaldron@gmail.com>
2015-02-07 15:50:34 -0500

    add missing glsl files

---
+++ src/osgEarthUtil/ContourMap.frag.glsl
@@ -0,0 +1,15 @@
+#version $GLSL_VERSION_STR
+$GLSL_DEFAULT_PRECISION_FLOAT
+
+#pragma vp_entryPoint "oe_contour_fragment"
+#pragma vp_location   "fragment_coloring"
+
+uniform sampler1D oe_contour_xfer;
+uniform float oe_contour_opacity;
+varying float oe_contour_lookup;
+
+void oe_contour_fragment( inout vec4 color )
+{
+    vec4 texel = texture1D( oe_contour_xfer, oe_contour_lookup );
+    color.rgb = mix(color.rgb, texel.rgb, texel.a * oe_contour_opacity);
+}

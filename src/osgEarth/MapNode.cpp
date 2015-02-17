165fa45faeeaf0a2fb1a3e588317baa67f0ad77a
gwaldron <gwaldron@gmail.com>
2015-02-17 15:13:34 -0500

    Fix compile issue

--- src/osgEarth/MapNode.cpp
+++ src/osgEarth/MapNode.cpp
@@ -534,7 +534,8 @@ MapNode::onModelLayerAdded( ModelLayer* layer, unsigned int index )
     }
 
     // create the scene graph:
-    osg::Node* node = layer->getOrCreateSceneGraph( _map.get(), _map->getDBOptions(), 0L );
+    //osg::Node* node = layer->getOrCreateSceneGraph( _map.get(), _map->getDBOptions(), 0L );
+    osg::Node* node = layer->getOrCreateSceneGraph( _map.get(), 0L );
 
     if ( node )
     {

a5d77b223a73dd83be7e31da2131a520698f5139
Jason Beverage <jasonbeverage@gmail.com>
2015-05-28 14:45:50 -0400

    Reworked osgEarthDrivers to load all the plugins in the current directory instead of having to specify each one in the CMake.

--- CMakeModules/OsgEarthMacroUtils.cmake
+++ CMakeModules/OsgEarthMacroUtils.cmake
@@ -513,3 +513,15 @@ macro(configure_shaders templateFile autoGenCppFile)
 			"${CMAKE_SOURCE_DIR}/CMakeModules/ConfigureShaders.cmake.in" )
 	
 endmacro(configure_shaders)
+
+# http://stackoverflow.com/questions/7787823/cmake-how-to-get-the-name-of-all-subdirectories-of-a-directory
+MACRO(SUBDIRLIST result curdir)
+  FILE(GLOB children RELATIVE ${curdir} ${curdir}/*)
+  SET(dirlist "")
+  FOREACH(child ${children})
+    IF(IS_DIRECTORY ${curdir}/${child})
+        LIST(APPEND dirlist ${child})
+    ENDIF()
+  ENDFOREACH()
+  SET(${result} ${dirlist})
+ENDMACRO()

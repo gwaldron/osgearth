3e04e1fa24e76db7d9cab3a7739a130b29751f14
gwaldron <gwaldron@gmail.com>
2015-09-16 07:15:30 -0400

    Remove missing include

--- src/osgEarthDrivers/engine_rex/EngineContext.cpp
+++ src/osgEarthDrivers/engine_rex/EngineContext.cpp
@@ -17,7 +17,6 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 #include "EngineContext"
-#include "SurfaceNodeFactory"
 
 using namespace osgEarth::Drivers::RexTerrainEngine;
 using namespace osgEarth;

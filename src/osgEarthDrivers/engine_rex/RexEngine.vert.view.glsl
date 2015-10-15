4def5483305e0dda2374aeaf2a12821259bc200a
gwaldron <gwaldron@gmail.com>
2015-10-15 16:01:03 -0400

    GPU speedup by using scale/bias vs full matrix multiply

--- src/osgEarthDrivers/engine_rex/RexEngine.vert.view.glsl
+++ src/osgEarthDrivers/engine_rex/RexEngine.vert.view.glsl
@@ -17,14 +17,26 @@ uniform mat4 oe_layer_texParentMatrix;
 // SDK functions:
 float oe_terrain_getElevation(in vec2 uv);
 
+// marker that indicates the vertex belongs to a masking geometry
+// and has a hard-coded height value
+#define MASK_MARKER 2.0
+
 
 void oe_rex_elevateVertexAndSetTexCoords(inout vec4 vertexView)
 {
-    float elev = oe_layer_tilec.z == 2.0 ? 0.0f : oe_terrain_getElevation( oe_layer_tilec.st );
+    float elev = oe_layer_tilec.z == MASK_MARKER ? 0.0f : oe_terrain_getElevation( oe_layer_tilec.st );
 
-    vertexView.xyz += normalize(oe_UpVectorView) * elev;
+    vertexView.xyz += oe_UpVectorView * elev;
 
+#if 0
     // calculate the texture coordinates:
-	oe_layer_texc		= oe_layer_texMatrix       * oe_layer_tilec;
+    oe_layer_texc       = oe_layer_texMatrix       * oe_layer_tilec;
 	oe_layer_texcParent = oe_layer_texParentMatrix * oe_layer_tilec;
+#else
+    // faster (MAD)
+	oe_layer_texc.xy	   = oe_layer_tilec.xy*oe_layer_texMatrix[0][0] + oe_layer_texMatrix[3].xy;
+    oe_layer_texc.zw       = oe_layer_tilec.zw;
+    oe_layer_texcParent.xy = oe_layer_tilec.xy*oe_layer_texParentMatrix[0][0] + oe_layer_texParentMatrix[3].xy;
+    oe_layer_texcParent.zw = oe_layer_tilec.zw;
+#endif
 }

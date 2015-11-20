c509cfa125c98fda5c7358f946c3205f9abc6e53
gwaldron <gwaldron@gmail.com>
2015-11-20 09:11:22 -0500

    3pv: fix so it compiled with pre-3.4 OSG

--- src/applications/osgearth_3pv/osgearth_3pv.cpp
+++ src/applications/osgearth_3pv/osgearth_3pv.cpp
@@ -137,7 +137,8 @@ makeFrustumFromCamera( osg::Camera* camera )
 
     osg::Vec4Array* c = new osg::Vec4Array;
     c->push_back( osg::Vec4( 1., 1., 0., 1. ) );
-    geom->setColorArray( c, osg::Array::BIND_OVERALL );
+    geom->setColorArray( c );
+    geom->setColorBinding( osg::Geometry::BIND_OVERALL );
 
     GLushort idxLines[8] = {
         0, 5, 0, 6, 0, 7, 0, 8 };

67a595785e75ee7ea44d28f4208227b753b2bb72
gwaldron <gwaldron@gmail.com>
2016-01-14 10:56:53 -0500

    Add getModels to the resource library

--- src/osgEarthSymbology/ResourceLibrary.cpp
+++ src/osgEarthSymbology/ResourceLibrary.cpp
@@ -356,3 +356,15 @@ ResourceLibrary::getModel( const ModelSymbol* ms, const osgDB::Options* dbOption
     ResourceMap<InstanceResource>::const_iterator i = _instances.find( ms->name()->eval() );
     return i != _instances.end() ? dynamic_cast<ModelResource*>(i->second.get()) : 0L;
 }
+
+void
+ResourceLibrary::getModels( ModelResourceVector& output, const osgDB::Options* dbOptions ) const
+{
+    const_cast<ResourceLibrary*>(this)->initialize( dbOptions );
+    Threading::ScopedReadLock shared( _mutex );
+    output.reserve( _instances.size() );
+    for( ResourceMap<SkinResource>::const_iterator i = _skins.begin(); i != _skins.end(); ++i ) {
+        ModelResource* m = dynamic_cast<ModelResource*>(i->second.get());
+        if ( m ) output.push_back( m );
+    }
+}

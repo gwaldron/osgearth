ab861aeb3e1bfe03be51556a0ac4cf9849b14e65
Jason Beverage <jasonbeverage@gmail.com>
2016-04-05 10:56:25 -0400

    osgearth_server building on 3.2.1 and 3.4.0
    Stripped out unneeded code in osgearth_server

--- src/applications/osgearth_server/osgearth_server.cpp
+++ src/applications/osgearth_server/osgearth_server.cpp
@@ -44,15 +44,7 @@ using namespace osgViewer;
 class WindowCaptureCallback : public osg::Camera::DrawCallback
 {
     public:
-    
-        enum Mode
-        {
-            READ_PIXELS,
-            SINGLE_PBO,
-            DOUBLE_PBO,
-            TRIPLE_PBO
-        };
-    
+       
         enum FramePosition
         {
             START_FRAME,
@@ -62,11 +54,9 @@ class WindowCaptureCallback : public osg::Camera::DrawCallback
         struct ContextData : public osg::Referenced
         {
         
-            ContextData(osg::GraphicsContext* gc, Mode mode, GLenum readBuffer, const std::string& name):
+            ContextData(osg::GraphicsContext* gc, GLenum readBuffer):
                 _gc(gc),
-                _mode(mode),
                 _readBuffer(readBuffer),
-                _fileName(name),
                 _pixelFormat(GL_BGRA),
                 _type(GL_UNSIGNED_BYTE),
                 _width(0),
@@ -102,31 +92,8 @@ class WindowCaptureCallback : public osg::Camera::DrawCallback
             
                 // single buffered image
                 _imageBuffer.push_back(new osg::Image);
-                
-                // double buffer PBO.
-                switch(_mode)
-                {
-                    case(READ_PIXELS):
-                        osg::notify(osg::NOTICE)<<"Reading window usig glReadPixels, with out PixelBufferObject."<<std::endl;
-                        break;
-                    case(SINGLE_PBO): 
-                        osg::notify(osg::NOTICE)<<"Reading window usig glReadPixels, with a single PixelBufferObject."<<std::endl;
-                        _pboBuffer.push_back(0); 
-                        break;
-                    case(DOUBLE_PBO): 
-                        osg::notify(osg::NOTICE)<<"Reading window usig glReadPixels, with a double buffer PixelBufferObject."<<std::endl;
-                        _pboBuffer.push_back(0); 
-                        _pboBuffer.push_back(0); 
-                        break;
-                    case(TRIPLE_PBO): 
-                        osg::notify(osg::NOTICE)<<"Reading window usig glReadPixels, with a triple buffer PixelBufferObject."<<std::endl;
-                        _pboBuffer.push_back(0); 
-                        _pboBuffer.push_back(0); 
-                        _pboBuffer.push_back(0); 
-                        break;
-                    default:
-                        break;                                
-                }
+
+                _pboBuffer.push_back(0);
             }
             
             void getSize(osg::GraphicsContext* gc, int& width, int& height)
@@ -137,41 +104,41 @@ class WindowCaptureCallback : public osg::Camera::DrawCallback
                     height = gc->getTraits()->height;
                 }
             }
-            
-            void updateTimings(osg::Timer_t tick_start,
-                               osg::Timer_t tick_afterReadPixels,
-                               osg::Timer_t tick_afterMemCpy,
-                               unsigned int dataSize);
+           
 
             void read()
             {
+#if OSG_VERSION_GREATER_OR_EQUAL(3,4,0)
                 osg::GLExtensions* ext = osg::GLExtensions::Get(_gc->getState()->getContextID(),true);
+#else
+                osg::GLBufferObject::Extensions* ext = osg::GLBufferObject::getExtensions(_gc->getState()->getContextID(),true);
+#endif
 
+#if OSG_VERSION_GREATER_OR_EQUAL(3,4,0)
                 if (ext->isPBOSupported && !_pboBuffer.empty())
+#else
+                if (ext->isPBOSupported() && !_pboBuffer.empty())
+#endif
                 {
-                    if (_pboBuffer.size()==1)
-                    {
                         singlePBO(ext);
-                    }
-                }
-                else
-                {
-                    readPixels();
                 }
             }
             
             void readPixels();
 
+#if OSG_VERSION_GREATER_OR_EQUAL(3,4,0)
             void singlePBO(osg::GLExtensions* ext);
+#else
+            void singlePBO(osg::GLBufferObject::Extensions* ext);
+#endif
+
        
             typedef std::vector< osg::ref_ptr<osg::Image> >             ImageBuffer;
             typedef std::vector< GLuint > PBOBuffer;
 
        
             osg::GraphicsContext*   _gc;
-            Mode                    _mode;
             GLenum                  _readBuffer;
-            std::string             _fileName;
             
             GLenum                  _pixelFormat;
             GLenum                  _type;
@@ -194,8 +161,7 @@ class WindowCaptureCallback : public osg::Camera::DrawCallback
             osg::ref_ptr< osg::Image > _lastImage;
         };
     
-        WindowCaptureCallback(Mode mode, FramePosition position, GLenum readBuffer):
-            _mode(mode),
+        WindowCaptureCallback(FramePosition position, GLenum readBuffer):
             _position(position),
             _readBuffer(readBuffer)
         {
@@ -205,9 +171,7 @@ class WindowCaptureCallback : public osg::Camera::DrawCallback
 
         ContextData* createContextData(osg::GraphicsContext* gc) const
         {
-            std::stringstream filename;
-            filename << "test_"<<_contextDataMap.size()<<".jpg";
-            return new ContextData(gc, _mode, _readBuffer, filename.str());
+            return new ContextData(gc, _readBuffer);
         }
         
         ContextData* getContextData(osg::GraphicsContext* gc) const
@@ -231,7 +195,6 @@ class WindowCaptureCallback : public osg::Camera::DrawCallback
         
         typedef std::map<osg::GraphicsContext*, osg::ref_ptr<ContextData> > ContextDataMap;
 
-        Mode                        _mode;        
         FramePosition               _position;
         GLenum                      _readBuffer;
         mutable OpenThreads::Mutex  _mutex;
@@ -241,101 +204,12 @@ class WindowCaptureCallback : public osg::Camera::DrawCallback
         
 };
 
-void WindowCaptureCallback::ContextData::updateTimings(osg::Timer_t tick_start,
-                                                       osg::Timer_t tick_afterReadPixels,
-                                                       osg::Timer_t tick_afterMemCpy,
-                                                       unsigned int dataSize)
-{
-    if (!_reportTimingFrequency) return;
-
-    double timeForReadPixels = osg::Timer::instance()->delta_s(tick_start, tick_afterReadPixels);
-    double timeForFullCopy = osg::Timer::instance()->delta_s(tick_start, tick_afterMemCpy);
-    double timeForMemCpy = osg::Timer::instance()->delta_s(tick_afterReadPixels, tick_afterMemCpy);
-
-    _timeForReadPixels += timeForReadPixels;
-    _timeForFullCopy += timeForFullCopy;
-    _timeForMemCpy += timeForMemCpy;
-    
-    ++_numTimeValuesRecorded;
-    
-    if (_numTimeValuesRecorded==_reportTimingFrequency)
-    {
-        timeForReadPixels = _timeForReadPixels/double(_numTimeValuesRecorded);
-        timeForFullCopy = _timeForFullCopy/double(_numTimeValuesRecorded);
-        timeForMemCpy = _timeForMemCpy/double(_numTimeValuesRecorded);
-        
-        double averageFrameTime =  osg::Timer::instance()->delta_s(_previousFrameTick, tick_afterMemCpy)/double(_numTimeValuesRecorded);
-        double fps = 1.0/averageFrameTime;    
-        _previousFrameTick = tick_afterMemCpy;
-
-        _timeForReadPixels = 0.0;
-        _timeForFullCopy = 0.0;
-        _timeForMemCpy = 0.0;
-
-        _numTimeValuesRecorded = 0;
-
-        double numMPixels = double(_width * _height) / 1000000.0;
-        double numMb = double(dataSize) / (1024*1024);
-
-        int prec = osg::notify(osg::NOTICE).precision(5);
-
-        if (timeForMemCpy==0.0)
-        {
-            osg::notify(osg::NOTICE)<<"fps = "<<fps<<", full frame copy = "<<timeForFullCopy*1000.0f<<"ms rate = "<<numMPixels / timeForFullCopy<<" Mpixel/sec, copy speed = "<<numMb / timeForFullCopy<<" Mb/sec"<<std::endl;
-        }
-        else
-        {
-            osg::notify(osg::NOTICE)<<"fps = "<<fps<<", full frame copy = "<<timeForFullCopy*1000.0f<<"ms rate = "<<numMPixels / timeForFullCopy<<" Mpixel/sec, "<<numMb / timeForFullCopy<< " Mb/sec "<<
-                                      "time for memcpy = "<<timeForMemCpy*1000.0<<"ms  memcpy speed = "<<numMb / timeForMemCpy<<" Mb/sec"<<std::endl;
-        }
-        osg::notify(osg::NOTICE).precision(prec);
-
-    }
-
-}
-
-void WindowCaptureCallback::ContextData::readPixels()
-{
-    // std::cout<<"readPixels("<<_fileName<<" image "<<_currentImageIndex<<" "<<_currentPboIndex<<std::endl;
-
-    unsigned int nextImageIndex = (_currentImageIndex+1)%_imageBuffer.size();
-    unsigned int nextPboIndex = _pboBuffer.empty() ? 0 : (_currentPboIndex+1)%_pboBuffer.size();
-
-    int width=0, height=0;
-    getSize(_gc, width, height);
-    if (width!=_width || _height!=height)
-    {
-        std::cout<<"   Window resized "<<width<<", "<<height<<std::endl;
-        _width = width;
-        _height = height;
-    }
-
-    osg::Image* image = _imageBuffer[_currentImageIndex].get();
-
-    osg::Timer_t tick_start = osg::Timer::instance()->tick();
-
-#if 1
-    image->readPixels(0,0,_width,_height,
-                      _pixelFormat,_type);
+#if OSG_VERSION_GREATER_OR_EQUAL(3,4,0)
+            void WindowCaptureCallback::ContextData::singlePBO(osg::GLExtensions* ext)
+#else
+            void WindowCaptureCallback::ContextData::singlePBO(osg::GLBufferObject::Extensions* ext)
 #endif
-
-    osg::Timer_t tick_afterReadPixels = osg::Timer::instance()->tick();
-
-    updateTimings(tick_start, tick_afterReadPixels, tick_afterReadPixels, image->getTotalSizeInBytes());
-
-    if (!_fileName.empty())
-    {
-        // osgDB::writeImageFile(*image, _fileName);
-    }
-
-    _currentImageIndex = nextImageIndex;
-    _currentPboIndex = nextPboIndex;
-}
-
-void WindowCaptureCallback::ContextData::singlePBO(osg::GLExtensions* ext)
-{
-    // std::cout<<"singelPBO(  "<<_fileName<<" image "<<_currentImageIndex<<" "<<_currentPboIndex<<std::endl;
-
+{ 
     unsigned int nextImageIndex = (_currentImageIndex+1)%_imageBuffer.size();
 
     int width=0, height=0;
@@ -397,12 +271,6 @@ void WindowCaptureCallback::ContextData::singlePBO(osg::GLExtensions* ext)
 
     ext->glBindBuffer(GL_PIXEL_PACK_BUFFER_ARB, 0);
 
-    osg::Timer_t tick_afterMemCpy = osg::Timer::instance()->tick();
-
-    updateTimings(tick_start, tick_afterReadPixels, tick_afterMemCpy, image->getTotalSizeInBytes());
-
-    OE_NOTICE << "Saving image" << std::endl;
-    //osgDB::writeImageFile(*image, "test.png");
     _lastImage = new osg::Image(*image);
 
     _currentImageIndex = nextImageIndex;
@@ -440,32 +308,6 @@ static std::string response(std::string const &content
   }
 
 
-struct CloneImageHandler : public osgViewer::ScreenCaptureHandler::CaptureOperation
-{
-    virtual void operator()(const osg::Image& image, const unsigned int context_id)
-    {        
-        setImage(image);
-    }
-
-    void setImage(const osg::Image &image)
-    {
-        _image = new osg::Image(image);
-    }
-
-    osg::Image* getImage()
-    {     
-        if (_image.valid())
-        {
-            OE_NOTICE << "Returning image" << std::endl;
-            return new osg::Image(*_image.get());
-        }
-        return 0;
-    }
-
-    osg::ref_ptr< osg::Image > _image;
-};
-
-
 
 class TileImageServer
 {
@@ -476,7 +318,6 @@ public:
       _windowCaptureCallback(0)
       {
           _viewer = new osgViewer::Viewer;
-          //_viewer->setUpViewInWindow(0,0,256,256);
           _viewer->getCamera()->setSmallFeatureCullingPixelSize(-1.0f);
           float half = 256.0/2.0;
           _viewer->getCamera()->setProjectionMatrixAsOrtho2D(-half, half, -half, half);
@@ -493,14 +334,7 @@ public:
               OE_NOTICE << "Failed to create graphics context" << std::endl;
           }
 
-          /*
-          _screenCaptureHandler = new ScreenCaptureHandler;
-          _cloneImageHandler = new CloneImageHandler();
-          _screenCaptureHandler->setCaptureOperation(_cloneImageHandler.get());
-          _viewer->addEventHandler(_screenCaptureHandler);
-          */
-
-          _windowCaptureCallback = new WindowCaptureCallback(WindowCaptureCallback::SINGLE_PBO, WindowCaptureCallback::END_FRAME, GL_BACK);
+          _windowCaptureCallback = new WindowCaptureCallback(WindowCaptureCallback::END_FRAME, GL_BACK);
 
            GLenum buffer = GL_BACK;
            _viewer->getCamera()->setDrawBuffer(buffer);
@@ -550,27 +384,23 @@ public:
 
           TileKey key(z, x, y, _mapNode->getMap()->getProfile());
 
-          OE_NOTICE << "key=" << key.str() << std::endl;
+          OE_DEBUG << "key=" << key.str() << std::endl;
 
           // Set the projection matrix to capture the tile.                    
-          OE_NOTICE << "Key extent " << z << "(" << x << ", " << y << ") = " << key.getExtent().toString() << std::endl;
+          OE_DEBUG << "Key extent " << z << "(" << x << ", " << y << ") = " << key.getExtent().toString() << std::endl;
           _viewer->getCamera()->setProjectionMatrixAsOrtho2D(key.getExtent().xMin(), key.getExtent().xMax(), key.getExtent().yMin(), key.getExtent().yMax());
           _viewer->frame();
           while (_viewer->getDatabasePager()->getRequestsInProgress())
           {
-              OE_NOTICE << "Waiting...." << std::endl;
+              OE_DEBUG << "Waiting...." << std::endl;
               _viewer->frame();
           }
-
-          OE_NOTICE << "Snap!" << std::endl;
 
           _viewer->getCamera()->setFinalDrawCallback(_windowCaptureCallback);
           _viewer->frame();
           _viewer->frame();
           _viewer->getCamera()->setFinalDrawCallback( 0 );
-         
-
-           //return _cloneImageHandler->getImage();
+                   
           if (_windowCaptureCallback->_image)
           {
               return new osg::Image(*_windowCaptureCallback->_image);
@@ -581,8 +411,6 @@ public:
     osgViewer::Viewer *_viewer;
     osg::ref_ptr< MapNode > _mapNode;
     osg::ref_ptr< osg::Group > _root;
-    osg::ref_ptr< CloneImageHandler > _cloneImageHandler;
-    osg::ref_ptr< ScreenCaptureHandler > _screenCaptureHandler;
     osg::ref_ptr< WindowCaptureCallback > _windowCaptureCallback;
     OpenThreads::Mutex _mutex;
 };
@@ -601,7 +429,7 @@ static void ev_handler(struct mg_connection *nc, int ev, void *ev_data) {
   case MG_EV_HTTP_REQUEST:
       {
           std::string url(hm->uri.p, hm->uri.len);
-          OE_NOTICE << "url=" << url << std::endl;
+          OE_DEBUG << "url=" << url << std::endl;
           StringTokenizer tok("/");
           StringVector tized;
           tok.tokenize(url, tized);            
@@ -611,10 +439,11 @@ static void ev_handler(struct mg_connection *nc, int ev, void *ev_data) {
               int x = as<int>(tized[2], 0);
               unsigned int y = as<int>(osgDB::getNameLessExtension(tized[3]),0);
               std::string ext = osgDB::getFileExtension(tized[3]);
-              std::cout << "z=" << z << std::endl;
-              std::cout << "x=" << x << std::endl;
-              std::cout << "y=" << y << std::endl;              
-              std::cout << "ext=" << ext << std::endl;
+              
+              OE_DEBUG << "z=" << z << std::endl;
+              OE_DEBUG << "x=" << x << std::endl;
+              OE_DEBUG << "y=" << y << std::endl;              
+              OE_DEBUG << "ext=" << ext << std::endl;
 
               osg::ref_ptr< osg::Image > image = _server->getTile(z, x, y );
 
@@ -633,8 +462,7 @@ static void ev_handler(struct mg_connection *nc, int ev, void *ev_data) {
                           mime = "image/jpeg";
                       }
                       std::string res = response(buf.str(), mime);
-                      mg_send(nc, res.c_str(), res.size());       
-                      //mg_send_http_chunk(nc, res.c_str(), res.size());
+                      mg_send(nc, res.c_str(), res.size());                             
                       sent = true;
                   }             
               }

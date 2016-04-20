5b8364782fa3dbe4c7157701bbbadca75163e131
Jason Beverage <jasonbeverage@gmail.com>
2016-04-20 10:18:08 -0400

    Moved MVT decoding logic into it's own file

--- src/osgEarthDrivers/feature_mapnikvectortiles/FeatureSourceMVT.cpp
+++ src/osgEarthDrivers/feature_mapnikvectortiles/FeatureSourceMVT.cpp
@@ -19,13 +19,10 @@
 #include "MVTFeatureOptions"
 
 #include <osgEarth/Registry>
-#include <osgEarth/XmlUtils>
 #include <osgEarth/FileUtils>
 #include <osgEarth/GeoData>
 #include <osgEarthFeatures/FeatureSource>
 #include <osgEarthFeatures/Filter>
-#include <osgEarthFeatures/BufferFilter>
-#include <osgEarthFeatures/ScaleFilter>
 #include <osg/Notify>
 #include <osgDB/FileNameUtils>
 #include <osgDB/FileUtils>
@@ -34,7 +31,7 @@
 #include <stdlib.h>
 #include <sqlite3.h>
 
-#include "vector_tile.pb.h"
+#include "MVT"
 
 #define LC "[MVT FeatureSource] "
 
@@ -42,25 +39,6 @@ using namespace osgEarth;
 using namespace osgEarth::Features;
 using namespace osgEarth::Drivers;
 
-#define CMD_BITS 3
-#define CMD_MOVETO 1
-#define CMD_LINETO 2
-#define CMD_CLOSEPATH 7
-
-// https://github.com/mapbox/mapnik-vector-tile/blob/master/examples/c%2B%2B/tileinfo.cpp
-enum CommandType {
-    SEG_END    = 0,
-    SEG_MOVETO = 1,
-    SEG_LINETO = 2,
-    SEG_CLOSE = (0x40 | 0x0f)
-};
-
-enum eGeomType {
-    Unknown = 0,
-    Point = 1,
-    LineString = 2,
-    Polygon = 3
-};
 
 class MVTFeatureSource : public FeatureSource
 {
@@ -128,11 +106,6 @@ public:
         return result;
     }
 
-    int zig_zag_decode(int n)
-    {
-        return (n >> 1) ^ (-(n & 1));
-    }
-
     FeatureCursor* createFeatureCursor( const Symbology::Query& query )
     {
         TileKey key = *query.tileKey();
@@ -170,186 +143,9 @@ public:
             // the pointer returned from _blob gets freed internally by sqlite, supposedly
             const char* data = (const char*)sqlite3_column_blob( select, 0 );
             int dataLen = sqlite3_column_bytes( select, 0 );
-
             std::string dataBuffer( data, dataLen );
-
-            // decompress if necessary:
-            if ( _compressor.valid() )
-            {
-                std::istringstream inputStream(dataBuffer);
-                std::string value;
-                if ( !_compressor->decompress(inputStream, value) )
-                {
-                    OE_WARN << LC << "Decompression failed" << std::endl;
-                    valid = false;
-                }
-                else
-                {
-                    dataBuffer = value;
-                }
-            }
-
-            
-            
-            mapnik::vector::tile tile;
-
-            if (tile.ParseFromString(dataBuffer))
-            {
-                // Get the layer in question
-                for (unsigned int i = 0; i < tile.layers().size(); i++)
-                {
-                    const mapnik::vector::tile_layer &layer = tile.layers().Get(i);
-
-                    //OE_NOTICE << layer.name() << std::endl;
-                    //if (layer.name() != "road") continue;
-                    //if (layer.name() != "building") continue;
-
-                    for (unsigned int j = 0; j < layer.features().size(); j++)
-                    {
-                        const mapnik::vector::tile_feature &feature = layer.features().Get(j);
-
-                        osg::ref_ptr< osgEarth::Symbology::Geometry > geometry; 
-
-                        eGeomType geomType = static_cast<eGeomType>(feature.type());
-                        if (geomType == ::Polygon)
-                        {
-                            //OE_NOTICE << "Polygon " << std::endl;
-                            geometry = new osgEarth::Symbology::Polygon();
-                        }
-                        else if (geomType == ::LineString)
-                        {
-                            //OE_NOTICE << "LineString" << std::endl;
-                            geometry = new osgEarth::Symbology::LineString();
-                        }
-                        else if (geomType == ::Point)
-                        {
-                            //OE_NOTICE << "Point" << std::endl;
-                            geometry = new osgEarth::Symbology::PointSet();
-                        }
-                        else
-                        {
-                            //OE_NOTICE << "uknown" << std::endl;
-                            geometry = new osgEarth::Symbology::LineString();
-                        }
-
-                        osg::ref_ptr< Feature > oeFeature = new Feature(geometry, key.getProfile()->getSRS());
-                        features.push_back(oeFeature.get());                    
-
-                        // Read attributes
-                        for (unsigned int k = 0; k < feature.tags().size(); k+=2)
-                        {
-                            std::string key = layer.keys().Get(feature.tags().Get(k));
-                            mapnik::vector::tile_value value = layer.values().Get(feature.tags().Get(k+1));
-
-                            if (value.has_bool_value())
-                            {
-                                oeFeature->set(key, value.bool_value());
-                            }
-                            else if (value.has_double_value())
-                            {
-                                oeFeature->set(key, value.double_value());
-                            }
-                            else if (value.has_float_value())
-                            {
-                                oeFeature->set(key, value.float_value());
-                            }
-                            else if (value.has_int_value())
-                            {
-                                oeFeature->set(key, (int)value.int_value());
-                            }
-                            else if (value.has_sint_value())
-                            {
-                                oeFeature->set(key, (int)value.sint_value());
-                            }
-                            else if (value.has_string_value())
-                            {
-                                oeFeature->set(key, value.string_value());
-                            }
-                            else if (value.has_uint_value())
-                            {
-                                oeFeature->set(key, (int)value.uint_value());
-                            }
-
-                            // Special path for getting heights from our test dataset.
-                            if (key == "other_tags")
-                            {
-                                std::string other_tags = value.string_value();
-                                
-                                StringTokenizer tok("=>");
-                                StringVector tized;
-                                tok.tokenize(other_tags, tized);            
-                                if (tized.size() == 3)
-                                {
-                                    if (tized[0] == "height")
-                                    {
-                                        std::string value = tized[2];
-                                        // Remove quotes from the height
-                                        float height = as<float>(value, FLT_MAX);
-                                        if (height != FLT_MAX)
-                                        {
-                                            oeFeature->set("height", height);                                            
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        
-                        
-                        unsigned int length = 0;
-                        int cmd = -1;
-                        const int cmd_bits = 3;
-
-                        unsigned int tileres = layer.extent();
-
-                        int x = 0;
-                        int y = 0;
-
-                        for (int k = 0; k < feature.geometry_size();)
-                        {
-                            if (!length)
-                            {
-                                unsigned int cmd_length = feature.geometry(k++);
-                                cmd = cmd_length & ((1 << cmd_bits) - 1);
-                                length = cmd_length >> cmd_bits;
-                            } 
-                            if (length > 0)
-                            {
-                                length--;
-                                if (cmd == SEG_MOVETO || cmd == SEG_LINETO)
-                                {
-                                    int px = feature.geometry(k++);
-                                    int py = feature.geometry(k++);
-                                    px = zig_zag_decode(px);
-                                    py = zig_zag_decode(py);
-
-                                    x += px;
-                                    y += py;
-
-                                    double width = key.getExtent().width();
-                                    double height = key.getExtent().height();
-
-                                    double geoX = key.getExtent().xMin() + (width/(double)tileres) * (double)x;
-                                    double geoY = key.getExtent().yMax() - (height/(double)tileres) * (double)y;
-                                    geometry->push_back(geoX, geoY, 0);
-                                }
-                                else if (cmd == (SEG_CLOSE & ((1 << cmd_bits) - 1)))
-                                {
-                                    geometry->push_back(geometry->front());
-                                }
-                            }
-                        }
-
-                        if (geometry->getType() == Geometry::TYPE_POLYGON)
-                        {
-                            geometry->rewind(osgEarth::Symbology::Geometry::ORIENTATION_CCW);                                               
-                        }
-                    }
-                }
-            }
-            else
-            {
-                OE_WARN << LC << "Failed to parse " << key.str() << std::endl;
-            }
+            std::stringstream in(dataBuffer);
+            MVT::read(in, key, features);
         }
         else
         {

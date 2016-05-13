88d2dc58ec8d983a3b0f80d69165d63192a5abf8
Jason Beverage <jasonbeverage@gmail.com>
2016-05-13 13:55:38 -0400

    Multiplying the height by the min tile range factor to make sure the ranges are correct

--- src/applications/osgearth_server/osgearth_server.cpp
+++ src/applications/osgearth_server/osgearth_server.cpp
@@ -400,7 +400,9 @@ public:
           OE_DEBUG << "Key extent " << z << "(" << x << ", " << y << ") = " << key.getExtent().toString() << std::endl;
           //_viewer->getCamera()->setProjectionMatrixAsOrtho2D(key.getExtent().xMin(), key.getExtent().xMax(), key.getExtent().yMin(), key.getExtent().yMax());
           unsigned int heightIndex = osg::clampBetween(z, 0u, LOD_COUNT -1u);
-          double height = oe_LODRanges[heightIndex];
+
+          // Multiply by the min_tile_range_factor to get close to what the correct distance would be for this tile.
+          double height = oe_LODRanges[heightIndex] * *_mapNode->getMapNodeOptions().getTerrainOptions().minTileRangeFactor();
           osg::Vec3d center = key.getExtent().getCentroid();
           osg::Vec3d eye = center + osg::Vec3d(0,0,height);
           _viewer->getCamera()->setViewMatrixAsLookAt(eye, center, osg::Vec3d(0,1,0));

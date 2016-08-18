ae1509dda5a7de583c6ad430d6e030ae42d7045b
Jason Beverage <jasonbeverage@gmail.com>
2016-08-18 13:46:53 -0400

    Implemented other fog algorithms.  New FogCallback

--- src/osgEarthUtil/Fog.cpp
+++ src/osgEarthUtil/Fog.cpp
@@ -20,6 +20,7 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 #include <osgEarthUtil/Fog>
+#include <osg/Fog>
 #include <osgEarthUtil/Shaders>
 #include <osgEarth/Registry>
 #include <osgEarth/Capabilities>
@@ -30,6 +31,30 @@
 
 using namespace osgEarth;
 using namespace osgEarth::Util;
+
+
+void FogCallback::operator() (osg::StateAttribute* attr, osg::NodeVisitor* nv)
+{
+    // Update the oe_fog_algo uniform to reflect the current algorithm.
+    osg::Fog* fog = static_cast<osg::Fog*>(attr);
+    for (unsigned int i = 0; i < attr->getNumParents(); i++)
+    {
+        osg::StateSet* stateSet = attr->getParent(i);
+
+        if (fog->getMode() == osg::Fog::LINEAR)
+        {
+            stateSet->getOrCreateUniform("oe_fog_algo", osg::Uniform::INT)->set(0);
+        }
+        else if (fog->getMode() == osg::Fog::EXP)
+        {
+            stateSet->getOrCreateUniform("oe_fog_algo", osg::Uniform::INT)->set(1);
+        }
+        else if (fog->getMode() == osg::Fog::EXP2)
+        {
+            stateSet->getOrCreateUniform("oe_fog_algo", osg::Uniform::INT)->set(2);
+        }
+    }
+}
 
 
 FogEffect::FogEffect()

bca42596ea47cfbbfaa59a978df9a815152205e6
gwaldron <gwaldron@gmail.com>
2016-10-24 12:54:53 -0400

    Remove lighting defines from simple sky atmospheric shaders

--- src/osgEarthDrivers/sky_simple/SimpleSky.Atmosphere.vert.glsl
+++ src/osgEarthDrivers/sky_simple/SimpleSky.Atmosphere.vert.glsl
@@ -5,8 +5,6 @@ $GLSL_DEFAULT_PRECISION_FLOAT
 #pragma vp_location   vertex_view
 #pragma vp_order      0.5
 
-#pragma import_defines(OE_LIGHTING)
-
 // Atmospheric Scattering and Sun Shaders
 // Adapted from code that is Copyright (c) 2004 Sean ONeil
 
@@ -142,10 +140,6 @@ void atmos_SkyFromAtmosphere(void)
 
 void atmos_vertex_main(inout vec4 VertexVIEW) 
 {
-#ifndef OE_LIGHTING
-    return;
-#endif
-
     // Get camera position and height 
     vVec = osg_ViewMatrixInverse[3].xyz; 
     atmos_fCameraHeight = length(vVec); 

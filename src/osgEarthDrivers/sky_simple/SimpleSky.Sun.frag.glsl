de5879338c573b64e06a82acd051e31122525c2d
Jason Beverage <jasonbeverage@gmail.com>
2016-11-03 16:53:12 -0400

    Fix sun shader by removing gl_FragColor.r reference

--- src/osgEarthDrivers/sky_simple/SimpleSky.Sun.frag.glsl
+++ src/osgEarthDrivers/sky_simple/SimpleSky.Sun.frag.glsl
@@ -15,5 +15,5 @@ void main( void )
 { 
    float fCos = -atmos_v3Direction[2];          
    float fMiePhase = 0.050387596899224826 * (1.0 + fCos*fCos) / atmos_fastpow(1.9024999999999999 - -1.8999999999999999*fCos, 1.5); 
-   out_FragColor = vec4(fMiePhase*vec3(.3,.3,.2), atmos_sunAlpha*gl_FragColor.r);
+   out_FragColor = vec4(fMiePhase*vec3(.3,.3,.2), atmos_sunAlpha);
 }

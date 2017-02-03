b8fce08fafe636cf1c0e820107cc055fbc8d4fa1
gwaldron <gwaldron@gmail.com>
2017-02-03 12:19:55 -0500

    add TritonLayer, update triton.earth to use it

--- src/osgEarthTriton/TritonContext.cpp
+++ src/osgEarthTriton/TritonContext.cpp
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
-#include <Triton.h>
 #include "TritonContext"
 #include <osg/GLExtensions>
 #include <osg/Math>
---
+++ src/osgEarthTriton/TritonLayer.cpp
@@ -0,0 +1,97 @@
+/* -*-c++-*- */
+/* osgEarth - Dynamic map generation toolkit for OpenSceneGraph
+ * Copyright 2016 Pelican Mapping
+ * http://osgearth.org
+ *
+ * osgEarth is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+#include "TritonLayer"
+#include "TritonNode"
+#include "TritonContext"
+#include <osgEarth/ImageLayer>
+
+#define LC "[TritonLayer] "
+
+using namespace osgEarth::Triton;
+
+/** Register this layer so it can be used in an earth file */
+namespace osgEarth { namespace Triton
+{
+    REGISTER_OSGEARTH_LAYER(triton, TritonLayer);
+    REGISTER_OSGEARTH_LAYER(triton_ocean, TritonLayer);
+} }
+
+
+TritonLayer::TritonLayer() :
+osgEarth::VisibleLayer(&_optionsConcrete),
+_options(&_optionsConcrete)
+{
+    init();
+}
+
+TritonLayer::TritonLayer(const TritonLayerOptions& options) :
+osgEarth::VisibleLayer(&_optionsConcrete),
+_options(&_optionsConcrete),
+_optionsConcrete(options)
+{
+    init();
+}
+
+void
+TritonLayer::init()
+{
+    OE_INFO << LC << "Creating a TritonLayer\n";
+
+    osgEarth::VisibleLayer::init();
+
+    this->setName("Triton");
+    setRenderType(RENDERTYPE_NONE);
+
+    TritonOptions legacyOptions(options());
+    _tritonNode = new TritonNode(0L, legacyOptions);
+}
+
+osg::Node*
+TritonLayer::getNode() const
+{
+    return _tritonNode.get();
+}
+
+bool
+TritonLayer::setMaskLayer(const osgEarth::ImageLayer* maskLayer)
+{
+    _tritonNode->setMaskLayer(maskLayer);
+    return true;
+}
+
+void
+TritonLayer::addedToMap(const osgEarth::Map* map)
+{    
+    if (options().maskLayer().isSet())
+    {
+        // listen for the mask layer.
+        _layerListener.listen(map, options().maskLayer().get(), this, &TritonLayer::setMaskLayer);
+    }      
+}
+
+void
+TritonLayer::removedFromMap(const osgEarth::Map* map)
+{
+    if (options().maskLayer().isSet())
+    {
+        _layerListener.clear();
+        setMaskLayer(0L);
+    }
+}
+

6c7514e1058a9abdaa2c6704f9c1cd5fbdbc1296
gwaldron <gwaldron@gmail.com>
2017-11-14 12:13:00 -0500

    Fix #999 DepthOffset setGraph(this) causing occaisonal crashes due to stateset manipulation on shared graph

--- src/osgEarthFeatures/FeatureModelGraph.cpp
+++ src/osgEarthFeatures/FeatureModelGraph.cpp
@@ -1508,8 +1508,9 @@ FeatureModelGraph::applyRenderSymbology(const Style& style, osg::Node* node)
     {
         if ( render->depthOffset().isSet() )
         {
-            _depthOffsetAdapter.setGraph( this );
-            _depthOffsetAdapter.setDepthOffsetOptions( *render->depthOffset() );
+            DepthOffsetAdapter doa;
+            doa.setGraph(node);
+            doa.setDepthOffsetOptions( *render->depthOffset() );
         }
 
         if ( render->renderBin().isSet() )

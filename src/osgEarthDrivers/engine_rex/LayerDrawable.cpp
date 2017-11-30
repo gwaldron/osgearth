e70f8173a87185e3879420b0de3bbd9c3d8cd2e3
gwaldron <gwaldron@gmail.com>
2017-11-30 14:32:45 -0500

    Fix #1011; caused by calling State.dirtyAllModes and confusing the OSG state tracking w.r.t. CullFace

--- src/osgEarthDrivers/engine_rex/LayerDrawable.cpp
+++ src/osgEarthDrivers/engine_rex/LayerDrawable.cpp
@@ -77,8 +77,11 @@ LayerDrawable::drawImplementation(osg::RenderInfo& ri) const
     // necessary when doing custom OpenGL within a Drawable.
     if (_clearOsgState)
     {
+        // Necessary because tile->draw() applies texture attributes without informing the State:
         ri.getState()->dirtyAllAttributes();
-        ri.getState()->dirtyAllModes();
+
+        // NOTE: this is a NOOP in OSG 3.5.x, but not in 3.4.x ... Later we will need to
+        // revisit whether to call disableAllVertexArrays() in 3.5.x instead.
         ri.getState()->dirtyAllVertexArrays();
         
         // unbind local buffers when finished.

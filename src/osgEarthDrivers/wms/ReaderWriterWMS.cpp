5e1ecf2622cfc46dc36c1478644fa3d11ffa0282
John Vidar Larring <john.larring@chyronhego.com>
2018-01-22 12:37:14 +0100

    Added new optional http_header config to osgEarth::URI
    The new URL::http_header config is used to pass custom http header to Curl in osgEarth::HTTPClient. The prime reason for adding the URI::http_header is to enable configuration of oauth2 bearer token for sources protected by oauth2 authentication (Ref. ECMWF data at https://thredds.met.no/thredds/metno.html).
    
    Example imagelayer accessing WMS restricted by oauth2:
    
       <image name="met-no-ecmwf-air-temperature" driver="wms">
          <url>https://thredds.met.no/thredds/wms/ecmwf/atmo/ec_atmo_0_1deg_20180118T120000Z_pl.nc</url>
          <http_header>Authorization: Bearer YOUR-TOKEN-STRING</http_header>
          <format>png</format>
          <version>1.3.0</version>
          <layers>air_temperature_pl</layers>
          <tile_size>256</tile_size>
          <srs>EPSG:4326</srs>
          <transparent>true</transparent>
          <times>2018-01-18T18:00:00.000Z</times>
          <cache_policy usage="no_cache"/>
       </image>
    
    Passing URI::http_header to CURL has been enabled for: TFS. WFS, TMS, WMS, and XYZ.
    
    Note 1: CURLOPT_XOAUTH2_BEARER was not used, since it is not supported by current curl version on CentOS7 (curl-7.29.0)
    
    Note 2: It would be possible to omit using osgDB::Options::pluginStringData if urls were passed as URI classes rather than std::string. However, this would be a much larger code change.

--- src/osgEarthDrivers/wms/ReaderWriterWMS.cpp
+++ src/osgEarthDrivers/wms/ReaderWriterWMS.cpp
@@ -113,8 +113,16 @@ public:
                 std::string("&REQUEST=GetCapabilities") );
         }
 
+	// Add URI::http_header as plugin string data to be passed as custom header to CURL
+	// later in HTTPClient::doGet().
+	osg::ref_ptr<osgDB::Options> localDbOptions = Registry::instance()->cloneOrCreateOptions( dbOptions );
+	if (_options.url()->httpHeader().isSet())
+	{
+	    localDbOptions->setPluginStringData("osgEarth::URI::httpHeader", _options.url()->httpHeader().get());
+	}
+
         //Try to read the WMS capabilities
-        osg::ref_ptr<WMSCapabilities> capabilities = WMSCapabilitiesReader::read( capUrl.full(), dbOptions );
+	osg::ref_ptr<WMSCapabilities> capabilities = WMSCapabilitiesReader::read( capUrl.full(), localDbOptions );
         if ( !capabilities.valid() )
         {
             return Status::Error( Status::ResourceUnavailable, "Unable to read WMS GetCapabilities." );
@@ -236,7 +244,7 @@ public:
         }
 
         OE_INFO << LC << "Testing for JPL/TileService at " << tsUrl.full() << std::endl;
-        _tileService = TileServiceReader::read(tsUrl.full(), dbOptions);
+	_tileService = TileServiceReader::read(tsUrl.full(), localDbOptions);
         if (_tileService.valid())
         {
             OE_INFO << LC << "Found JPL/TileService spec" << std::endl;
@@ -272,7 +280,7 @@ public:
             OE_INFO << LC << "Profile=" << getProfile()->toString() << std::endl;
 
             // set up the cache options properly for a TileSource.
-            _dbOptions = Registry::instance()->cloneOrCreateOptions( dbOptions );            
+	    _dbOptions = Registry::instance()->cloneOrCreateOptions( localDbOptions );
 
             return Status::OK();
         }

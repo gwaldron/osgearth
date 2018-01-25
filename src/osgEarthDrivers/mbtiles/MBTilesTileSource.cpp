129b64a864a12b540cd817729101b44bc62e0141
Jason Beverage <jasonbeverage@gmail.com>
2018-01-25 13:28:02 -0500

    Added new readStream function to ImageUtils to support reading images from a stream without having to know the format up front.

--- src/osgEarthDrivers/mbtiles/MBTilesTileSource.cpp
+++ src/osgEarthDrivers/mbtiles/MBTilesTileSource.cpp
@@ -193,10 +193,6 @@ MBTilesTileSource::initialize(const osgDB::Options* dbOptions)
         if ( _tileFormat.empty() )
             return Status::Error(Status::ConfigurationError, "Required format not in metadata, nor specified in the options.");
 
-        _rw = getReaderWriter( _tileFormat );
-        if ( !_rw.valid() )
-            return Status::Error(Status::ServiceUnavailable, "No plugin to load format \"" + _tileFormat + "\"");
-
         // check for compression.
         std::string compression;
         getMetaData("compression", compression);
@@ -374,11 +370,7 @@ MBTilesTileSource::createImage(const TileKey&    key,
         if ( valid )
         {
             std::istringstream inputStream(dataBuffer);
-            osgDB::ReaderWriter::ReadResult rr = _rw->readImage( inputStream, _dbOptions.get() );
-            if (rr.validImage())
-            {
-                result = rr.takeImage();
-            }
+            result = ImageUtils::readStream(inputStream, _dbOptions.get());
         }
     }
     else

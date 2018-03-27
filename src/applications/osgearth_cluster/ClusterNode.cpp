ffd2ac4bc33576a28dc00c4118cd94390846b896
Jason Beverage <jasonbeverage@gmail.com>
2018-03-27 13:55:52 -0400

    More work on clustering

---
+++ src/applications/osgearth_cluster/ClusterNode.cpp
@@ -0,0 +1,127 @@
+#include "ClusterNode"
+
+ClusterNode::ClusterNode():
+    _radius(50)
+{
+    setNumChildrenRequiringUpdateTraversal(1);
+    setCullingActive(false);
+
+    _group = new osg::Group;
+}
+
+void ClusterNode::addNode(PlaceNode* node)
+{
+    _placeNodes.push_back(node);
+}
+
+void ClusterNode::removeNode(PlaceNode* node)
+{
+    PlaceNodeList::iterator itr = std::find(_placeNodes.begin(), _placeNodes.end(), node);
+    if (itr != _placeNodes.end())
+    {
+        _placeNodes.erase(itr);
+    }
+}
+
+unsigned int ClusterNode::getRadius() const
+{
+    return _radius;
+}
+
+void ClusterNode::setRadius(unsigned int radius)
+{
+    _radius = radius;
+}
+
+void ClusterNode::getPlaces(osg::Camera* camera, PlaceNodeList& out)
+{
+    osg::Viewport* viewport = camera->getViewport();
+    if (!viewport)
+    {
+        return;
+    }
+
+    osg::Matrixd mvpw = camera->getViewMatrix() *
+        camera->getProjectionMatrix() *
+        camera->getViewport()->computeWindowMatrix();
+
+    std::vector<TPoint> points;
+
+    std::vector< osg::ref_ptr< PlaceNode > > validPlaces;
+
+    for (unsigned int i = 0; i < _placeNodes.size(); i++)
+    {
+        osg::Vec3d world;
+        _placeNodes[i]->getPosition().toWorld(world);
+        osg::Vec3d screen = world * mvpw;
+        //OE_NOTICE << "Screen " << screen.x() << ", " << screen.y() << std::endl;
+
+        if (screen.x() >= 0 && screen.x() <= viewport->width() &&
+            screen.y() >= 0 && screen.y() <= viewport->height())
+        {
+            validPlaces.push_back(_placeNodes[i]);
+            points.push_back({ screen.x(), screen.y() });
+        }
+    }
+
+    kdbush::KDBush<TPoint> index(points);
+    std::set< unsigned int > clustered;
+
+    for (unsigned int i = 0; i < validPlaces.size(); i++)
+    {
+        TPoint &screen = points[i];
+        PlaceNode* place = validPlaces[i].get();
+
+        // If this thing is already part of a cluster then just continue.
+        if (clustered.find(i) != clustered.end())
+        {
+            continue;
+        }
+
+        // Get any matching indices that are part of this cluster.
+        TIds indices;
+        index.within(screen.first, screen.second, _radius, [&indices](const auto id) { indices.push_back(id); });
+
+        unsigned int actualCount = 0;
+
+        // Add all of the points to the cluster.
+        for (unsigned int j = 0; j < indices.size(); j++)
+        {
+            if (clustered.find(indices[j]) == clustered.end())
+            {
+                actualCount++;
+                clustered.insert(indices[j]);
+            }
+        }
+
+        std::stringstream buf;
+        buf << actualCount << std::endl;
+        place->setText(buf.str());
+
+        out.push_back(place);
+
+        clustered.insert(i);
+    }
+}
+
+void ClusterNode::traverse(osg::NodeVisitor& nv)
+{
+    if (nv.getVisitorType() == osg::NodeVisitor::UPDATE_VISITOR)
+    {
+    }
+    else if (nv.getVisitorType() == osg::NodeVisitor::CULL_VISITOR)
+    {
+        osgUtil::CullVisitor* cv = nv.asCullVisitor();
+
+        PlaceNodeList places;
+        getPlaces(cv->getCurrentCamera(), places);
+
+        _group->removeChildren(0, _group->getNumChildren());
+        for (unsigned int i = 0; i < places.size(); i++)
+        {
+            _group->addChild(places[i].get());
+        }
+    }
+
+    _group->traverse(nv);
+}
--- src/applications/osgearth_cluster/osgearth_cluster.cpp
+++ src/applications/osgearth_cluster/osgearth_cluster.cpp
@@ -31,50 +31,13 @@
 
 #include <osgEarthAnnotation/PlaceNode>
 
-#include "kdbush.hpp"
+#include "ClusterNode"
 
 #define LC "[viewer] "
 
 using namespace osgEarth;
 using namespace osgEarth::Util;
 using namespace osgEarth::Annotation;
-
-using TPoint = std::pair<int, int>;
-using TIds = std::vector<std::size_t>;
-
-static std::vector<TPoint> points = {
-    { 54, 1 },{ 97, 21 },{ 65, 35 },{ 33, 54 },{ 95, 39 },{ 54, 3 },{ 53, 54 },{ 84, 72 },
-    { 33, 34 },{ 43, 15 },{ 52, 83 },{ 81, 23 },{ 1, 61 },{ 38, 74 },{ 11, 91 },{ 24, 56 },
-    { 90, 31 },{ 25, 57 },{ 46, 61 },{ 29, 69 },{ 49, 60 },{ 4, 98 },{ 71, 15 },{ 60, 25 },
-    { 38, 84 },{ 52, 38 },{ 94, 51 },{ 13, 25 },{ 77, 73 },{ 88, 87 },{ 6, 27 },{ 58, 22 },
-    { 53, 28 },{ 27, 91 },{ 96, 98 },{ 93, 14 },{ 22, 93 },{ 45, 94 },{ 18, 28 },{ 35, 15 },
-    { 19, 81 },{ 20, 81 },{ 67, 53 },{ 43, 3 },{ 47, 66 },{ 48, 34 },{ 46, 12 },{ 32, 38 },
-    { 43, 12 },{ 39, 94 },{ 88, 62 },{ 66, 14 },{ 84, 30 },{ 72, 81 },{ 41, 92 },{ 26, 4 },
-    { 6, 76 },{ 47, 21 },{ 57, 70 },{ 71, 82 },{ 50, 68 },{ 96, 18 },{ 40, 31 },{ 78, 53 },
-    { 71, 90 },{ 32, 14 },{ 55, 6 },{ 32, 88 },{ 62, 32 },{ 21, 67 },{ 73, 81 },{ 44, 64 },
-    { 29, 50 },{ 70, 5 },{ 6, 22 },{ 68, 3 },{ 11, 23 },{ 20, 42 },{ 21, 73 },{ 63, 86 },
-    { 9, 40 },{ 99, 2 },{ 99, 76 },{ 56, 77 },{ 83, 6 },{ 21, 72 },{ 78, 30 },{ 75, 53 },
-    { 41, 11 },{ 95, 20 },{ 30, 38 },{ 96, 82 },{ 65, 48 },{ 33, 18 },{ 87, 28 },{ 10, 10 },
-    { 40, 34 },{ 10, 20 },{ 47, 29 },{ 46, 78 }
-};
-
-static void testRange() {
-    kdbush::KDBush<TPoint> index(points, 10);
-    TIds expectedIds = { 3, 90, 77, 72, 62, 96, 47, 8, 17, 15, 69, 71, 44, 19, 18, 45, 60, 20 };
-    TIds result;
-    index.range(20, 30, 50, 70, [&result](const auto id) { result.push_back(id); });
-
-    assert(std::equal(expectedIds.begin(), expectedIds.end(), result.begin()));
-}
-
-static void testRadius() {
-    kdbush::KDBush<TPoint> index(points, 10);
-    TIds expectedIds = { 3, 96, 71, 44, 18, 45, 60, 6, 25, 92, 42, 20 };
-    TIds result;
-    index.within(50, 50, 20, [&result](const auto id) { result.push_back(id); });
-
-    assert(std::equal(expectedIds.begin(), expectedIds.end(), result.begin()));
-}
 
 int
 usage(const char* name)
@@ -87,11 +50,7 @@ usage(const char* name)
 }
 
 void makePlaces(MapNode* mapNode, unsigned int count, std::vector< osg::ref_ptr< PlaceNode > >& placeNodes)
-{
-    // Make a group for 2D items, and activate the decluttering engine. Decluttering
-    // will migitate overlap between elements that occupy the same screen real estate.
-    osg::Group* labelGroup = new osg::Group();
-   
+{    
     // set up a style to use for placemarks:
     Style placeStyle;    
     placeStyle.getOrCreate<TextSymbol>()->declutter() = false;
@@ -119,86 +78,58 @@ void makePlaces(MapNode* mapNode, unsigned int count, std::vector< osg::ref_ptr<
             PlaceNode* place = new PlaceNode(mapNode, GeoPoint(geoSRS, lon, lat, 0.0), pin.get(), "Placemark", placeStyle);
             place->setDynamic(true);
             placeNodes.push_back(place);
-            labelGroup->addChild(place);
+            //labelGroup->addChild(place);
         }
-    }
-
-    mapNode->addChild(labelGroup);    
+    }    
 }
 
-void updatePlaces(std::vector< osg::ref_ptr< PlaceNode > >& places, osg::Matrixd& mvpw, osg::Viewport* viewport)
+Container*
+createControlPanel(osgViewer::View* view)
 {
-    if (!viewport)
+    ControlCanvas* canvas = ControlCanvas::getOrCreate(view);
+    VBox* vbox = canvas->addControl(new VBox());
+    vbox->setChildSpacing(10);
+    return vbox;
+}
+
+struct SetRadius : public ControlEventHandler
+{
+    SetRadius(ClusterNode* clusterNode) :
+        _clusterNode( clusterNode )
+    { }
+
+    void onValueChanged(Control* control, float value)
     {
-        return;
+        _clusterNode->setRadius(value);
     }
 
-    std::vector<TPoint> points;
+    ClusterNode* _clusterNode;    
+};
 
-    std::vector< osg::ref_ptr< PlaceNode > > validPlaces;
 
-    for (unsigned int i = 0; i < places.size(); i++)
-    {
-        osg::Vec3d world;
-        places[i]->getPosition().toWorld(world);
-        osg::Vec3d screen = world * mvpw;
-        //OE_NOTICE << "Screen " << screen.x() << ", " << screen.y() << std::endl;
+void buildControls(Container* container, ClusterNode* clusterNode)
+{
+    // the outer container:
+    Grid* grid = container->addControl(new Grid());
+    grid->setBackColor(0, 0, 0, 0.5);
+    grid->setMargin(10);
+    grid->setPadding(10);
+    grid->setChildSpacing(10);
+    grid->setChildVertAlign(Control::ALIGN_CENTER);
+    grid->setAbsorbEvents(true);
+    grid->setVertAlign(Control::ALIGN_TOP);
 
-        if (screen.x() >= 0 || screen.x() <= viewport->width() ||
-            screen.y() >= 0 || screen.y() <= viewport->height())
-        {
-            validPlaces.push_back(places[i]);
-            points.push_back({ screen.x(), screen.y() });
-        }
-        else
-        {
-            places[i]->setNodeMask(0);
-        }
-        
-    }    
+    // Radius
+    LabelControl* radiusLabel = new LabelControl("Radius");
+    radiusLabel->setVertAlign(Control::ALIGN_CENTER);
+    grid->setControl(0, 0, radiusLabel);
 
-    kdbush::KDBush<TPoint> index(points);
-    std::set< unsigned int > clustered;
-
-    for (unsigned int i = 0; i < validPlaces.size(); i++)
-    {
-        TPoint &screen = points[i];
-        PlaceNode* place = validPlaces[i].get();
-
-        // If this thing is already part of a cluster then just continue.
-        if (clustered.find(i) != clustered.end())
-        {
-            // Hide the placenode
-            place->setNodeMask(0);
-            continue;
-        }
-
-        // Show the place node, this is the master of the cluster
-        place->setNodeMask(~0);
-
-        // Get any matching indices that are part of this cluster.
-        TIds indices;
-        index.within(screen.first, screen.second, 50, [&indices](const auto id) { indices.push_back(id); });
-        OE_NOTICE << "Found " << indices.size() << " points in cluster" << std::endl;
-
-        unsigned int actualCount = 0;
-
-        // Add all of the points to the cluster.
-        for (unsigned int j = 0; j < indices.size(); j++)
-        {
-            if (clustered.find(indices[j]) == clustered.end())
-            {
-                actualCount++;
-                clustered.insert(indices[j]);
-            }            
-        }
-
-        std::stringstream buf;
-        buf << actualCount << std::endl;
-        place->setText(buf.str());
-
-        clustered.insert(i);
-    }
+    HSliderControl* radiusAdjust = new HSliderControl(1, 500, clusterNode->getRadius(), new SetRadius(clusterNode));
+    radiusAdjust->setWidth(125);
+    radiusAdjust->setHeight(12);
+    radiusAdjust->setVertAlign(Control::ALIGN_CENTER);
+    grid->setControl(1, 0, radiusAdjust);
+    grid->setControl(2, 0, new LabelControl(radiusAdjust));
 }
 
 
@@ -213,6 +144,9 @@ main(int argc, char** argv)
 
     // create a viewer:
     osgViewer::Viewer viewer(arguments);
+
+    //Create the control panel    
+    Container* container = createControlPanel(&viewer);
 
     // Tell the database pager to not modify the unref settings
     viewer.getDatabasePager()->setUnrefImageDataAfterApplyPolicy(true, false);
@@ -235,20 +169,22 @@ main(int argc, char** argv)
         std::vector< osg::ref_ptr< PlaceNode > > placeNodes;
 
         MapNode* mapNode = MapNode::findMapNode(node);
-        makePlaces(mapNode, 100000, placeNodes);
+        makePlaces(mapNode, 10000, placeNodes);
+
+        //osg::Group* labels = new osg::Group;
+        ClusterNode* clusterNode = new ClusterNode;
+        for (unsigned int i = 0; i < placeNodes.size(); i++)
+        {
+            clusterNode->addNode(placeNodes[i]);
+        }
+        mapNode->addChild(clusterNode);
+
+        buildControls(container, clusterNode);
 
         viewer.setSceneData(node);
 
         while (!viewer.done())
         {
-            if (viewer.getCamera()->getViewport())
-            {
-                osg::Matrixd mvpw = viewer.getCamera()->getViewMatrix() *
-                    viewer.getCamera()->getProjectionMatrix() *
-                    viewer.getCamera()->getViewport()->computeWindowMatrix();
-                updatePlaces(placeNodes, mvpw, viewer.getCamera()->getViewport());
-            }
-
             viewer.frame();
         }
         return viewer.run();

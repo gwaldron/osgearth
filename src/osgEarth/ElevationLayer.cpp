c5400f60f933328150e36b0b4aa19b201f5b3f0f
gwaldron <gwaldron@gmail.com>
2018-09-11 11:16:57 -0400

    ElevationLayer: fix bug that caused populate to take the fast path when offset layers exist

--- src/osgEarth/ElevationLayer.cpp
+++ src/osgEarth/ElevationLayer.cpp
@@ -995,7 +995,7 @@ ElevationLayerVector::populateHeightFieldAndNormalMap(osg::HeightField*      hf,
 
     // If we only have a single contender layer, and the tile is the same size as the requested 
     // heightfield then we just use it directly and avoid having to resample it
-    if (contenders.size() == 1)
+    if (contenders.size() == 1 && offsets.empty())
     {
         ElevationLayer* layer = contenders[0].layer.get();
         TileKey& contenderKey = contenders[0].key;

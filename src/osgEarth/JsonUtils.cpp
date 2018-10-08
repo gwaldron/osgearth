3beb547542e7c228de44608674a6be7a15c72e72
Pierre Hallot <pierre.hallot@sensefly.com>
2018-10-08 14:41:57 +0200

    Fix signed interger overflow when computing the threshold.
    osgearth/src/osgEarth/JsonUtils.cpp:2483:54: warning: integer overflow in expression of type ‘osgEarth::Json::Value::Int’ {aka ‘int’} results in ‘-2147483648’ [-Woverflow]
        Value::UInt threshold = (isNegative ? Value::UInt(-Value::minInt)
    
    For signed integers, -minInt = maxInt + 1.
    This results in an overflow, which is undefined behaviour for signed integers.
    
    Moreover, on most machines, the overflow would wrap around, resulting in the threshold being minInt for negative numbers.
    Negative values would always be decoded as double.

--- src/osgEarth/JsonUtils.cpp
+++ src/osgEarth/JsonUtils.cpp
@@ -2480,7 +2480,7 @@ Reader::decodeNumber( Token &token )
    bool isNegative = *current == '-';
    if ( isNegative )
       ++current;
-   Value::UInt threshold = (isNegative ? Value::UInt(-Value::minInt) 
+   Value::UInt threshold = (isNegative ? Value::UInt(Value::maxInt)
                                        : Value::maxUInt) / 10;
    Value::UInt value = 0;
    while ( current < token.end_ )

621ad1f3075ea96dba27dbac1d4b194992993e50
gwaldron <gwaldron@gmail.com>
2018-10-16 08:40:05 -0400

    RTTPicker: new path for avoiding bitwise ops in GLSL

--- src/osgEarthUtil/RTTPicker.glsl
+++ src/osgEarthUtil/RTTPicker.glsl
@@ -3,6 +3,8 @@ $GLSL_DEFAULT_PRECISION_FLOAT
 
 #pragma vp_entryPoint oe_pick_encodeObjectID
 #pragma vp_location   vertex_clip
+
+//#define USE_BITWISE_MATH
         
 // Vertex stage global containing the Object ID; set in ObjectIndex shader.
 uint oe_index_objectid;
@@ -21,11 +23,18 @@ void oe_pick_encodeObjectID(inout vec4 vertex)
     if ( oe_pick_color_contains_objectid == 0 )
     {
         // encode the objectID as a vec4 (color)
-        float b0 = float((oe_index_objectid & 0xff000000u) >> 24u);
-        float b1 = float((oe_index_objectid & 0x00ff0000u) >> 16u);
-        float b2 = float((oe_index_objectid & 0x0000ff00u) >> 8u );
-        float b3 = float((oe_index_objectid & 0x000000ffu)       );
-        oe_pick_encoded_objectid = vec4(b0, b1, b2, b3) / 255.0;
+#ifdef USE_BITWISE_MATH
+        uint u0 = (oe_index_objectid & 0xff000000u) >> 24u;
+        uint u1 = (oe_index_objectid & 0x00ff0000u) >> 16u;
+        uint u2 = (oe_index_objectid & 0x0000ff00u) >> 8u;
+        uint u3 = (oe_index_objectid & 0x000000ffu);
+#else
+        uint u0 = (oe_index_objectid / 16777216u);
+        uint u1 = (oe_index_objectid / 65536u) - (u0 * 256u);
+        uint u2 = (oe_index_objectid / 256u) - (u1 * 256u) - (u0 * 65536u);
+        uint u3 = (oe_index_objectid) - (u2 * 256u) - (u1 * 65536u) - (u0 * 16777216u);
+#endif
+        oe_pick_encoded_objectid = vec4(float(u0), float(u1), float(u2), float(u3)) / 255.0;
     }
 }
 

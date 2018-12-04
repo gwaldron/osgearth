63873be2bc27f01d8e408ff6ea9d116d605f51bb
gwaldron <gwaldron@gmail.com>
2018-12-04 11:17:58 -0500

    Light the moon properly

--- src/osgEarthDrivers/sky_simple/SimpleSky.Moon.vert.glsl
+++ src/osgEarthDrivers/sky_simple/SimpleSky.Moon.vert.glsl
@@ -1,10 +1,17 @@
 #version $GLSL_VERSION_STR
 $GLSL_DEFAULT_PRECISION_FLOAT
 
+uniform vec3 moonToSun;
 out vec4 moon_TexCoord;
+out float moon_Lighting;
 
 void main() 
 { 
-    moon_TexCoord = gl_MultiTexCoord0; 
+    moon_TexCoord = gl_MultiTexCoord0;
     gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex; 
+
+    // dot product results in a "lighting" factor, 0=none, 1=full,
+    // to send to the fragment shader
+    moon_Lighting = clamp(dot(gl_Normal, moonToSun), 0, 1);
+    moon_Lighting = pow(moon_Lighting, 0.4);
 }

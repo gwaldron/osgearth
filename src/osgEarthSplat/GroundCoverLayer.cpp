cedee2ea2cb823065254e1bf9fc7de51e8738325
gwaldron <gwaldron@gmail.com>
2019-10-03 10:09:52 -0400

    Removed the workaround for releaseObjects issues after failing to reproduce them

--- src/osgEarthSplat/GroundCoverLayer.cpp
+++ src/osgEarthSplat/GroundCoverLayer.cpp
@@ -483,5 +483,5 @@ GroundCoverLayer::releaseGLObjects(osg::State* state) const
     // For some unknown reason, release doesn't work on the zone 
     // texture def data (SplatTextureDef). So we have to recreate
     // it here.
-    const_cast<GroundCoverLayer*>(this)->buildStateSets();
+    //const_cast<GroundCoverLayer*>(this)->buildStateSets();
 }

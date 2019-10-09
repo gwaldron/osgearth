779f80f98385a614b82adc38cddf2819951141e0
gwaldron <gwaldron@gmail.com>
2019-10-09 17:03:34 -0400

    Fix: check for valid elevation pool envelope to prevent case where the map disappears and the client gets an incomplete envelope

--- src/osgEarth/FeatureModelGraph.cpp
+++ src/osgEarth/FeatureModelGraph.cpp
@@ -551,11 +551,15 @@ FeatureModelGraph::getBoundInWorldCoords(const GeoExtent& extent) const
     
     if ( mapf )
     {
-        // Use an appropriate resolution for this extents width
-        double resolution = workingExtent.width();
-        ElevationQuery query( *mapf );
-        GeoPoint p( mapf->getProfile()->getSRS(), center, ALTMODE_ABSOLUTE );
-        float elevation = query.getElevation( p, resolution );
+        // TODO: Use an appropriate resolution for this extents width
+        unsigned lod = 23u;
+        osg::ref_ptr<ElevationEnvelope> env = map->getElevationPool()->createEnvelope(center.getSRS(), lod);
+        float elevation = NO_DATA_VALUE;
+        if (env.valid())
+        {
+            elevation = env->getElevation(center.x(), center.y());
+        }
+
         // Check for NO_DATA_VALUE and use zero instead.
         if (elevation == NO_DATA_VALUE)
         {

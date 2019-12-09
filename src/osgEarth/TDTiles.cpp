b07baf1fff037f02d12c63b76400fd0ac58916d1
Jason Beverage <jasonbeverage@gmail.com>
2019-12-09 14:11:05 -0500

    Move CullVisitor import to header in TDTiles

--- src/osgEarth/TDTiles.cpp
+++ src/osgEarth/TDTiles.cpp
@@ -31,7 +31,6 @@
 #include <osgDB/FileNameUtils>
 #include <osgDB/WriteFile>
 #include <osg/CoordinateSystemNode>
-#include <osgUtil/CullVisitor>
 
 using namespace osgEarth;
 using namespace osgEarth::Util;

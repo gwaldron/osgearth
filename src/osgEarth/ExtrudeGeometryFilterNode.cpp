2c299e4b02bc8e11b1944afcc89317095c0e3569
gwaldron <gwaldron@gmail.com>
2020-01-17 17:55:10 -0500

    Fix mismatchef namespaces in mak classes

--- src/osgEarth/ExtrudeGeometryFilterNode.cpp
+++ src/osgEarth/ExtrudeGeometryFilterNode.cpp
@@ -3,35 +3,31 @@
 
 namespace osgEarth
 {
-    namespace Util
+    ExtrudeGeometryFilterNode::ExtrudeGeometryFilterNode()
     {
-        ExtrudeGeometryFilterNode::ExtrudeGeometryFilterNode()
-        {
 
-        }
+    }
 
-        ExtrudeGeometryFilterNode::ExtrudeGeometryFilterNode(osg::Group* extrusionGroup, const osg::Matrixd& xform)
-            : _extrusionGroup(extrusionGroup)
-            , _xform(xform)
-        {
+    ExtrudeGeometryFilterNode::ExtrudeGeometryFilterNode(osg::Group* extrusionGroup, const osg::Matrixd& xform)
+        : _extrusionGroup(extrusionGroup)
+        , _xform(xform)
+    {
 
-        }
-        ExtrudeGeometryFilterNode::ExtrudeGeometryFilterNode(const ExtrudeGeometryFilterNode& rhs, const osg::CopyOp& copyop)
-        {
-            _extrusionGroup = rhs._extrusionGroup;
-            _xform = rhs._xform;
-        }
+    }
+    ExtrudeGeometryFilterNode::ExtrudeGeometryFilterNode(const ExtrudeGeometryFilterNode& rhs, const osg::CopyOp& copyop)
+    {
+        _extrusionGroup = rhs._extrusionGroup;
+        _xform = rhs._xform;
+    }
 
 
-        REGISTER_OBJECT_WRAPPER(ExtrudeGeometryFilterNode,
-            new ExtrudeGeometryFilterNode,
-            osgEarth::Util::ExtrudeGeometryFilterNode,
-            "osg::Object osgEarth::Util::ExtrudeGeometryFilterNode")
-        {
-            ADD_OBJECT_SERIALIZER(_extrusionGroup, osg::Group, NULL);
+    REGISTER_OBJECT_WRAPPER(ExtrudeGeometryFilterNode,
+        new ExtrudeGeometryFilterNode,
+        osgEarth::ExtrudeGeometryFilterNode,
+        "osg::Object osgEarth::ExtrudeGeometryFilterNode")
+    {
+        ADD_OBJECT_SERIALIZER(_extrusionGroup, osg::Group, NULL);
 
-            ADD_MATRIX_SERIALIZER(_xform, osg::Matrixd::identity());
-        }
-
+        ADD_MATRIX_SERIALIZER(_xform, osg::Matrixd::identity());
     }
 }

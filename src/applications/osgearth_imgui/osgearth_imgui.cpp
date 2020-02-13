0a4cdf22dd1273901eb03aa9c52fc4ed1702b731
Jason Beverage <jasonbeverage@gmail.com>
2020-02-13 10:34:46 -0500

    Added osgearth_imgui application.

---
+++ src/applications/osgearth_imgui/osgearth_imgui.cpp
@@ -0,0 +1,169 @@
+/* -*-c++-*- */
+/* osgEarth - Geospatial SDK for OpenSceneGraph
+* Copyright 2018 Pelican Mapping
+* http://osgearth.org
+*
+* osgEarth is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+* IN THE SOFTWARE.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#include <osgEarth/ImGuiUtils>
+#include <osgEarth/OsgImGuiHandler.hpp>
+#include <imgui_internal.h>
+
+#include <osgViewer/Viewer>
+#include <osgEarth/Notify>
+#include <osgEarth/EarthManipulator>
+#include <osgEarth/ExampleResources>
+#include <osgEarth/MapNode>
+#include <osgEarth/ThreadingUtils>
+#include <osgEarth/Geocoder>
+
+#include <iostream>
+
+#include <osgEarth/Metrics>
+
+
+#define LC "[viewer] "
+
+using namespace osgEarth;
+using namespace osgEarth::Util;
+using namespace osgEarth::Contrib;
+
+class ImGuiDemo : public OsgImGuiHandler
+{
+public:
+    ImGuiDemo(MapNode* mapNode, EarthManipulator* earthManip) :
+        _mapNode(mapNode),
+        _earthManip(earthManip)        
+    {        
+    }
+
+protected:
+    void drawUi() override
+    {
+        // ImGui code goes here...
+        //ImGui::ShowDemoWindow();        
+
+        _layers.draw(_mapNode.get());
+
+        _search.draw(_earthManip);
+        /*
+
+        ImGui::Begin("Search");
+        //ImGuiInputTextFlags_Readonly
+        if (ImGui::InputText("Search", search, 128, ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_AutoSelectAll))
+        {
+            Geocoder geocoder;
+            _results = geocoder.search(search, _options.get());
+        }        
+
+        if (_results.isReady())
+        {
+            if (_results.getStatus().isOK())
+            {
+                while (_results.getFeatures()->hasMore())
+                {
+                    Feature* f = _results.getFeatures()->nextFeature();
+                    if (f->getGeometry())
+                    {
+                        GeoExtent extent(f->getSRS(), f->getGeometry()->getBounds());
+                        double maxDim = osg::maximum(extent.width(), extent.height());
+                        double range = ((0.5 * maxDim) / 0.267949849) * 111000.0;
+                        double lon, lat;
+                        extent.getCentroid(lon, lat);
+                        _earthManip->setViewpoint(Viewpoint("", lon, lat, 0, 0, -90, range), 3.0);
+                        break;
+                    }                    
+                }
+            }
+            _results = Geocoder::Results(Status::Code::ServiceUnavailable, 0);
+        }
+        ImGui::End();        
+        */
+    }
+
+    osg::ref_ptr< MapNode > _mapNode;
+    osg::ref_ptr<EarthManipulator> _earthManip;
+    LayersGUI _layers;
+    SearchGUI _search;        
+};
+
+int
+usage(const char* name)
+{
+    OE_NOTICE
+        << "\nUsage: " << name << " file.earth" << std::endl
+        << MapNodeHelper().usage() << std::endl;
+
+    return 0;
+}
+
+
+int
+main(int argc, char** argv)
+{
+    osg::ArgumentParser arguments(&argc, argv);
+
+    // help?
+    if (arguments.read("--help"))
+        return usage(argv[0]);
+
+    // create a viewer:
+    osgViewer::Viewer viewer(arguments);
+
+    // Tell the database pager to not modify the unref settings
+    viewer.getDatabasePager()->setUnrefImageDataAfterApplyPolicy(true, false);
+
+    // thread-safe initialization of the OSG wrapper manager. Calling this here
+    // prevents the "unsupported wrapper" messages from OSG
+    osgDB::Registry::instance()->getObjectWrapperManager()->findWrapper("osg::Image");
+
+    // install our default manipulator (do this before calling load)
+    EarthManipulator* manip = new EarthManipulator(arguments);
+    viewer.setCameraManipulator(manip);
+
+    // disable the small-feature culling
+    viewer.getCamera()->setSmallFeatureCullingPixelSize(-1.0f);
+
+    // set a near/far ratio that is smaller than the default. This allows us to get
+    // closer to the ground without near clipping. If you need more, use --logdepth
+    viewer.getCamera()->setNearFarRatio(0.0001);
+
+    // Setup the viewer for imgui
+    viewer.setRealizeOperation(new GlewInitOperation);
+
+    viewer.realize();
+
+    // load an earth file, and support all or our example command-line options
+    // and earth file <external> tags    
+    osg::Node* node = MapNodeHelper().load(arguments, &viewer);
+    if (node)
+    {
+        MapNode* mapNode = MapNode::findMapNode(node);
+        if (mapNode)
+        {
+            viewer.getEventHandlers().push_front(new ImGuiDemo(mapNode, manip));
+        }
+
+        viewer.setSceneData(node);
+        return viewer.run();// return Metrics::run(viewer);
+    }
+    else
+    {
+        return usage(argv[0]);
+    }
+}

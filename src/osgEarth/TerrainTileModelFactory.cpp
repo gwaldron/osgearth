fdc892e76a977ed9358f3bbe01d28605bedc7b25
Jason Beverage <jasonbeverage@gmail.com>
2020-02-20 12:22:58 -0500

    Setting data variance to static on gltf textures and textures created in TerrainTileModelFactory so they have a fighting chance of being unreffed by setUnrefImageAfterApply.

--- src/osgEarth/TerrainTileModelFactory.cpp
+++ src/osgEarth/TerrainTileModelFactory.cpp
@@ -606,6 +606,7 @@ TerrainTileModelFactory::createImageTexture(osg::Image*       image,
                                             const ImageLayer* layer) const
 {
     osg::Texture2D* tex = new osg::Texture2D( image );
+    tex->setDataVariance(osg::Object::STATIC);
 
     tex->setWrap( osg::Texture::WRAP_S, osg::Texture::CLAMP_TO_EDGE );
     tex->setWrap( osg::Texture::WRAP_T, osg::Texture::CLAMP_TO_EDGE );
@@ -639,6 +640,7 @@ osg::Texture*
 TerrainTileModelFactory::createCoverageTexture(osg::Image* image) const
 {
     osg::Texture2D* tex = new osg::Texture2D( image );
+    tex->setDataVariance(osg::Object::STATIC);
 
     tex->setInternalFormat(GL_R16F);
 
@@ -660,6 +662,7 @@ osg::Texture*
 TerrainTileModelFactory::createElevationTexture(osg::Image* image) const
 {
     osg::Texture2D* tex = new osg::Texture2D( image );
+    tex->setDataVariance(osg::Object::STATIC);
     tex->setInternalFormat(GL_R32F);
     tex->setFilter( osg::Texture::MAG_FILTER, osg::Texture::LINEAR );
     tex->setFilter( osg::Texture::MIN_FILTER, osg::Texture::NEAREST );
@@ -693,6 +696,7 @@ TerrainTileModelFactory::createNormalTexture(osg::Image* image, bool compress) c
     }    
 
     osg::Texture2D* tex = new osg::Texture2D(image);
+    tex->setDataVariance(osg::Object::STATIC);
     tex->setInternalFormatMode(osg::Texture::USE_IMAGE_DATA_FORMAT);
     tex->setFilter(osg::Texture::MAG_FILTER, osg::Texture::LINEAR);
     tex->setFilter(osg::Texture::MIN_FILTER, osg::Texture::LINEAR_MIPMAP_LINEAR);

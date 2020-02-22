8207db5a0c0620c78fd270d52d36d907860eee15
Glenn Waldron <gwaldron@gmail.com>
2020-02-22 11:56:05 -0500

    ShadowCaster: reduce default ranges

--- src/osgEarth/Shadowing.cpp
+++ src/osgEarth/Shadowing.cpp
@@ -62,8 +62,8 @@ _traversalMask( ~0 )
     {
         // default slices:
         _ranges.push_back(0.0f);
-        _ranges.push_back(1750.0f);
-        _ranges.push_back(25000.0f);
+        _ranges.push_back(500.0f);
+        _ranges.push_back(1250.0f);
 
         reinitialize();
     }

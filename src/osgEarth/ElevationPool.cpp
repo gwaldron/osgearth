ff8b080b5a187e34487a1622a26fb108474891a3
Glenn Waldron <gwaldron@gmail.com>
2020-02-24 15:05:23 -0500

    gcc compile issues

--- src/osgEarth/ElevationPool.cpp
+++ src/osgEarth/ElevationPool.cpp
@@ -441,8 +441,8 @@ float
 ElevationEnvelope::getElevation(double x, double y)
 {
     OE_PROFILING_ZONE;
-    char buf[16]; itoa(_lod, buf, 10);
-    OE_PROFILING_ZONE_TEXT(buf);
+    //char buf[16]; itoa(_lod, buf, 10);
+    //OE_PROFILING_ZONE_TEXT(buf);
 
     float elevation, resolution;
     sample(x, y, elevation, resolution);

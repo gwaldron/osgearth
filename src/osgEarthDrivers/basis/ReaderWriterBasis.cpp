8ceabe84e920bb61d51675a1d52d793fcda086f1
Jason Beverage <jasonbeverage@gmail.com>
2020-03-04 09:43:07 -0500

    Flipping basis image vertically to correct orientation.  Picking correct dxt level based on alpha in basis loader

--- src/osgEarthDrivers/basis/ReaderWriterBasis.cpp
+++ src/osgEarthDrivers/basis/ReaderWriterBasis.cpp
@@ -30,7 +30,7 @@
 using namespace basisu;
 
 class ReaderWriterBasis : public osgDB::ReaderWriter
-{   
+{
 public:
     ReaderWriterBasis()
     {
@@ -38,11 +38,11 @@ public:
 
         // one-time initialization at startup
         basist::basisu_transcoder_init();
-        sel_codebook = basist::etc1_global_selector_codebook(basist::g_global_selector_cb_size, basist::g_global_selector_cb);        
+        sel_codebook = basist::etc1_global_selector_codebook(basist::g_global_selector_cb_size, basist::g_global_selector_cb);
     }
-    
+
     virtual const char* className() const { return "Basis Universal Image Reader/Writer"; }
-   
+
     virtual ReadResult readObject(std::istream& fin, const osgDB::ReaderWriter::Options* options = NULL) const
     {
         return readImage(fin, options);
@@ -59,35 +59,21 @@ public:
         fin.seekg(0, fin.end);
         int length = fin.tellg();
         fin.seekg(0, fin.beg);
-        char * data = new char[length];
-        fin.read(data, length);        
+        char* data = new char[length];
+        fin.read(data, length);
 
         basist::basisu_transcoder transcoder(&sel_codebook);
 
-        unsigned int numImages = transcoder.get_total_images(data, length);        
+        unsigned int numImages = transcoder.get_total_images(data, length);
 
         // TODO:  Pass in an option or automatically determine the desired output format
 
-        /*
-                basist::transcoder_texture_format transcoder_texture_format = basist::transcoder_texture_format::cTFETC1;
-                GLenum internalTextureFormat = GL_COMPRESSED_RGB8_ETC2;
-                GLenum pixelFormat = GL_COMPRESSED_RGB8_ETC2;
-                */
 
-        basist::transcoder_texture_format transcoder_texture_format = basist::transcoder_texture_format::cTFBC1;
-        GLenum internalTextureFormat = GL_COMPRESSED_RGB_S3TC_DXT1_EXT;
-        GLenum pixelFormat = GL_COMPRESSED_RGB_S3TC_DXT1_EXT;
-
-        /*
-        basist::transcoder_texture_format transcoder_texture_format = basist::transcoder_texture_format::cTFBC3;
-        GLenum internalTextureFormat = GL_COMPRESSED_RGBA_S3TC_DXT5_EXT;
-        GLenum pixelFormat = GL_COMPRESSED_RGBA_S3TC_DXT5_EXT;
-        */
 
         basist::basisu_image_info image_info;
         unsigned int imageIndex = 0;
         if (transcoder.get_image_info(data, length, image_info, imageIndex))
-        {            
+        {
             OSG_INFO << "Got image info " << std::endl
                 << "Dimensions " << image_info.m_width << "x" << image_info.m_height << std::endl
                 << "Total levels " << image_info.m_total_levels << std::endl
@@ -96,10 +82,28 @@ public:
 
             transcoder.start_transcoding(data, length);
 
+            /*
+                basist::transcoder_texture_format transcoder_texture_format = basist::transcoder_texture_format::cTFETC1;
+                GLenum internalTextureFormat = GL_COMPRESSED_RGB8_ETC2;
+                GLenum pixelFormat = GL_COMPRESSED_RGB8_ETC2;
+                */
+
+            basist::transcoder_texture_format transcoder_texture_format = basist::transcoder_texture_format::cTFBC1;
+            GLenum internalTextureFormat = GL_COMPRESSED_RGB_S3TC_DXT1_EXT;
+            GLenum pixelFormat = GL_COMPRESSED_RGB_S3TC_DXT1_EXT;
+
+            // If the image has alpha then switch to dxt5 to get a format that supports alpha.
+            if (image_info.m_alpha_flag)
+            {
+                transcoder_texture_format = basist::transcoder_texture_format::cTFBC3;
+                internalTextureFormat = GL_COMPRESSED_RGBA_S3TC_DXT5_EXT;
+                pixelFormat = GL_COMPRESSED_RGBA_S3TC_DXT5_EXT;
+            }
+
             unsigned int totalSize = 0;
 
             std::vector< unsigned int > mipmapDataOffsets;
-            mipmapDataOffsets.reserve( image_info.m_total_levels - 1);
+            mipmapDataOffsets.reserve(image_info.m_total_levels - 1);
 
             // Compute the total size of the output buffer
             for (unsigned int levelIndex = 0; levelIndex < image_info.m_total_levels; levelIndex++)
@@ -118,13 +122,13 @@ public:
             }
 
             // Allocate memory for the total image including mipmaps
-            unsigned char *decoded = new unsigned char[totalSize];
-            memset(decoded, 0, totalSize);            
+            unsigned char* decoded = new unsigned char[totalSize];
+            memset(decoded, 0, totalSize);
 
             for (unsigned int levelIndex = 0; levelIndex < image_info.m_total_levels; levelIndex++)
             {
-                basist::basisu_image_level_info level_info;            
-                transcoder.get_image_level_info(data, length, level_info, 0, levelIndex);                
+                basist::basisu_image_level_info level_info;
+                transcoder.get_image_level_info(data, length, level_info, 0, levelIndex);
 
                 unsigned int offset = 0;
                 if (levelIndex > 0)
@@ -132,7 +136,7 @@ public:
                     offset = mipmapDataOffsets[levelIndex - 1];
                 }
 
-                transcoder.transcode_image_level(data, length, imageIndex, levelIndex, &decoded[offset], level_info.m_total_blocks, transcoder_texture_format, 0);                
+                transcoder.transcode_image_level(data, length, imageIndex, levelIndex, &decoded[offset], level_info.m_total_blocks, transcoder_texture_format, 0);
             }
 
             osg::Image* image = new osg::Image;
@@ -141,8 +145,10 @@ public:
             {
                 image->setMipmapLevels(mipmapDataOffsets);
             }
+
+            image->flipVertical();
             return image;
-        }        
+        }
 
         return ReadResult::ERROR_IN_READING_FILE;
     }

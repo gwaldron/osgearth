a11d83002b1fca1bf6b8dd58ff5aceaec0797523
Glenn Waldron <gwaldron@gmail.com>
2020-04-10 14:51:21 -0400

    Grass: fix various multisampling/A2C issues and apply A2C sharpening patch

--- src/osgEarthSplat/Grass.VS.glsl
+++ src/osgEarthSplat/Grass.VS.glsl
@@ -58,7 +58,7 @@ uniform mat4 osg_ViewMatrix;
 
 uniform vec3 oe_Camera; // (vp width, vp height, LOD scale)
 
-// VRV: use a custom LOD scale uniform
+                        // VRV: use a custom LOD scale uniform
 #pragma import_defines(VRV_OSG_LOD_SCALE)
 #ifdef VRV_OSG_LOD_SCALE
 uniform float VRV_OSG_LOD_SCALE;
@@ -66,7 +66,7 @@ uniform float VRV_OSG_LOD_SCALE;
 #define VRV_OSG_LOD_SCALE oe_Camera.z
 #endif
 
-// Output grass texture coords to the FS
+                        // Output grass texture coords to the FS
 out vec2 oe_GroundCover_texCoord;
 
 // Output that selects the land cover texture from the texture array (flat)
@@ -266,7 +266,7 @@ void oe_Grass_VS(inout vec4 vertex)
     vp_Color = vec4(1,1,1,falloff);
 
     // darken as the fill level decreases
-    vp_Color.rgb *= decel(fillEdgeFactor);
+    vp_Color.rgb *= 0.75+( decel(fillEdgeFactor)*(1.0-0.75) );
 
     // texture coordinate:
     float row = float(which/4);
@@ -341,13 +341,9 @@ void oe_Grass_VS(inout vec4 vertex)
 
     vertex.xyz += bendVec;
 
-    // makeshift AO:
-    if (row == 0)
-        vp_Color.rgb *= 0.75;
-    else if (row == 1)
-        vp_Color.rgb *= 0.9;
-
-    //oe_GroundCover_texCoord.t *= 0.4;
+    // VRV_PATCH - grass textures don't cover the whole quad
+    oe_GroundCover_texCoord.t *= 0.4;
+    // VRV_PATCH END
 }
 
 
@@ -360,6 +356,7 @@ $GLSL_DEFAULT_PRECISION_FLOAT
 
 #pragma import_defines(OE_GROUNDCOVER_COLOR_SAMPLER)
 #pragma import_defines(OE_GROUNDCOVER_COLOR_MATRIX)
+
 #ifdef OE_GROUNDCOVER_COLOR_SAMPLER
 uniform sampler2D OE_GROUNDCOVER_COLOR_SAMPLER ;
 uniform mat4 OE_GROUNDCOVER_COLOR_MATRIX ;
@@ -372,6 +369,7 @@ flat in float oe_GroundCover_atlasIndex;
 vec3 vp_Normal;
 
 uniform float oe_GroundCover_maxAlpha;
+uniform int oe_GroundCover_A2C;
 
 void oe_Grass_FS(inout vec4 color)
 {
@@ -381,14 +379,21 @@ void oe_Grass_FS(inout vec4 color)
     // paint the texture
     color = texture(oe_GroundCover_billboardTex, vec3(oe_GroundCover_texCoord, oe_GroundCover_atlasIndex)) * color;
 
-    if(color.a < oe_GroundCover_maxAlpha) //0.15)
+    if (oe_GroundCover_A2C == 1)
+    {
+        // https://medium.com/@bgolus/anti-aliased-alpha-test-the-esoteric-alpha-to-coverage-8b177335ae4f
+        color.a = (color.a - oe_GroundCover_maxAlpha) / max(fwidth(color.a), 0.0001) + 0.5;
+    }
+    else if (color.a < oe_GroundCover_maxAlpha)
+    {
         discard;
+    }
 
     // VRV_PATCH
-    // support double-sided geometry
     // counteract the normal-flipping in VRV's lighting shader
     if (gl_FrontFacing == false)
         vp_Normal = -vp_Normal;
+    //VRV_PATCH END
 
 #ifdef OE_GROUNDCOVER_COLOR_SAMPLER
     const float modulation = 0.75;

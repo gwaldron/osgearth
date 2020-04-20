54f89ccf0dee55d00b4f5d4588fba17d0f214770
Tim Moore <timoore33@gmail.com>
2020-04-20 11:02:19 +0200

    Limit cantenary curves to a maximum drop
    This uses a new bisecting root solver.

--- src/osgEarth/PowerlineLayer.cpp
+++ src/osgEarth/PowerlineLayer.cpp
@@ -361,7 +361,70 @@ namespace
         }
         double d;
     };
+
+    // Support around evaluating the catenary formula x = a * cosh((x + x1) / a) + C
+    // and solving for the parameters a,x1, and C given the arc length of the curve L and the
+    // horizontal and vertical displacement of the endpoints.
+    struct CatenaryFunc
+    {
+        double a;
+        double x1;
+        double C;
+        double L;
+        double d;
+        double h;
+        CatenaryFunc(double a_, double x1_, double C_, double L_ = 0.0, double d_ = 0.0, double h_ = 0.0)
+            : a(a_), x1(x1_), C(C_), L(L_), d(d_), h(h_)
+        {
+        }
+        double operator()(double x) const
+        {
+            return a * cosh((x + x1) /a) + C;
+        }
+        static CatenaryFunc solveIt(double L, double d, double h)
+        {
+            GFunc gfunc(d, L, h);
+            GFuncDeriv gfuncDeriv(d);
+            bool validSoln = false;
+            const double a = solve(gfunc, gfuncDeriv, d / 2, 1.0e-6, validSoln);
+            // Parameters of the curve
+            const double x1 = (a * log((L + h) / (L - h)) - d) / 2.0;
+            const double C = -a * cosh(x1 / a);
+            return CatenaryFunc(a, x1, C, L, d, h);
+        }
+    };
+    // Find the difference between a catenary's minimum and refHeight. minHeight should be negative
+    // i.e., below both towers in this usage.  d, h, and L are as in CatenaryFunc, but now L is a
+    // free variable.
+    struct MinCatHeight
+    {
+        double d;
+        double h;
+        double refHeight;
+        MinCatHeight(double d_, double h_, double refHeight_ = 0.0)
+            : d(d_), h(h_), refHeight(refHeight_)
+        {
+        }
+        double operator()(double L) const
+        {
+            if (L * L < d * d + h * h)
+            {
+                // not happening
+                return 0.0;
+            }
+            CatenaryFunc func = CatenaryFunc::solveIt(L, d, h);
+            double minHeight = 0.0;
+            if (func.x1 > 0.0)
+            {
+                // minimum is out of the range of the curve (x < 0)
+                return 0.0;
+            }
+            // Could use func.a + func.C
+            return (func(-func.x1) -refHeight);
+        }
+    };
 }
+
 
 void makeCatenary(osg::Vec3d p1, osg::Vec3d p2, const osg::Matrixd& orientation, double slack,
                   std::vector<osg::Vec3d>& result, float tessellationSize)
@@ -409,18 +472,26 @@ void makeCatenary(osg::Vec3d p1, osg::Vec3d p2, const osg::Matrixd& orientation,
     {
         h = p2local.z();
     }
-    // Arc length of the cable
-    const double L = p2local.length() * slack;
-    GFunc gfunc(d, L, h);
-    GFuncDeriv gfuncDeriv(d);
-    bool validSoln = false;
-    const double a = solve(gfunc, gfuncDeriv, d / 2, 1.0e-6, validSoln);
-    // Parameters of the curve
-    const double x1 = (a * log((L + h) / (L - h)) - d) / 2.0;
-    const double C = -a * cosh(x1 / a);
+    // straight distance between attachment points; obviously a lower limit on the cable length
+    double straightDist = p2local.length();
+    CatenaryFunc func = CatenaryFunc::solveIt(straightDist * slack, d, h);
+
+    const double maxDrop = 5.0;
+    double xMin = -func.x1;
+    double yMin = func(xMin);
+    if (0.0 < xMin && yMin < -maxDrop)
+    {
+        // Cable droops too much, so try a more expensive strategy
+        MinCatHeight minimum(d, h, -maxDrop);
+        // No downside in choosing a very low lower bound
+        double newGuess = ((slack - 1.0) * .01 + 1.0) * straightDist;
+        // centimeter tolerence is fine
+        double Lmin = solveBisect(minimum, newGuess, straightDist * slack, 0.01, 8);
+        func = CatenaryFunc::solveIt(Lmin, d, h);
+    }
     const osg::Vec3d P1(0.0, 0.0, 0.0), P2(d, 0.0, h);
     double begin, inc;
-    int numSteps = ceil(L / tessellationSize);
+    int numSteps = ceil(p2local.length() / tessellationSize);
     std::vector<osg::Vec3d> cablePts;
     if (swapped)
     {
@@ -437,7 +508,7 @@ void makeCatenary(osg::Vec3d p1, osg::Vec3d p2, const osg::Matrixd& orientation,
     double x = begin;
     for (int i = 1; i <= numSteps; ++i, x += inc)
     {
-        double z = a * cosh((x + x1) / a) + C;
+        double z = func(x);
         cablePts.push_back(osg::Vec3d(x, 0.0, z));
     }
     osg::Matrixd cat2world = FrameCat * FrameP1;

1a84a0101245dd50a22107d889f7a44f714779fc
Glenn Waldron <gwaldron@gmail.com>
2020-04-30 13:39:07 -0400

    Fix SSBO alignment issue in ground cover

--- src/osgEarth/InstanceCloud.cpp
+++ src/osgEarth/InstanceCloud.cpp
@@ -20,9 +20,8 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 #include <osgEarth/InstanceCloud>
-
-#include <osgEarth/VirtualProgram>
 #include <osgEarth/ShaderLoader>
+#include <osgEarth/Math>
 #include <osg/Program>
 #include <osg/GLExtensions>
 
@@ -41,152 +40,6 @@ using namespace osgEarth;
 #define BINDING_RENDER_BUFFER 1
 #define BINDING_POINTS_BUFFER 2
 
-namespace
-{
-
-    const char* cull_CS =
-        "#version 430\n"
-
-        "layout(local_size_x=1, local_size_y=1, local_size_z=1) in; \n"
-
-        "struct DrawElementsIndirectCommand { \n"
-        "    uint count; \n"
-        "    uint instanceCount; \n"
-        "    uint firstIndex; \n"
-        "    uint baseVertex; \n"
-        "    uint baseInstance; \n"
-        "}; \n"
-
-        "layout(binding=0, std430) buffer DrawCommandsBuffer { \n"
-        "    DrawElementsIndirectCommand cmd[]; \n"
-        "}; \n"
-
-        "struct RenderData { \n"
-        "    vec4 vertex; \n"
-        "    vec2 tilec; \n"
-        "    vec2 _padding; \n"
-        "}; \n"
-
-        "layout(binding=1, std430) writeonly buffer RenderBuffer { \n"
-        "    RenderData render[]; \n"
-        "}; \n"
-
-        "bool inFrustum(in vec4 vertex_view) \n"
-        "{ \n"
-        "    vec4 clip = gl_ProjectionMatrix * vertex_view; \n"
-        "    clip.xyz /= clip.w; \n"
-        "    return abs(clip.x) <= 1.01 && clip.y < 1.0; \n"
-        "} \n"
-
-        "uniform vec3 oe_GroundCover_LL, oe_GroundCover_UR; \n"
-        "uniform sampler2D oe_GroundCover_noiseTex; \n"
-        "uniform vec2 oe_tile_elevTexelCoeff; \n"
-        "uniform sampler2D oe_tile_elevationTex; \n"
-        "uniform mat4 oe_tile_elevationTexMatrix; \n"
-        "uniform uint oe_GroundCover_tileNum; \n"
-        "uniform float oe_GroundCover_maxDistance; \n"
-        "uniform vec3 oe_Camera; \n"
-        "uniform float oe_GroundCover_colorMinSaturation; \n"
-
-        "#pragma import_defines(OE_GROUNDCOVER_COLOR_SAMPLER) \n"
-        "#pragma import_defines(OE_GROUNDCOVER_COLOR_MATRIX) \n"
-        "#ifdef OE_GROUNDCOVER_COLOR_SAMPLER \n"
-                "uniform sampler2D OE_GROUNDCOVER_COLOR_SAMPLER ; \n"
-                "uniform mat4 OE_GROUNDCOVER_COLOR_MATRIX ; \n"
-        "#endif \n"
-
-        "#ifdef OE_GROUNDCOVER_COLOR_SAMPLER \n"
-
-        "// https://stackoverflow.com/a/17897228/4218920 \n"
-        "vec3 rgb2hsv(vec3 c) { \n"
-        "    const vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0); \n"
-        "    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g)); \n"
-        "    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r)); \n"
-        "    float d = q.x - min(q.w, q.y); \n"
-        "    const float e = 1.0e-10; \n"
-        "    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x); \n"
-        "} \n"
-
-        "bool isLegalColor(in vec2 tilec) { \n"
-        "    vec4 c = texture(OE_GROUNDCOVER_COLOR_SAMPLER, (OE_GROUNDCOVER_COLOR_MATRIX*vec4(tilec,0,1)).st); \n"
-        "    vec3 hsv = rgb2hsv(c.rgb); \n"
-        "    return hsv[1] > oe_GroundCover_colorMinSaturation; \n"
-        "} \n"
-
-        "#endif // OE_GROUNDCOVER_COLOR_SAMPLER \n"
-
-        "bool inRange(in vec4 vertex_view) { \n"
-        "    float maxRange = oe_GroundCover_maxDistance / oe_Camera.z; \n"
-        "    return (-vertex_view.z <= oe_GroundCover_maxDistance); \n"
-        "} \n"
-
-        "float getElevation(in vec2 tilec) { \n"        
-        "    vec2 elevc = tilec \n"
-        "       * oe_tile_elevTexelCoeff.x * oe_tile_elevationTexMatrix[0][0] // scale \n"
-        "       + oe_tile_elevTexelCoeff.x * oe_tile_elevationTexMatrix[3].st // bias \n"
-        "       + oe_tile_elevTexelCoeff.y; \n"
-        "    return texture(oe_tile_elevationTex, elevc).r; \n"
-        "} \n"
-
-        "void main() { \n"
-        "    const uint x = gl_GlobalInvocationID.x; \n"
-        "    const uint y = gl_GlobalInvocationID.y; \n"
-
-        "    vec2 offset = vec2(float(x), float(y)); \n"
-        "    vec2 halfSpacing = 0.5 / vec2(gl_NumWorkGroups.xy); \n"
-        "    vec2 tilec = halfSpacing + offset / vec2(gl_NumWorkGroups.xy); \n"
-
-        "    vec4 noise = textureLod(oe_GroundCover_noiseTex, tilec, 0); \n"
-
-        "    vec2 shift = vec2(fract(noise[1]*1.5), fract(noise[2]*1.5))*2.0-1.0; \n"
-        "    tilec += shift * halfSpacing; \n"
-
-        "#ifdef OE_GROUNDCOVER_COLOR_SAMPLER \n"
-        "    if (!isLegalColor(tilec)) \n"
-        "         return; \n"
-        "#endif \n"
-
-        "    vec4 vertex_model = vec4( \n"
-        "        mix(oe_GroundCover_LL.xy, oe_GroundCover_UR.xy, tilec), \n"
-        "        getElevation(tilec), 1); \n"
-
-        "    vec4 vertex_view = gl_ModelViewMatrix * vertex_model; \n"
-
-        "    if (!inRange(vertex_view)) \n"
-        "        return; \n"
-
-        "    if (!inFrustum(vertex_view)) \n"
-        "        return; \n"
-
-        "    uint slot = atomicAdd(cmd[oe_GroundCover_tileNum].instanceCount, 1); \n"
-        "    uint start = oe_GroundCover_tileNum * gl_NumWorkGroups.y * gl_NumWorkGroups.x; \n"
-        "    render[start + slot].vertex = vertex_model; \n"
-        "    render[start + slot].tilec = tilec; \n"
-        "} \n";
-
-    const char* oe_IC_renderVS =
-        "#version 430 \n"
-
-        "struct RenderData { \n"
-        "    vec4 vertex; \n"
-        "    vec2 tilec; \n"
-        "    vec2 _padding; \n"
-        "}; \n"
-
-        "layout(binding=1, std430) buffer RenderBuffer { \n"
-        "    RenderData render[]; \n"
-        "}; \n"
-
-        "out vec4 oe_layer_tilec; \n"
-
-        "void oe_InstanceCloud_renderVS(inout vec4 vertex) { \n"
-        "    vertex = render[gl_InstanceID].vertex; \n"
-        "    oe_layer_tilec = vec4(render[gl_InstanceID].tilec, 0, 1); \n"
-        "} \n";
-}
-
-//...................................................................
-
 // pre OSG-3.6 support
 #ifndef GL_DRAW_INDIRECT_BUFFER
 #define GL_DRAW_INDIRECT_BUFFER 0x8F3F
@@ -198,7 +51,8 @@ InstanceCloud::InstancingData::InstancingData() :
     commandBuffer(-1),
     pointsBuffer(-1),
     renderBuffer(-1),
-    numTilesAllocated(0u)
+    numTilesAllocated(0u),
+    ssboOffsetAlignment(-1)
 {
     // polyfill for pre-OSG 3.6 support
     osg::setGLExtensionFuncPtr(_glBufferStorage, "glBufferStorage", "glBufferStorageARB");
@@ -236,9 +90,15 @@ InstanceCloud::InstancingData::allocateGLObjects(osg::State* state, unsigned num
             commands[i].baseInstance = 0;
         }
 
+        if (ssboOffsetAlignment < 0)
+        {
+            glGetIntegerv(GL_SHADER_STORAGE_BUFFER_OFFSET_ALIGNMENT, &ssboOffsetAlignment);
+            OE_DEBUG << "SSBO ALIGN = " << ssboOffsetAlignment << std::endl;
+        }
+
         GLuint baseSize = 0; 
         GLuint numInstances = numX * numY;
-        GLuint instanceSize = 48; // see RenderData in shader (must be a multiple of 16)
+        GLuint instanceSize = 48; // see RenderData in shader (must be a multiple of a vec4)
 
         ext->glGenBuffers(1, &commandBuffer);
         ext->glBindBuffer(GL_SHADER_STORAGE_BUFFER, commandBuffer);
@@ -248,8 +108,11 @@ InstanceCloud::InstancingData::allocateGLObjects(osg::State* state, unsigned num
             NULL,                    // uninitialized memory
             GL_DYNAMIC_STORAGE_BIT); // so we can reset each frame
 
-                                     // buffer for the output data (culled points, written by compute shader)
+        // Buffer for the output data (culled points, written by compute shader)
         renderBufferTileSize = baseSize + numInstances*instanceSize;
+
+        // Align properly to satidfy glBindBufferRange
+        renderBufferTileSize = roundUpToNearestMultiple(renderBufferTileSize, ssboOffsetAlignment);
 
         ext->glGenBuffers(1, &renderBuffer);
         ext->glBindBuffer(GL_SHADER_STORAGE_BUFFER, renderBuffer);

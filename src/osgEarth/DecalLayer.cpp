4eb0901b8752891d42a3d66b37d82cf33cd7ba5c
Glenn Waldron <gwaldron@gmail.com>
2020-06-12 08:37:39 -0400

    DecalLayer: change the API for adding an elevation decal to let the user pick the image channel

--- src/osgEarth/DecalLayer.cpp
+++ src/osgEarth/DecalLayer.cpp
@@ -324,7 +324,12 @@ DecalElevationLayer::createHeightFieldImplementation(const TileKey& key, Progres
 }
 
 bool
-DecalElevationLayer::addDecal(const std::string& id, const GeoExtent& extent, const osg::Image* image, float scale)
+DecalElevationLayer::addDecal(
+    const std::string& id, 
+    const GeoExtent& extent,
+    const osg::Image* image, 
+    float scale,
+    GLenum channel)
 {
     if (!extent.isValid() || !image)
         return false;
@@ -340,9 +345,12 @@ DecalElevationLayer::addDecal(const std::string& id, const GeoExtent& extent, co
 
     ImageUtils::PixelReader read(image);
 
-    // read the value from the RED channel when there is only one channel;
-    // else read it rom the BLUE channel
-    int channel = image->getPixelFormat() == GL_RED ? 0 : 2;
+    unsigned c =
+        channel == GL_RED   ? 0u :
+        channel == GL_GREEN ? 1u :
+        channel == GL_BLUE  ? 2u :
+        3u;
+    c = osg::minimum(c, osg::Image::computeNumComponents(image->getPixelFormat())-1u);
 
     // scale up the values so that [0...1/2] is below ground
     // and [1/2...1] is above ground.
@@ -352,7 +360,7 @@ DecalElevationLayer::addDecal(const std::string& id, const GeoExtent& extent, co
         for(int s=0; s<read.s(); ++s)
         {
             read(value, s, t);
-            float h = scale * value[channel];
+            float h = scale * value[c];
             hf->setHeight(s, t, h);
         }
     }
@@ -369,7 +377,13 @@ DecalElevationLayer::addDecal(const std::string& id, const GeoExtent& extent, co
 }
 
 bool
-DecalElevationLayer::addDecal(const std::string& id, const GeoExtent& extent, const osg::Image* image, float zeroValue, float oneValue)
+DecalElevationLayer::addDecal(
+    const std::string& id, 
+    const GeoExtent& extent, 
+    const osg::Image* image, 
+    float minOffset, 
+    float maxOffset,
+    GLenum channel)
 {
     if (!extent.isValid() || !image)
         return false;
@@ -385,9 +399,12 @@ DecalElevationLayer::addDecal(const std::string& id, const GeoExtent& extent, co
 
     ImageUtils::PixelReader read(image);
 
-    // read the value from the RED channel when there is only one channel;
-    // else read it rom the BLUE channel
-    int channel = image->getPixelFormat() == GL_RED ? 0 : 2;
+    unsigned c =
+        channel == GL_RED   ? 0u :
+        channel == GL_GREEN ? 1u :
+        channel == GL_BLUE  ? 2u :
+        3u;
+    c = osg::maximum(c, osg::Image::computeNumComponents(image->getPixelFormat())-1u);
 
     osg::Vec4 value;
     for(int t=0; t<read.t(); ++t)
@@ -395,7 +412,7 @@ DecalElevationLayer::addDecal(const std::string& id, const GeoExtent& extent, co
         for(int s=0; s<read.s(); ++s)
         {
             read(value, s, t);
-            float h = zeroValue + (oneValue-zeroValue)*value[channel];
+            float h = minOffset + (maxOffset-minOffset)*value[c];
             hf->setHeight(s, t, h);
         }
     }

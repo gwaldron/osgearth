b3c44776c66ec8a45ef272a22ba3f389d8b716e8
Glenn Waldron <gwaldron@gmail.com>
2020-07-20 14:03:32 -0400

    FIL: fix infinite look bug

--- src/osgEarth/FeatureImageLayer.cpp
+++ src/osgEarth/FeatureImageLayer.cpp
@@ -925,7 +925,9 @@ FeatureImageRenderer::getFeatures(Session* session,
             }
 
             // If we didn't get any features and we have a tilekey set, try falling back.
-            if (features.empty() && localQuery.tileKey().isSet())
+            if (features.empty() && 
+                localQuery.tileKey().isSet() &&
+                localQuery.tileKey()->valid())
             {
                 localQuery.tileKey() = localQuery.tileKey().get().createParentKey();
                 if (!localQuery.tileKey()->valid())

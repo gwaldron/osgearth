2da500d0adc6993af89677fe5fe1fbcf9ac29bc2
Glenn Waldron <gwaldron@gmail.com>
2020-11-02 08:17:33 -0500

    update examples

--- tests/runterroir.bat
+++ tests/runterroir.bat
@@ -1,9 +1,8 @@
 @echo off
 osgearth_toc terroir.earth ^
-    --uniform density_power 1 2 ^
-    --uniform moisture_power 1 2 ^
-    --uniform temperature_power 1 2 ^
-    --uniform rugged_power 1 2 ^
+    --uniform dense_power 0 2 ^
+    --uniform lush_power 0 2 ^
+    --uniform rugged_power 0 2 ^
     --uniform depth 0.05 1.0 ^
     --uniform oe_sky_exposure 4.5 7.0 ^
     --uniform oe_sky_contrast 1 3 ^

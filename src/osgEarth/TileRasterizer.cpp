8b3d4f711c93cf5902d6c147c695415f7f497a17
Glenn Waldron <gwaldron@gmail.com>
2021-03-29 15:44:41 -0400

    TileRasterizer: refactor to work around texturing issue

--- src/osgEarth/TileRasterizer.cpp
+++ src/osgEarth/TileRasterizer.cpp
@@ -44,147 +44,50 @@
 using namespace osgEarth;
 using namespace osgEarth::Util;
 
-TileRasterizer::RenderOperation::RenderOperation(
-    osg::Node* node, 
-    const GeoExtent& extent,
-    std::shared_ptr<TileRasterizer::RenderData> renderData) :
-
-    _node(node),
-    _extent(extent),
-    _rd(renderData)
-{
-    //nop
-}
-
-osg::ref_ptr<osg::Image>
-TileRasterizer::RenderOperation::run(osg::State* state)
-{
-    OE_PROFILING_ZONE_NAMED("TileRasterizer:RenderOperation");
-
-    // lock the weak pointer to make sure the TileRasterizer still exists
-    // and didn't disappear out from under us
-    std::shared_ptr<RenderData> rd = _rd.lock();
-    if (rd == nullptr)
-    {
-        return nullptr;
-    }
-
-    osg::Camera* camera = rd->_sv->getCamera();
-
-    //unsigned id = gc->getState()->getContextID();
-    unsigned id = state->getContextID();
-    osg::GLExtensions* ext = osg::GLExtensions::Get(id, true);
-
-#ifdef USE_PBO
-    if (rd->_pbo == 0 && ext->isPBOSupported)
-    {
-        // Allocate a pixel buffer object for DMA readback
-        unsigned size = rd->_width * rd->_height * 4u;
-        ext->glGenBuffers(1, &rd->_pbo);
-        ext->glBindBuffer(GL_PIXEL_PACK_BUFFER_ARB, rd->_pbo);
-        ext->glBufferData(GL_PIXEL_PACK_BUFFER_ARB, size, 0, GL_STREAM_READ);
-    }
-#endif
-
-    // Set up the projection to match the geo extents
-    camera->setProjectionMatrixAsOrtho2D(
-        _extent.xMin(), _extent.xMax(),
-        _extent.yMin(), _extent.yMax());
-
-    rd->_sv->setSceneData(_node.get());
-
-    if (rd->_samplesQuery == 0)
-    {
-        // Allocate a sample-counting query
-        ext->glGenQueries(1, &rd->_samplesQuery);
-    }
-
-    rd->_sv->cull();
-
-    // initiate a query for samples passing the fragment shader
-    // to see whether we drew anything.
-    GLuint samples = 0u;
-    ext->glBeginQuery(GL_ANY_SAMPLES_PASSED, rd->_samplesQuery);
-
-    rd->_sv->draw();
-
-    {
-        OE_PROFILING_ZONE_NAMED("glEndQuery/glGet");
-        ext->glEndQuery(GL_ANY_SAMPLES_PASSED);
-        ext->glGetQueryObjectuiv(rd->_samplesQuery, GL_QUERY_RESULT, &samples);
-    }
-
-    osg::ref_ptr<osg::Image> image;
-
-    // if we rendered any samples, allocate an image and copy it over.
-    if (samples > 0u)
-    {
-        // create our new target image:
-        image = new osg::Image();
-        image->allocateImage(rd->_width, rd->_height, 1, rd->_tex->getSourceFormat(), rd->_tex->getSourceType());
-        image->setInternalTextureFormat(rd->_tex->getInternalFormat());
-
-        OE_PROFILING_ZONE_NAMED("Readback");
-
-        // make the target texture current so we can read it back.
-        rd->_tex->apply(*state);
-
-        if (rd->_pbo > 0)
-        {
-            // Use the PBO to perform a DMA transfer (faster than straight glReadPixels)
-            ext->glBindBuffer(GL_PIXEL_PACK_BUFFER_ARB, rd->_pbo);
-            glGetTexImage(GL_TEXTURE_2D, 0, rd->_tex->getSourceFormat(), rd->_tex->getSourceType(), 0);
-            GLubyte* src = (GLubyte*)ext->glMapBuffer(GL_PIXEL_PACK_BUFFER_ARB, GL_READ_ONLY_ARB);
-            if (src)
-            {
-                memcpy(image->data(), src, image->getTotalSizeInBytes());
-                ext->glUnmapBuffer(GL_PIXEL_PACK_BUFFER_ARB);
-            }
-            ext->glBindBuffer(GL_PIXEL_PACK_BUFFER_ARB, 0);
-        }
-        else
-        {
-            image->readImageFromCurrentTexture(id, false);
-        }
-    }
-    else
-    {
-        //OE_INFO << "No data" << std::endl;
-    }
-
-    return image;
-}
-
 
 TileRasterizer::TileRasterizer(unsigned width, unsigned height)
 {
-    _renderData = std::make_shared<RenderData>();
+    setCullingActive(false);
 
-    _renderData->_initialized = false;
-    _renderData->_width = width;
-    _renderData->_height = height;
+    _cx = std::make_shared<RenderContext>();
 
-    _renderData->_tex = new osg::Texture2D();
-    _renderData->_tex->setTextureSize(_renderData->_width, _renderData->_height);
-    _renderData->_tex->setSourceFormat(GL_RGBA);
-    _renderData->_tex->setInternalFormat(GL_RGBA8);
-    _renderData->_tex->setSourceType(GL_UNSIGNED_BYTE);
+    _cx->_width = width;
+    _cx->_height = height;
+
+    _cx->_tex = new osg::Texture2D();
+    _cx->_tex->setTextureSize(_cx->_width, _cx->_height);
+    _cx->_tex->setSourceFormat(GL_RGBA);
+    _cx->_tex->setInternalFormat(GL_RGBA8);
+    _cx->_tex->setSourceType(GL_UNSIGNED_BYTE);
 
     // set up the FBO camera
-    osg::Camera* rtt = new osg::Camera();
-    rtt->setCullingActive(false);
-    rtt->setClearColor(osg::Vec4(0,0,0,0));
-    rtt->setClearMask(GL_COLOR_BUFFER_BIT);
-    rtt->setReferenceFrame(rtt->ABSOLUTE_RF);
-    rtt->setRenderOrder(rtt->PRE_RENDER);
-    rtt->setRenderTargetImplementation(rtt->FRAME_BUFFER_OBJECT);
-    rtt->setImplicitBufferAttachmentMask(0, 0);
-    rtt->setSmallFeatureCullingPixelSize(0.0f);
-    rtt->setViewMatrix(osg::Matrix::identity());
-    rtt->setViewport(0, 0, width, height);
-    rtt->attach(rtt->COLOR_BUFFER, _renderData->_tex.get());
+    _cx->_rtt = new osg::Camera();
+    _cx->_rtt->setCullingActive(false);
+    _cx->_rtt->setClearColor(osg::Vec4(0,0,0,0));
+    _cx->_rtt->setClearMask(GL_COLOR_BUFFER_BIT);
+    _cx->_rtt->setReferenceFrame(osg::Camera::ABSOLUTE_RF);
+    _cx->_rtt->setRenderOrder(osg::Camera::PRE_RENDER);
+    _cx->_rtt->setRenderTargetImplementation(osg::Camera::FRAME_BUFFER_OBJECT);
+    _cx->_rtt->setImplicitBufferAttachmentMask(0, 0);
+    _cx->_rtt->setSmallFeatureCullingPixelSize(0.0f);
+    _cx->_rtt->setViewMatrix(osg::Matrix::identity());
+    _cx->_rtt->setViewport(0, 0, width, height);
+    _cx->_rtt->attach(osg::Camera::COLOR_BUFFER, _cx->_tex.get());
 
-    osg::StateSet* ss = rtt->getOrCreateStateSet();
+    _cx->_rtt->setPreDrawCallback(new DrawCallback(
+        [this](osg::RenderInfo& ri) {this->preDraw(ri); }));
+
+    _cx->_rtt->setPostDrawCallback(new DrawCallback(
+        [this](osg::RenderInfo& ri) {this->postDraw(ri); }));
+
+    _cx->_activeJob = nullptr;
+    _cx->_rttActive = false;
+
+    // GL objects
+    _cx->_samplesQuery = 0;
+    _cx->_pbo = 0;
+
+    osg::StateSet* ss = _cx->_rtt->getOrCreateStateSet();
     ss->setMode(GL_BLEND, 1);
     ss->setMode(GL_CULL_FACE, 0);
     GLUtils::setLighting(ss, 0);
@@ -193,25 +96,6 @@ TileRasterizer::TileRasterizer(unsigned width, unsigned height)
     VirtualProgram* vp = VirtualProgram::getOrCreate(ss);
     vp->setName("TileRasterizer");
     vp->setInheritShaders(false);
-
-    // GL objects
-    _renderData->_samplesQuery = 0;
-    _renderData->_pbo = 0;
-
-    // set up a sceneview to render the graph
-    _renderData->_sv = new osgUtil::SceneView();
-    _renderData->_sv->setAutomaticFlush(true);
-    _renderData->_sv->setGlobalStateSet(rtt->getOrCreateStateSet());
-    _renderData->_sv->setCamera(rtt, true);
-    _renderData->_sv->setDefaults(0u);
-    _renderData->_sv->getCullVisitor()->setIdentifier(new osgUtil::CullVisitor::Identifier());
-    _renderData->_sv->setFrameStamp(new osg::FrameStamp());
-}
-
-bool
-TileRasterizer::valid() const
-{
-    return _renderData->_sv.valid();
 }
 
 TileRasterizer::~TileRasterizer()
@@ -222,22 +106,125 @@ TileRasterizer::~TileRasterizer()
 Future<osg::ref_ptr<osg::Image>>
 TileRasterizer::render(osg::Node* node, const GeoExtent& extent)
 {
-    using ImageJob = GPUJob<osg::ref_ptr<osg::Image>>;
+    ScopedMutexLock lock(_queue);
 
-    ImageJob::Result result;
+    RenderJob* job = new RenderJob();
+    job->_node = node;
+    job->_extent = extent;
+    Future<osg::ref_ptr<osg::Image>> result = job->_promise.getFuture();
+    _queue.push(job);
+    return result;
+}
 
-    if (_renderData->_sv.valid())
+void
+TileRasterizer::traverse(osg::NodeVisitor& nv)
+{
+    if (nv.getVisitorType() == nv.CULL_VISITOR)
     {
-        std::shared_ptr<RenderOperation> op = std::make_shared<RenderOperation>(
-            node, extent, _renderData);
-
-        result = ImageJob::dispatch(
-            [op](osg::State* state, Cancelable* progress)
+        if (!_queue.empty() && !_cx->_rttActive.exchange(true))
+        {
+            ScopedMutexLock lock(_queue);
+            if (!_queue.empty())
             {
-                return op->run(state);
+                RenderJob* job = _queue.front();
+                _queue.pop();
+                _cx->_activeJob = job;
             }
-        );
+
+            if (_cx->_activeJob)
+            {
+                _cx->_rtt->setProjectionMatrixAsOrtho2D(
+                    _cx->_activeJob->_extent.xMin(), _cx->_activeJob->_extent.xMax(),
+                    _cx->_activeJob->_extent.yMin(), _cx->_activeJob->_extent.yMax());
+
+                _cx->_rtt->removeChildren(0, _cx->_rtt->getNumChildren());
+
+                _cx->_rtt->addChild(_cx->_activeJob->_node.get());
+                
+                _cx->_rtt->accept(nv);
+            }
+            else
+            {
+                _cx->_rttActive.exchange(false);
+            }
+        }
+    }    
+}
+
+void
+TileRasterizer::preDraw(osg::RenderInfo& ri)
+{
+    unsigned id = ri.getContextID();
+    osg::GLExtensions* ext = osg::GLExtensions::Get(id, true);
+
+#ifdef USE_PBO
+    if (_cx->_pbo == 0 && ext->isPBOSupported)
+    {
+        // Allocate a pixel buffer object for DMA readback
+        unsigned size = _cx->_width * _cx->_height * 4u;
+        ext->glGenBuffers(1, &_cx->_pbo);
+        ext->glBindBuffer(GL_PIXEL_PACK_BUFFER_ARB, _cx->_pbo);
+        ext->glBufferData(GL_PIXEL_PACK_BUFFER_ARB, size, 0, GL_STREAM_READ);
+    }
+#endif
+
+    if (_cx->_samplesQuery == 0)
+    {
+        // Allocate a sample-counting query
+        ext->glGenQueries(1, &_cx->_samplesQuery);
     }
 
-    return result;
+    _cx->_samples = 0u;
+    ext->glBeginQuery(GL_ANY_SAMPLES_PASSED, _cx->_samplesQuery);
+}
+
+void
+TileRasterizer::postDraw(osg::RenderInfo& ri)
+{
+    osg::ref_ptr<osg::Image> image;
+    osg::GLExtensions* ext = ri.getState()->get<osg::GLExtensions>();
+
+    // finalize the samples query
+    {
+        OE_PROFILING_ZONE_NAMED("glEndQuery/glGet");
+        ext->glEndQuery(GL_ANY_SAMPLES_PASSED);
+        ext->glGetQueryObjectuiv(_cx->_samplesQuery, GL_QUERY_RESULT, &_cx->_samples);
+    }
+
+    if (_cx->_samples > 0)
+    {
+        // create our new target image:
+        image = new osg::Image();
+        image->allocateImage(_cx->_width, _cx->_height, 1, _cx->_tex->getSourceFormat(), _cx->_tex->getSourceType());
+        image->setInternalTextureFormat(_cx->_tex->getInternalFormat());
+
+        OE_PROFILING_ZONE_NAMED("Readback");
+
+        // make the target texture current so we can read it back.
+        _cx->_tex->apply(*ri.getState());
+
+        if (_cx->_pbo > 0)
+        {
+            // Use the PBO to perform a DMA transfer (faster than straight glReadPixels)
+            ext->glBindBuffer(GL_PIXEL_PACK_BUFFER_ARB, _cx->_pbo);
+            glGetTexImage(GL_TEXTURE_2D, 0, _cx->_tex->getSourceFormat(), _cx->_tex->getSourceType(), 0);
+            GLubyte* src = (GLubyte*)ext->glMapBuffer(GL_PIXEL_PACK_BUFFER_ARB, GL_READ_ONLY_ARB);
+            if (src)
+            {
+                memcpy(image->data(), src, image->getTotalSizeInBytes());
+                ext->glUnmapBuffer(GL_PIXEL_PACK_BUFFER_ARB);
+            }
+            ext->glBindBuffer(GL_PIXEL_PACK_BUFFER_ARB, 0);
+        }
+        else
+        {
+            image->readImageFromCurrentTexture(ri.getContextID(), false);
+        }
+    }
+
+    _cx->_activeJob->_promise.resolve(image);
+    _cx->_activeJob = nullptr;
+
+    // unblock for the next frame.
+    _cx->_rttActive.exchange(false);
 }

8a53ecc48b194cd6c855adc38042f37988e20af9
Glenn Waldron <gwaldron@gmail.com>
2021-07-26 09:57:03 -0400

    Fix a bug in which the GeoTransform with a PAT was not returning the correct root node - thanks to liugaijin

--- src/osgEarth/ModelLayer.cpp
+++ src/osgEarth/ModelLayer.cpp
@@ -294,8 +294,8 @@ ModelLayer::openImplementation()
             geo->setPosition(options().location().get());
             if (pat)
                 geo->addChild(pat);
-            else
-                modelNodeParent = geo;
+
+            modelNodeParent = geo;
         }
 
         result = modelNodeParent.get();

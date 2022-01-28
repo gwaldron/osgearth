8f556fe6da1561a2dade9a20f6e7c9d3f35cf90c
Glenn Waldron <gwaldron@gmail.com>
2022-01-28 14:24:22 -0500

    reinstate the imposterbaker

--- src/applications/osgearth_imposterbaker/osgearth_imposterbaker.cpp
+++ src/applications/osgearth_imposterbaker/osgearth_imposterbaker.cpp
@@ -55,15 +55,15 @@ fail(const std::string& msg, char** argv)
 }
 
 osg::Camera*
-createColorCamera(unsigned dim)
+createColorCamera(unsigned dimx, unsigned dimy)
 {
     osg::Image* image = new osg::Image();
-    image->allocateImage(dim, dim, 1, GL_RGBA, GL_UNSIGNED_BYTE);
+    image->allocateImage(dimx, dimy, 1, GL_RGBA, GL_UNSIGNED_BYTE);
 
     osg::Camera* rtt = new osg::Camera();
     rtt->setReferenceFrame(osg::Camera::ABSOLUTE_RF);
-    rtt->setViewport(0, 0, dim, dim);
-    rtt->setClearColor(osg::Vec4(0,0,0,0));
+    rtt->setViewport(0, 0, dimx, dimy);
+    rtt->setClearColor(osg::Vec4(0, 0, 0, 0));
     rtt->setRenderOrder(osg::Camera::PRE_RENDER);
     rtt->setRenderTargetImplementation(osg::Camera::FRAME_BUFFER_OBJECT);
     //rtt->setImplicitBufferAttachmentMask(0, 0);
@@ -99,43 +99,46 @@ getImageAttachment(osg::Camera* camera)
 osg::Vec3d
 hemiOctahedronToVec3(const osg::Vec2d& e)
 {
-    osg::Vec2d temp = osg::Vec2d(e.x()+e.y(), e.x()-e.y())*0.5;
-    osg::Vec3d v(temp.x(), temp.y(), 1.0-fabs(temp.x())-fabs(temp.y()));
+    osg::Vec2d temp = osg::Vec2d(e.x() + e.y(), e.x() - e.y())*0.5;
+    osg::Vec3d v(temp.x(), temp.y(), 1.0 - fabs(temp.x()) - fabs(temp.y()));
     v.normalize();
     return v;
 }
 
 // Shaders for albedo rendering
 const char* removeVertexColors =
-    "#version 330 \n"
-    "void removeVertexColors(inout vec4 color) { \n" 
-    "    color = vec4(1,1,1,color.a); \n"
-    "} \n";
+"#version 330 \n"
+"void removeVertexColors(inout vec4 color) { \n"
+"    color = vec4(1,1,1,color.a); \n"
+"} \n";
 
 const char* discardAlpha =
-    "#version 330 \n"
-    "void discardAlpha(inout vec4 color) { \n" 
-    "    if (color.a < 0.15) discard; \n"
-    "} \n";
+"#version 330 \n"
+"void discardAlpha(inout vec4 color) { \n"
+"    color.a = step(0.15, color.a); \n"
+"    if (color.a < 0.15) discard; \n"
+"} \n";
 
 // Shaders for rendering the normal map
 const char* normalMapVS =
-    "#version 330 \n"
-    "out vec3 modelNormal; \n"
-    "void normalMapVS(inout vec4 vertex) { \n"
-    "    modelNormal = gl_NormalMatrix * gl_Normal; \n"
-    "} \n";
+"#version 330 \n"
+"out vec3 modelNormal; \n"
+"void normalMapVS(inout vec4 vertex) { \n"
+"    modelNormal = gl_NormalMatrix * gl_Normal; \n"
+"} \n";
 const char* normalMapFS =
-    "#version 330 \n"
-    "in vec3 modelNormal; \n"
-    "out vec4 encodedNormal; \n"
-    "void normalMapFS(inout vec4 color) { \n" 
-    "    encodedNormal = vec4((modelNormal.xyz+1.0)*0.5, 1.0); \n"
-    "} \n";
+"#version 330 \n"
+"in vec3 modelNormal; \n"
+"out vec4 encodedNormal; \n"
+"void normalMapFS(inout vec4 color) { \n"
+"    encodedNormal = vec4((modelNormal.xyz+1.0)*0.5, 1.0); \n"
+"} \n";
 
 int
 main(int argc, char** argv)
 {
+    osgEarth::initialize();
+
     osg::ArgumentParser arguments(&argc, argv);
     osgViewer::Viewer viewer(arguments);
     viewer.setUpViewInWindow(30, 30, 1024, 1024);
@@ -152,7 +155,7 @@ main(int argc, char** argv)
     if (!arguments.read("--out", outfile))
         return fail("Missing required --out", argv);
 
-    std::string ext  = osgDB::getFileExtension(outfile);
+    std::string ext = osgDB::getFileExtension(outfile);
     std::string base = osgDB::getNameLessExtension(outfile);
     std::string normalMapOutFile = Stringify() << base << ".normal." << ext;
 
@@ -172,6 +175,10 @@ main(int argc, char** argv)
     myu = osg::clampBetween(myu, 0.0, 0.99999);
     myv = osg::clampBetween(myv, 0.0, 0.49999);
 
+    bool tight_fit = arguments.read("--tight");
+    if (tight_fit)
+        numFrames = 1;
+
     osg::ref_ptr<osg::Node> model = osgDB::readRefNodeFile(infile);
     if (!model.valid())
         return fail("Cannot load model", argv);
@@ -180,7 +187,7 @@ main(int argc, char** argv)
     Registry::instance()->shaderGenerator().run(model.get());
 
     // test:
-    osg::Vec3d test = hemiOctahedronToVec3(osg::Vec2d(0,0));
+    osg::Vec3d test = hemiOctahedronToVec3(osg::Vec2d(0, 0));
     OE_INFO << LC << "oct(0,0) => " << test.x() << "," << test.y() << "," << test.z() << std::endl;
 
     // compute the bbox of the model and compute the maximum span (x,y,z)
@@ -190,7 +197,7 @@ main(int argc, char** argv)
     double x = modelBB.xMax() - modelBB.xMin();
     double y = modelBB.yMax() - modelBB.yMin();
     double z = modelBB.zMax() - modelBB.zMin();
-    double maxSpan = osg::maximum(x, osg::maximum(y, z));    
+    double maxSpan = osg::maximum(x, osg::maximum(y, z));
     double halfMaxSpan = 0.5*maxSpan;
 
     // offset the model so the origin is at its bounding box centroid.
@@ -200,7 +207,7 @@ main(int argc, char** argv)
 
     // number of frames in each dimension.
     double unitFrameSize = 1.0 / (double)numFrames;
-    
+
     osg::Group* models = new osg::Group();
 
     models->getOrCreateStateSet()->setMode(GL_CULL_FACE, 0);
@@ -209,7 +216,7 @@ main(int argc, char** argv)
     if (numFrames == 1)
     {
         osg::PositionAttitudeTransform* xform = new osg::PositionAttitudeTransform();
-        xform->setPosition(osg::Vec3d(0,0,0));
+        xform->setPosition(osg::Vec3d(0, 0, 0));
         xform->addChild(modelPAT);
 
         models->addChild(xform);
@@ -257,11 +264,35 @@ main(int argc, char** argv)
     osg::Group* root = new osg::Group();
     root->addChild(models);
 
-    osg::Camera* colorCamera = createColorCamera(size);
+    osg::Camera* colorCamera;
+
+    if (tight_fit && numFrames == 1)
+    {
+        unsigned cam_w, cam_h;
+        if (myv == 0) {
+            double m = std::max(x, y);
+            if (z >= m) {
+                cam_h = size;
+                cam_w = float(size) * float(m) / float(z);
+            }
+            else {
+                cam_w = size;
+                cam_h = float(size) * float(z) / float(m);
+            }
+        }
+        else cam_w = size, cam_h = size;
+
+        colorCamera = createColorCamera(cam_w, cam_h);
+    }
+    else
+    {
+        colorCamera = createColorCamera(size, size);
+    }
+
     colorCamera->addChild(models);
     root->addChild(colorCamera);
 
-    VirtualProgram* colorVP = VirtualProgram::getOrCreate(colorCamera->getOrCreateStateSet());    
+    VirtualProgram* colorVP = VirtualProgram::getOrCreate(colorCamera->getOrCreateStateSet());
     colorVP->setFunction("removeVertexColors", removeVertexColors, ShaderComp::LOCATION_FRAGMENT_COLORING, 0.0f);
     colorVP->setFunction("discardAlpha", discardAlpha, ShaderComp::LOCATION_FRAGMENT_COLORING);
 
@@ -277,9 +308,20 @@ main(int argc, char** argv)
     viewer.setSceneData(root);
     viewer.realize();
 
-    
+
     osg::Matrix proj, view;
-    proj.makeOrtho(-halfMaxSpan, -halfMaxSpan+maxSpan*numFrames, -halfMaxSpan, -halfMaxSpan+maxSpan*numFrames, -maxSpan, maxSpan);
+    if (tight_fit && numFrames == 1)
+    {
+        double w = std::max(x, y);
+        if (myv == 0)
+            proj.makeOrtho(-w * 0.5, w*0.5, -z * 0.5, z*0.5, -w, +w);
+        else // myv == 1
+            proj.makeOrtho(-w * 0.5, w*0.5, -w * 0.5, w*0.5, -w, +w);
+    }
+    else
+    {
+        proj.makeOrtho(-halfMaxSpan, -halfMaxSpan + maxSpan * numFrames, -halfMaxSpan, -halfMaxSpan + maxSpan * numFrames, -maxSpan, maxSpan);
+    }
     view.makeLookAt(osg::Vec3d(0, 0, 0), osg::Vec3d(0, 1, 0), osg::Vec3d(0, 0, 1));
 
     colorCamera->setProjectionMatrix(proj);

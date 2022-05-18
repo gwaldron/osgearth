60a1b011eed28057384ce312f1e5b08616b6a6de
Jason Beverage <jasonbeverage@gmail.com>
2022-05-18 10:24:15 -0400

    Speed up getBiome by adding an additional biomes_by_id map

--- src/osgEarthProcedural/Biome.cpp
+++ src/osgEarthProcedural/Biome.cpp
@@ -382,7 +382,8 @@ BiomeCatalog::BiomeCatalog(const Config& conf) :
     {
         Biome biome(b_conf, &_assets);
         biome._index = _biomeIndexGenerator++;
-        _biomes_by_index[biome._index] = std::move(biome);
+        _biomes_by_index[biome._index] = biome;
+        _biomes_by_id[biome._id.get()] = biome;
     }
 
     // resolve the explicit parent biome pointers.
@@ -546,15 +547,9 @@ BiomeCatalog::getBiomeByIndex(int index) const
 
 const Biome*
 BiomeCatalog::getBiome(const std::string& id) const
-{
-    for (auto& iter : _biomes_by_index)
-    {
-        if (iter.second.id().get() == id)
-        {
-            return &iter.second;
-        }
-    }
-    return nullptr;
+{    
+    const auto i = _biomes_by_id.find(id);
+    return i != _biomes_by_id.end() ? &i->second : nullptr;
 }
 
 std::vector<const Biome*>

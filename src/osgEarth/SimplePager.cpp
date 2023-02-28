00f0770b1b8d084940ee8891a003a68f60b0634a
Glenn Waldron <gwaldron@gmail.com>
2023-02-28 11:04:38 -0500

    SimplePager: fixed a couple things preventing it from working in a projected map

--- src/osgEarth/SimplePager.cpp
+++ src/osgEarth/SimplePager.cpp
@@ -34,11 +34,11 @@ _mutex("SimplePager(OE)")
 {
     if (map)
     {
-        _mapProfile = Profile::create(Profile::GLOBAL_GEODETIC);
+        _mapProfile = map->getProfile();
     }
     else
     {
-        _mapProfile = map->getProfile();
+        _mapProfile = Profile::create(Profile::GLOBAL_GEODETIC);
     }
 }
 
@@ -67,14 +67,15 @@ osg::BoundingSphered SimplePager::getBounds(const TileKey& key) const
     // TODO:  This is very similar to the code in FeatureModelGraph::getBoundInWorldCoords, consolidate it at some point.
     GeoExtent workingExtent;
 
-    if (key.getProfile()->getSRS()->isGeographic())
-    {
-        workingExtent = key.getExtent();
-    }
-    else
-    {
-        workingExtent = _mapProfile->clampAndTransformExtent(key.getExtent());
-    }
+    workingExtent = _mapProfile->clampAndTransformExtent(key.getExtent());
+    //if (key.getProfile()->getSRS()->isGeographic())
+    //{
+    //    workingExtent = key.getExtent();
+    //}
+    //else
+    //{
+    //    workingExtent = _mapProfile->clampAndTransformExtent(key.getExtent());
+    //}
 
     GeoPoint center = workingExtent.getCentroid();
     unsigned lod = _mapProfile->getLOD(workingExtent.height());
@@ -174,7 +175,7 @@ SimplePager::createPagedNode(const TileKey& key, ProgressCallback* progress)
     pagedNode->setRadius(tileRadius);
 
     // Assume geocentric for now.
-    if (true)
+    if (_mapProfile->getSRS()->isGeographic())
     {
         const GeoExtent& ccExtent = key.getExtent();
         if (ccExtent.isValid())

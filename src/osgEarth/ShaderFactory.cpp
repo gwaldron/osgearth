9556b93762a5422b9309a8829e2ab80f4b5d3f40
Glenn Waldron <gwaldron@gmail.com>
2023-03-15 12:36:45 -0400

    Fix a missing return value bug

--- src/osgEarth/ShaderFactory.cpp
+++ src/osgEarth/ShaderFactory.cpp
@@ -102,6 +102,7 @@ ShaderFactory::addPreProcessorCallback(
 {  
     UID uid = osgEarth::createUID();
     ShaderPreProcessor::_pre_callbacks[uid] = { host, cb };
+    return uid;
 }
 
 void
@@ -117,6 +118,7 @@ ShaderFactory::addPostProcessorCallback(
 {
     UID uid = osgEarth::createUID();
     ShaderPreProcessor::_post_callbacks[uid] = { host, cb };
+    return uid;
 }
 
 void

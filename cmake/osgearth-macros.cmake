db26806c5aea0e7f4fb3bd7296fc4e11501d3086
Glenn Waldron <gwaldron@gmail.com>
2024-05-28 08:51:50 -0400

    Update cmake with verbosity settings

--- cmake/osgearth-macros.cmake
+++ cmake/osgearth-macros.cmake
@@ -343,7 +343,8 @@ macro(add_osgearth_library)
     
     # profiler:
     if(Tracy_FOUND)
-        target_link_libraries(${MY_TARGET} PUBLIC Tracy::TracyClient)    endif()
+        target_link_libraries(${MY_TARGET} PUBLIC Tracy::TracyClient)
+    endif()
     
     # soversions - append SO version to shared object files on unix (e.g., osgearth.so.123)
     if (OSGEARTH_SONAMES)

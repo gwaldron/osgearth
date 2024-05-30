db75a70da16cfa570a6f6ab54e37fba265b2d876
Glenn Waldron <gwaldron@gmail.com>
2024-05-30 13:24:08 -0400

    Cesium Native: rework the cmake configuration so cmake doesn't apply the wrong libraries to the osgEarthCesium nodekit.

--- cmake/FindCesiumNative.cmake
+++ cmake/FindCesiumNative.cmake
@@ -1,94 +1,105 @@
-SET(CESIUM_NATIVE_DIR "" CACHE PATH "Root directory of cesium-native distribution")
+set(CESIUM_NATIVE_DIR "" CACHE PATH "Root directory of cesium-native distribution")
 
-FIND_PATH(CESIUM_NATIVE_INCLUDE_DIR CesiumUtility/Uri.h
-  PATHS
-    ${CESIUM_NATIVE_DIR}
-    $ENV{CESIUM_NATIVE_DIR}
-  PATH_SUFFIXES include
-)
+set(CESIUM_NATIVE_FOUND FALSE)
 
-MACRO( FIND_CESIUM_LIBRARY MYLIBRARY MYLIBRARYNAME )
-
-FIND_LIBRARY(${MYLIBRARY}_LIBRARY_DEBUG
-    NAMES
-        ${MYLIBRARYNAME}d
+# Location the cesium-native installation:
+find_path(CESIUM_NATIVE_INCLUDE_DIR CesiumUtility/Uri.h
     PATHS
-        ${CESIUM_NATIVE_DIR}/lib
+        ${CESIUM_NATIVE_DIR}
         $ENV{CESIUM_NATIVE_DIR}
-        PATH_SUFFIXES lib64 lib
-    NO_DEFAULT_PATH
-     )
+    PATH_SUFFIXES
+        include )
+        
+# Macro to locate each cesium library.
+macro(find_cesium_library MY_LIBRARY_VAR MY_LIBRARY_NAME)
 
-FIND_LIBRARY(${MYLIBRARY}_LIBRARY_RELEASE
-     NAMES
-         ${MYLIBRARYNAME}
-     PATHS
-         ${CESIUM_NATIVE_DIR}/lib
-         $ENV{CESIUM_NATIVE_DIR}
-         PATH_SUFFIXES lib64 lib
-     NO_DEFAULT_PATH
-      )
+    # start by clearing any existing value so we pick the dependency
+    # that built with the cesium-native package
+    unset(${MY_LIBRARY_VAR}_LIBRARY_DEBUG CACHE)
+    unset(${MY_LIBRARY_VAR}_LIBRARY_RELEASE CACHE)
 
-select_library_configurations(${MYLIBRARY})
+    if (NOT CESIUM_LIBRARY_MISSING)
+        find_library(${MY_LIBRARY_VAR}_LIBRARY_DEBUG
+            NAMES
+                ${MY_LIBRARY_NAME}d
+            PATHS
+                ${CESIUM_NATIVE_DIR}/lib
+                $ENV{CESIUM_NATIVE_DIR}
+                PATH_SUFFIXES lib64 lib
+            NO_DEFAULT_PATH )
 
-ENDMACRO(FIND_CESIUM_LIBRARY LIBRARY LIBRARYNAME)
+        find_library(${MY_LIBRARY_VAR}_LIBRARY_RELEASE
+             NAMES
+                 ${MY_LIBRARY_NAME}
+             PATHS
+                 ${CESIUM_NATIVE_DIR}/lib
+                 $ENV{CESIUM_NATIVE_DIR}
+                 PATH_SUFFIXES lib64 lib
+             NO_DEFAULT_PATH )
 
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_3DTILES Cesium3DTiles)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_3DTILES_SELECTION Cesium3DTilesSelection)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_GEOSPATIAL CesiumGeospatial)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_UTILITY CesiumUtility)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_ION_CLIENT CesiumIonClient)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_GLTF_READER CesiumGltfReader)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_3DTILES_READER Cesium3DTilesReader)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_GEOMETRY CesiumGeometry)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_GLTF CesiumGltf)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_KTX_READ ktx_read)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_DRACO draco)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_SPDLOG spdlog)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_ASYNC++ async++)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_WEBPDECODER webpdecoder)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_MESHOPTIMIZER meshoptimizer)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_URIPARSER uriparser)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_TURBOJPEG turbojpeg)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_CSPRNG csprng)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_MODPB64 modp_b64)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_JSONREADER CesiumJsonReader)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_ASYNC CesiumAsync)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_S2GEOMETRY s2geometry)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_RASTER_OVERLAYS CesiumRasterOverlays)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_3DTILES_CONTENT Cesium3DTilesContent)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_GLTF_CONTENT CesiumGltfContent)
-FIND_CESIUM_LIBRARY(CESIUM_NATIVE_TINYXML2 tinyxml2)
+        set(MY_DEBUG_LIBRARY "${${MY_LIBRARY_VAR}_LIBRARY_DEBUG}")
+        set(MY_RELEASE_LIBRARY "${${MY_LIBRARY_VAR}_LIBRARY_RELEASE}")       
+        
+        if(MY_DEBUG_LIBRARY OR MY_RELEASE_LIBRARY)
+            # name of the import for this component:
+            set(MY_IMPORT_LIBRARY_NAME "CesiumNative::${MY_LIBRARY_NAME}")
+
+            # create the import library for this component:
+            add_library(${MY_IMPORT_LIBRARY_NAME} UNKNOWN IMPORTED)
+            set_target_properties(${MY_IMPORT_LIBRARY_NAME} PROPERTIES
+                INTERFACE_INCLUDE_DIRECTORIES "${CESIUM_NATIVE_INCLUDE_DIR}"
+                IMPORTED_LOCATION ${MY_RELEASE_LIBRARY}
+                IMPORTED_LOCATION_DEBUG ${MY_DEBUG_LIBRARY} )
+
+            # finally, add it to the main list for later.
+            list(APPEND CESIUM_NATIVE_IMPORT_LIBRARIES "${MY_IMPORT_LIBRARY_NAME}")
+        else()
+            message(STATUS "Cesium Native: Could not find ${MY_LIBRARY_NAME}...")
+            set(CESIUM_LIBRARY_MISSING TRUE)
+        endif()
+    endif()
+endmacro()
+
+# Note, it's not strictly necessary to set all these cache variables,
+# but it's nice to be able to see them in cmake-gui. -gw
+find_cesium_library(CESIUM_NATIVE_3DTILES Cesium3DTiles)
+find_cesium_library(CESIUM_NATIVE_3DTILES_SELECTION Cesium3DTilesSelection)
+find_cesium_library(CESIUM_NATIVE_GEOSPATIAL CesiumGeospatial)
+find_cesium_library(CESIUM_NATIVE_UTILITY CesiumUtility)
+find_cesium_library(CESIUM_NATIVE_ION_CLIENT CesiumIonClient)
+find_cesium_library(CESIUM_NATIVE_GLTF_READER CesiumGltfReader)
+find_cesium_library(CESIUM_NATIVE_3DTILES_READER Cesium3DTilesReader)
+find_cesium_library(CESIUM_NATIVE_GEOMETRY CesiumGeometry)
+find_cesium_library(CESIUM_NATIVE_GLTF CesiumGltf)
+find_cesium_library(CESIUM_NATIVE_KTX_READ ktx_read)
+find_cesium_library(CESIUM_NATIVE_DRACO draco)
+find_cesium_library(CESIUM_NATIVE_SPDLOG spdlog)
+find_cesium_library(CESIUM_NATIVE_ASYNC++ async++)
+find_cesium_library(CESIUM_NATIVE_WEBPDECODER webpdecoder)
+find_cesium_library(CESIUM_NATIVE_MESHOPTIMIZER meshoptimizer)
+find_cesium_library(CESIUM_NATIVE_URIPARSER uriparser)
+find_cesium_library(CESIUM_NATIVE_TURBOJPEG turbojpeg)
+find_cesium_library(CESIUM_NATIVE_CSPRNG csprng)
+find_cesium_library(CESIUM_NATIVE_MODPB64 modp_b64)
+find_cesium_library(CESIUM_NATIVE_JSONREADER CesiumJsonReader)
+find_cesium_library(CESIUM_NATIVE_ASYNC CesiumAsync)
+find_cesium_library(CESIUM_NATIVE_S2GEOMETRY s2geometry)
+find_cesium_library(CESIUM_NATIVE_RASTER_OVERLAYS CesiumRasterOverlays)
+find_cesium_library(CESIUM_NATIVE_3DTILES_CONTENT Cesium3DTilesContent)
+find_cesium_library(CESIUM_NATIVE_GLTF_CONTENT CesiumGltfContent)
+find_cesium_library(CESIUM_NATIVE_TINYXML2 tinyxml2)
 
 
+if(NOT CESIUM_LIBRARY_MISSING)
+    set(CESIUM_NATIVE_FOUND TRUE)
 
-SET(CESIUM_NATIVE_FOUND "NO")
-IF(CESIUM_NATIVE_3DTILES_LIBRARY AND
-   CESIUM_NATIVE_3DTILES_SELECTION_LIBRARY AND
-   CESIUM_NATIVE_GEOSPATIAL_LIBRARY AND
-   CESIUM_NATIVE_UTILITY_LIBRARY AND
-   CESIUM_NATIVE_ION_CLIENT_LIBRARY AND
-   CESIUM_NATIVE_GLTF_READER_LIBRARY AND
-   CESIUM_NATIVE_3DTILES_READER_LIBRARY AND
-   CESIUM_NATIVE_GEOMETRY_LIBRARY AND
-   CESIUM_NATIVE_GLTF_LIBRARY AND
-   CESIUM_NATIVE_KTX_READ_LIBRARY AND
-   CESIUM_NATIVE_DRACO_LIBRARY AND
-   CESIUM_NATIVE_SPDLOG_LIBRARY AND
-   CESIUM_NATIVE_ASYNC++_LIBRARY AND
-   CESIUM_NATIVE_WEBPDECODER_LIBRARY AND
-   CESIUM_NATIVE_MESHOPTIMIZER_LIBRARY AND
-   CESIUM_NATIVE_URIPARSER_LIBRARY AND
-   CESIUM_NATIVE_TURBOJPEG_LIBRARY AND
-   CESIUM_NATIVE_CSPRNG_LIBRARY AND
-   CESIUM_NATIVE_MODPB64_LIBRARY AND
-   CESIUM_NATIVE_JSONREADER_LIBRARY AND
-   CESIUM_NATIVE_ASYNC_LIBRARY AND
-   CESIUM_NATIVE_S2GEOMETRY_LIBRARY AND
-   CESIUM_NATIVE_TINYXML2_LIBRARY AND
-   CESIUM_NATIVE_RASTER_OVERLAYS_LIBRARY AND
-   CESIUM_NATIVE_3DTILES_CONTENT_LIBRARY AND
-   CESIUM_NATIVE_GLTF_CONTENT_LIBRARY AND
-   CESIUM_NATIVE_INCLUDE_DIR)
-  SET(CESIUM_NATIVE_FOUND "YES")
-ENDIF()
+    # Assemble all the CN components into one single import library:
+    # https://stackoverflow.com/a/48397346/4218920
+    
+    add_library(OE::CESIUM_NATIVE INTERFACE IMPORTED)
+    
+    set_property(TARGET OE::CESIUM_NATIVE PROPERTY
+        INTERFACE_LINK_LIBRARIES ${CESIUM_NATIVE_IMPORT_LIBRARIES} )
+else()
+    message(FATAL_ERROR "Failed to find all Cesium Native libraries. Did you set CESIUM_NATIVE_DIR?")
+endif()

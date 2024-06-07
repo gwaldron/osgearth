e7e23743954d80f2d5618246753a41936b22905c
Glenn Waldron <gwaldron@gmail.com>
2024-06-07 10:26:42 -0400

    Add missing debug postfix et al for windows builds

--- cmake/oe_win32.cmake
+++ cmake/oe_win32.cmake
@@ -1,6 +1,10 @@
 # Windows stuff
 if(WIN32 AND NOT ANDROID)
     if(MSVC)
+        set(CMAKE_DEBUG_POSTFIX  "d" CACHE STRING "add a Debug postfix, usually d on windows")
+        set(CMAKE_RELEASE_POSTFIX "" CACHE STRING "add a Release postfix, usually empty on windows")
+        set(CMAKE_RELWITHDEBINFO_POSTFIX "" CACHE STRING "add a RelWithDebInfo postfix, usually empty on windows")
+        set(CMAKE_MINSIZEREL_POSTFIX "s" CACHE STRING "add a MinSizeRel postfix, usually empty on windows")
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
         set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /bigobj") #MSVC 2017/2019
         add_definitions(-D_SCL_SECURE_NO_WARNINGS)

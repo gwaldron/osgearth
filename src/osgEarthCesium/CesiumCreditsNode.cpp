743a46e564a0f0354f39b1e4ea8d3d59ef3215ef
Remo E <remo.eichenberger@gmail.com>
2024-06-08 10:34:54 +0200

    fix: mac build

--- src/osgEarthCesium/CesiumCreditsNode.cpp
+++ src/osgEarthCesium/CesiumCreditsNode.cpp
@@ -45,7 +45,7 @@ namespace
             auto searchStartItr = input.begin();
             decltype(searchStartItr) imgItr;
             while ((imgItr = std::search(searchStartItr, input.end(), img.begin(), img.end(),
-                [&](unsigned char c1, unsigned char c2)
+                [&](char c1, char c2)
                 {
                     return std::toupper(c1, locale) == std::toupper(c2, locale);
                 })) != input.end())

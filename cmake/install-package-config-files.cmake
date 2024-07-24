0f0ecc7855ee24daa79c21a2034fb68fbe3ec7a4
Paul Levy <paul.levy@gmail.com>
2024-07-24 13:11:08 -0400

    changes for vcpkg (#2597)
    Updates to help osgEarth build and install properly with vcpkg

--- cmake/install-package-config-files.cmake
+++ cmake/install-package-config-files.cmake
@@ -2,31 +2,31 @@ function(osgearth_install_package_config_files TARGET TARGET_VERSION INCLUDE_INS
 
     include(CMakePackageConfigHelpers)
 
-    set(PACKAGE_INSTALL_DIR share)
+    set(PACKAGE_INSTALL_DIR share/osgearth)
     #set(INCLUDE_INSTALL_DIR ${CMAKE_INSTALL_INCLUDEDIR})
     #set(LIBRARY_INSTALL_DIR ${CMAKE_INSTALL_LIBDIR})
 
     configure_package_config_file(
-        "${TARGET}Config.cmake.in"
-        "${TARGET}Config.cmake"
+        "${TARGET}-config.cmake.in"
+        "${TARGET}-config.cmake"
         INSTALL_DESTINATION ${PACKAGE_INSTALL_DIR}
         PATH_VARS INCLUDE_INSTALL_DIR LIBRARY_INSTALL_DIR) 
 
     write_basic_package_version_file(
-        "${TARGET}ConfigVersion.cmake"
+        "${TARGET}-configVersion.cmake"
         VERSION ${TARGET_VERSION}
         COMPATIBILITY AnyNewerVersion)
 
     install(
         EXPORT ${TARGET}Targets
-        FILE ${TARGET}Targets.cmake
+        FILE ${TARGET}-targets.cmake
         NAMESPACE ${TARGET}::
         DESTINATION ${PACKAGE_INSTALL_DIR} )
         
     install(
         FILES
-            "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}Config.cmake"
-            "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}ConfigVersion.cmake"
+            "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}-config.cmake"
+            "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}-configVersion.cmake"
         DESTINATION
              ${PACKAGE_INSTALL_DIR} )
     

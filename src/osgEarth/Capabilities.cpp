4fadb6157abf1d9446cc4f75102a0a3b04665861
Daniel Emminizer <3845830+emminizer@users.noreply.github.com>
2024-08-27 08:36:17 -0400

    Capabilities: Remove unused Version include that does not compile (#2614)

--- src/osgEarth/Capabilities.cpp
+++ src/osgEarth/Capabilities.cpp
@@ -17,7 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 #include "Capabilities"
-#include "Version"
 #include "SpatialReference"
 #include "GEOS"
 #include "Registry"

0315fc5ee7732fc919b435242cfad1bc48b6ebd0
Glenn Waldron <gwaldron@gmail.com>
2025-01-02 11:52:48 -0500

    update weejobs

--- src/osgEarth/weejobs.h
+++ src/osgEarth/weejobs.h
@@ -11,7 +11,6 @@
 #include <condition_variable>
 #include <cstdlib>
 #include <functional>
-#include <list>
 #include <mutex>
 #include <thread>
 #include <type_traits>
@@ -19,7 +18,7 @@
 #include <string>
 #include <algorithm>
 
- // OPTIONAL: Define WEEJOBS_EXPORT if you want to use this library from multiple modules (DLLs)
+// OPTIONAL: Define WEEJOBS_EXPORT if you want to use this library from multiple modules (DLLs)
 #ifndef WEEJOBS_EXPORT
 #define WEEJOBS_EXPORT
 #endif
@@ -32,14 +31,14 @@
 // Version
 #define WEEJOBS_VERSION_MAJOR 1
 #define WEEJOBS_VERSION_MINOR 0
-#define WEEJOBS_VERSION_REV   1
+#define WEEJOBS_VERSION_REV   3
 #define WEEJOBS_STR_NX(s) #s
 #define WEEJOBS_STR(s) WEEJOBS_STR_NX(s)
 #define WEEJOBS_COMPUTE_VERSION(major, minor, patch) ((major) * 10000 + (minor) * 100 + (patch))
 #define WEEJOBS_VERSION_NUMBER WEEJOBS_COMPUTE_VERSION(WEEJOBS_VERSION_MAJOR, WEEJOBS_VERSION_MINOR, WEEJOBS_VERSION_REV)
 #define WEEJOBS_VERSION_STRING WEEJOBS_STR(WEEJOBS_VERSION_MAJOR) "." WEEJOBS_STR(WEEJOBS_VERSION_MINOR) "." WEEJOBS_STR(WEEJOBS_VERSION_REV)
 
-#if __cplusplus >= 201703L || _MSVC_LANG >= 201703L
+#if __cplusplus >= 201703L
 #define WEEJOBS_NO_DISCARD [[nodiscard]]
 #else
 #define WEEJOBS_NO_DISCARD
@@ -121,6 +120,14 @@ namespace WEEJOBS_NAMESPACE
                 return true;
             }
 
+            //! Set if true, reset if false.
+            inline void operator = (bool value) {
+                if (value)
+                    set();
+                else
+                    reset();
+            }
+
             //! Set the event state, causing any waiters to unblock.
             inline void set() {
                 if (!_set) {
@@ -143,6 +150,11 @@ namespace WEEJOBS_NAMESPACE
                 return _set;
             }
 
+            //! Synonymous with isSet()
+            inline operator bool() const {
+                return isSet();
+            }
+
         protected:
             bool _set;
             std::condition_variable_any _cond;
@@ -158,16 +170,18 @@ namespace WEEJOBS_NAMESPACE
         {
         public:
             //! Acquire, increasing the usage count by one
-            void acquire()
+            inline void acquire()
             {
                 std::unique_lock<std::mutex> lock(_m);
                 ++_count;
             }
 
+            inline void operator ++ () { acquire(); }
+
             //! Release, decreasing the usage count by one.
             //! When the count reaches zero, joiners will be notified and
             //! the semaphore will reset to its initial state.
-            void release()
+            inline void release()
             {
                 std::unique_lock<std::mutex> lock(_m);
                 _count = std::max(_count - 1, 0);
@@ -175,9 +189,11 @@ namespace WEEJOBS_NAMESPACE
                     _cv.notify_all();
             }
 
+            inline void operator -- () { release(); }
+
             //! Reset to initialize state; this will cause a join to occur
             //! even if no acquisitions have taken place.
-            void reset()
+            inline void reset()
             {
                 std::unique_lock<std::mutex> lock(_m);
                 _count = 0;
@@ -185,7 +201,7 @@ namespace WEEJOBS_NAMESPACE
             }
 
             //! Current count in the semaphore
-            std::size_t count() const
+            inline std::size_t count() const
             {
                 std::unique_lock<std::mutex> lock(_m);
                 return _count;
@@ -195,7 +211,7 @@ namespace WEEJOBS_NAMESPACE
             //! (It must first have left zero)
             //! Warning: this method will block forever if the count
             //! never reaches zero!
-            void join()
+            inline void join()
             {
                 std::unique_lock<std::mutex> lock(_m);
                 while (_count > 0)
@@ -205,7 +221,7 @@ namespace WEEJOBS_NAMESPACE
             //! Block until the semaphore count returns to zero, or
             //! the operation is canceled.
             //! (It must first have left zero)
-            void join(cancelable* c)
+            inline void join(cancelable* c)
             {
                 _cv.wait_for(_m, std::chrono::seconds(1), [this, c]() {
                     return
@@ -554,7 +570,6 @@ namespace WEEJOBS_NAMESPACE
         {
             std::lock_guard<std::mutex> lock(_queue_mutex);
             _queue.clear();
-            _queue_size = 0;
             _metrics.canceled += _metrics.pending;
             _metrics.pending = 0;
         }
@@ -578,7 +593,6 @@ namespace WEEJOBS_NAMESPACE
                     std::lock_guard<std::mutex> lock(_queue_mutex);
 
                     _queue.emplace_back(detail::job{ context, delegate });
-                    _queue_size++;
 
                     _metrics.pending++;
                     _metrics.total++;
@@ -607,7 +621,7 @@ namespace WEEJOBS_NAMESPACE
                 std::lock_guard<std::mutex> lock(_queue_mutex);
                 return _take_job(output, false);
             }
-            else if (!_done && _queue_size > 0)
+            else if (!_done && !_queue.empty())
             {
                 auto ptr = _queue.end();
                 float highest_priority = -FLT_MAX;
@@ -628,8 +642,10 @@ namespace WEEJOBS_NAMESPACE
                     ptr = _queue.begin();
 
                 output = std::move(*ptr);
-                _queue.erase(ptr);
-                _queue_size--;
+                if (_queue.size() > 1)
+                    *ptr = std::move(_queue.back());
+                _queue.resize(_queue.size() - 1);
+
                 _metrics.pending--;
                 return true;
             }
@@ -643,6 +659,7 @@ namespace WEEJOBS_NAMESPACE
         {
             _metrics.name = name;
             _metrics.concurrency = 0;
+            _queue.reserve(256);
         }
 
         //! Pulls queued jobs and runs them in whatever thread run() is called from.
@@ -659,8 +676,7 @@ namespace WEEJOBS_NAMESPACE
         inline void join_threads();
 
         bool _can_steal_work = true;
-        std::list<detail::job> _queue;
-        std::atomic_int _queue_size = { 0 }; // don't use list::size(), it's slow and not atomic
+        std::vector<detail::job> _queue;
         mutable std::mutex _queue_mutex; // protect access to the queue
         mutable std::mutex _quit_mutex; // protects access to _done
         std::atomic<unsigned> _target_concurrency; // target number of concurrent threads in the pool
@@ -723,7 +739,10 @@ namespace WEEJOBS_NAMESPACE
 
     //! Returns the job pool with the given name, creating a new one if it doesn't 
     //! already exist. If you don't specify a name, a default pool is used.
-    inline jobpool* get_pool(const std::string& name = {})
+    //! @param name Name of the pool to fetch (or create)
+    //! @param pool_size Number of threads in the pool (if it's a new pool)
+    //! @return Pointer to the job pool
+    inline jobpool* get_pool(const std::string& name = {}, unsigned pool_size = 2u)
     {
         std::lock_guard<std::mutex> lock(instance()._pools_mutex);
 
@@ -732,7 +751,7 @@ namespace WEEJOBS_NAMESPACE
             if (pool->name() == name)
                 return pool;
         }
-        auto new_pool = new jobpool(name, 2u);
+        auto new_pool = new jobpool(name, pool_size);
         instance()._pools.push_back(new_pool);
         instance()._metrics._pools.push_back(&new_pool->_metrics);
         new_pool->start_threads();
@@ -900,7 +919,7 @@ namespace WEEJOBS_NAMESPACE
                         // work-stealing enabled: wait until any queue is non-empty
                         _block.wait(lock, [this]() { return get_metrics()->total_pending() > 0 || _done; });
 
-                        if (!_done && _queue_size > 0)
+                        if (!_done && !_queue.empty())
                         {
                             have_next = _take_job(next, false);
                         }
@@ -916,9 +935,9 @@ namespace WEEJOBS_NAMESPACE
                     std::unique_lock<std::mutex> lock(_queue_mutex);
 
                     // wait until just our local queue is non-empty
-                    _block.wait(lock, [this] { return (_queue_size > 0) || _done; });
+                    _block.wait(lock, [this] { return !_queue.empty() || _done; });
 
-                    if (!_done && _queue_size > 0)
+                    if (!_done && !_queue.empty())
                     {
                         have_next = _take_job(next, false);
                     }
@@ -999,7 +1018,6 @@ namespace WEEJOBS_NAMESPACE
             }
         }
         _queue.clear();
-        _queue_size = 0;
 
         // wake up all threads so they can exit
         _block.notify_all();
@@ -1032,9 +1050,9 @@ namespace WEEJOBS_NAMESPACE
             {
                 if (pool != thief)
                 {
-                    if (static_cast<std::size_t>(pool->_queue_size) > max_num_jobs)
+                    if (static_cast<std::size_t>(pool->_queue.size()) > max_num_jobs)
                     {
-                        max_num_jobs = pool->_queue_size;
+                        max_num_jobs = pool->_queue.size();
                         pool_with_most_jobs = pool;
                     }
                 }

f448ab5abc3f44c50ab5326c12bafc75d38b553e
Jason Beverage <jasonbeverage@gmail.com>
2025-05-13 10:32:03 -0400

    Added frame loop to osgearth_server to allow it to render layers that use the TileRasterizer.

--- src/applications/osgearth_server/osgearth_server.cpp
+++ src/applications/osgearth_server/osgearth_server.cpp
@@ -7,6 +7,10 @@
 #include <osgEarth/ImageLayer>
 #include <osgEarth/ElevationLayer>
 #include <osgEarth/GDAL>
+#include <osgViewer/Viewer>
+#include <osgEarth/GLUtils>
+#include <osgEarth/TileRasterizer>
+#include <osgEarth/NodeUtils>
 #include "httplib.h"
 
 using namespace osgEarth;
@@ -41,18 +45,56 @@ main(int argc, char** argv)
     if (!mapNode)
         return usage(argv[0], "No MapNode in file");
 
+    unsigned int port = 1234;
+    if (arguments.read("--port", port))
+    {
+        std::cout << "Listening on " << port << std::endl;
+    }
+
+    // configure the fastest possible frame loop.
+    GL3RealizeOperation* realizer = new GL3RealizeOperation();
+    realizer->setSyncToVBlank(false);
+
+    osgViewer::Viewer viewer;
+    viewer.setRealizeOperation(realizer);
+    viewer.setThreadingModel(viewer.SingleThreaded);
+
+    // configure for headless rendering.
+    osg::ref_ptr<osg::GraphicsContext::Traits> traits = new osg::GraphicsContext::Traits();
+    traits->x = 0;
+    traits->y = 0;
+    traits->width = 1;
+    traits->height = 1;
+    traits->windowDecoration = false;
+    traits->pbuffer = true;
+    traits->doubleBuffer = true;
+    traits->sharedContext = nullptr;
+
+    osg::ref_ptr<osg::GraphicsContext> gc = osg::GraphicsContext::createGraphicsContext(traits.get());
+    auto* camera = viewer.getCamera();
+    camera->setGraphicsContext(gc.get());
+    camera->setDrawBuffer(GL_BACK);
+    camera->setReadBuffer(GL_BACK);
+    camera->setViewport(new osg::Viewport(0, 0, traits->width, traits->height));
+
+    // disable the kdtrees for performance boost
+    osgDB::Registry::instance()->setKdTreeBuilder(nullptr);
+
+    std::mutex rootMutex;
+    osg::ref_ptr< osg::Group > root = new osg::Group;
+    viewer.setSceneData(root);
+    viewer.realize();
+
+    std::atomic<int> pendingRasterizedImages;
+
     Server svr;
-    svr.Get("/layer/:layer/:z/:x/:y", [&mapNode](const Request& req, Response& res) {
+    //svr.new_task_queue = [] { return new ThreadPool(1); };
+
+    svr.Get("/layer/:layer/:z/:x/:y", [&mapNode, &viewer, &root, &pendingRasterizedImages, &rootMutex](const Request& req, Response& res) {
         auto layerName = req.path_params.at("layer");
         auto z = req.path_params.at("z");
         auto x = req.path_params.at("x");
-        auto y = req.path_params.at("y");  
-
-        std::cout 
-            << "/layer/"<< layerName
-            << "/" << z
-            << "/" << x
-            << "/" << y << std::endl;
+        auto y = req.path_params.at("y");
 
         osgEarth::Layer* layer = mapNode->getMap()->getLayerByName<osgEarth::Layer>(layerName);
         if (layer != nullptr)
@@ -61,7 +103,26 @@ main(int argc, char** argv)
             ElevationLayer* elevationLayer = dynamic_cast<ElevationLayer*>(layer);
             if (imageLayer)
             {
-                auto image = imageLayer->createImage(osgEarth::TileKey(std::stoi(z), std::stoi(x), std::stoi(y), imageLayer->getProfile()));
+                GeoImage image;
+
+                auto* rasterizer = osgEarth::findTopMostNodeOfType<TileRasterizer>(imageLayer->getNode());
+                if (rasterizer)
+                {
+                    // make the rasterizer faster? Maybe?
+                    rasterizer->setNumRenderersPerGraphicsContext(64);
+                    rasterizer->setNumJobsToDispatchPerFrame(64);
+
+                    std::lock_guard<std::mutex> lock(rootMutex);
+                    if (!root->containsNode(rasterizer))
+                    {
+                        root->addChild(rasterizer);
+                    }                    
+                }
+
+                if  (rasterizer) ++pendingRasterizedImages;
+                image = imageLayer->createImage(osgEarth::TileKey(std::stoi(z), std::stoi(x), std::stoi(y), imageLayer->getProfile()));                
+                if (rasterizer) --pendingRasterizedImages;
+
                 if (image.valid())
                 {
                     auto rw = osgDB::Registry::instance()->getReaderWriterForExtension("png");
@@ -104,12 +165,6 @@ main(int argc, char** argv)
         auto x = req.path_params.at("x");
         auto y = req.path_params.at("y");
 
-        std::cout
-            << "/elevation/"
-            << "/" << z
-            << "/" << x
-            << "/" << y << std::endl;
-
         osg::ref_ptr<ElevationTexture> elevTex;
         if (mapNode->getMap()->getElevationPool()->getTile(osgEarth::TileKey(std::stoi(z), std::stoi(x), std::stoi(y), mapNode->getMap()->getProfile()), false, elevTex, nullptr, nullptr))
         {
@@ -122,7 +177,24 @@ main(int argc, char** argv)
         }
         });
 
-    svr.listen("0.0.0.0", 1234);
+    // Run the server in a thread so we can run the frame loop in main
+    std::thread serverThread([&svr, &port]() {
+        svr.listen("0.0.0.0", port);
+    });
+
+
+    while (true)
+    {
+        // If there are any pending futures run the frame loop to render them.
+        if (pendingRasterizedImages > 0)
+        {            
+            viewer.frame();
+        }
+        else
+        {         
+            std::this_thread::yield();
+        }
+    }
 
     return 0;
 }

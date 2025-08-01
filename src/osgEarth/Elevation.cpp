650cc840795ce2c09ad6e1ac656092ab65b5e3c1
Glenn Waldron <gwaldron@gmail.com>
2025-08-01 09:27:16 -0400

    Elevation: fix a buffer overflow when creating a empty elevation texture

--- src/osgEarth/Elevation.cpp
+++ src/osgEarth/Elevation.cpp
@@ -35,7 +35,8 @@ osgEarth::createEmptyElevationTexture()
     osg::Image* image = new osg::Image();
     image->allocateImage(1, 1, 1, ELEV_PIXEL_FORMAT, ELEV_DATA_TYPE);
     image->setInternalTextureFormat(ELEV_INTERNAL_FORMAT);
-    *((GLfloat*)image->data()) = 0.0f;
+    ImageUtils::PixelWriter write(image);
+    write(osg::Vec4f(0.0f, 0.0f, 0.0f, 0.0f), 0, 0); // zero out
     osg::Texture2D* tex = new osg::Texture2D(image);
     tex->setInternalFormat(ELEV_INTERNAL_FORMAT);
     tex->setWrap(osg::Texture::WRAP_S, osg::Texture::CLAMP_TO_EDGE);

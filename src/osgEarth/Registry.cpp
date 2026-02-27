dc98b86564f0f0e8cf69295c4db025e33eb51be2
Glenn Waldron <gwaldron@gmail.com>
2026-02-27 07:24:11 -0500

    Add a file extension alias for glb mapping to the gltf loader

--- src/osgEarth/Registry.cpp
+++ src/osgEarth/Registry.cpp
@@ -169,6 +169,7 @@ Registry::Registry() :
     osgDB::Registry::instance()->addArchiveExtension( "kmz" );
     osgDB::Registry::instance()->addArchiveExtension( "3tz");
     osgDB::Registry::instance()->addFileExtensionAlias( "3tz", "zip" );
+    osgDB::Registry::instance()->addFileExtensionAlias("glb", "gltf");
     osgDB::Registry::instance()->addMimeTypeExtensionMapping( "application/vnd.google-earth.kml+xml", "kml" );
     osgDB::Registry::instance()->addMimeTypeExtensionMapping( "application/vnd.google-earth.kml+xml; charset=utf8", "kml");
     osgDB::Registry::instance()->addMimeTypeExtensionMapping( "application/vnd.google-earth.kmz",     "kmz" );
